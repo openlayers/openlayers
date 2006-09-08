openlayers/openlayers

commit e1878d3f65872cc3ec847206d496b04b6421b9ed

use inheritance for draw()
git-svn-id: http://svn.openlayers.org/trunk/openlayers@1421 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

lib/OpenLayers/Control/MouseToolbar.js
@@ -35,14 +35,8 @@ OpenLayers.Control.MouseToolbar.prototype =
     },
     
     draw: function() {
-        OpenLayers.Control.prototype.draw.apply(this, arguments);
+        OpenLayers.Control.MouseDefaults.prototype.draw.apply(this, arguments);
         this.buttons = new Object();
-        this.map.events.register( "click", this, this.defaultClick );
-        this.map.events.register( "dblclick", this, this.defaultDblClick );
-        this.map.events.register( "mousedown", this, this.defaultMouseDown );
-        this.map.events.register( "mouseup", this, this.defaultMouseUp );
-        this.map.events.register( "mousemove", this, this.defaultMouseMove );
-        this.map.events.register( "mouseout", this, this.defaultMouseOut );
         var sz = new OpenLayers.Size(28,28);
         var centered = this.position;
         this._addButton("zoombox", "drag-rectangle-off.png", "drag-rectangle-on.png", centered, sz, "Shift->Drag to zoom to area");

commit 83f26a9336e398f1c53a4073fdb101a76bb72f93

fix bug introduced with r1421 -- mousedefaults doesnt create a div
git-svn-id: http://svn.openlayers.org/trunk/openlayers@1422 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

lib/OpenLayers/Control/MouseToolbar.js
@@ -35,6 +35,7 @@ OpenLayers.Control.MouseToolbar.prototype =
     },
     
     draw: function() {
+        OpenLayers.Control.prototype.draw.apply(this, arguments); 
         OpenLayers.Control.MouseDefaults.prototype.draw.apply(this, arguments);
         this.buttons = new Object();
         var sz = new OpenLayers.Size(28,28);