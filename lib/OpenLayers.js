openlayers/openlayers

commit 9e4bf497cabb5708fae66ba36120f4783818ef18

Remove check for Prototype.js.
git-svn-id: http://svn.openlayers.org/trunk/openlayers@1600 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

lib/OpenLayers.js
@@ -101,13 +101,6 @@ if (typeof(_OPENLAYERS_SFL_) == "undefined") {
     var allScriptTags = "";
     var host = OpenLayers._getScriptLocation() + "lib/";
 
-    // check to see if prototype.js was already loaded
-    //  if so, skip the first dynamic include 
-    //
-    var start=1;
-    try { x = Prototype; }
-    catch (e) { start=0; }
-
     for (var i = start; i < jsfiles.length; i++) {
         if (/MSIE/.test(navigator.userAgent)) {
             var currentScriptTag = "<script src='" + host + jsfiles[i] + "'></script>"; 

commit 1f4a53291fdf42ffa5da7fa8c087abc644910a5a

Fix uninitialized var error introduced in r1600.
git-svn-id: http://svn.openlayers.org/trunk/openlayers@1602 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

lib/OpenLayers.js
@@ -101,7 +101,7 @@ if (typeof(_OPENLAYERS_SFL_) == "undefined") {
     var allScriptTags = "";
     var host = OpenLayers._getScriptLocation() + "lib/";
 
-    for (var i = start; i < jsfiles.length; i++) {
+    for (var i = 0; i < jsfiles.length; i++) {
         if (/MSIE/.test(navigator.userAgent)) {
             var currentScriptTag = "<script src='" + host + jsfiles[i] + "'></script>"; 
             allScriptTags += currentScriptTag;