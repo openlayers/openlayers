961a5a66134203cdd67f4e20177c843568af7256
crschmidt <crschmidt@crschmidt.net>
2007-10-12 13:30:22 +0000

    Update release building script to build naturaldocs.
    git-svn-id: http://svn.openlayers.org/trunk/openlayers@4928 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

--- tools/release.sh
+++ tools/release.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
+
 VERSION=$1
-echo "Building OpenLayers $VERSION"
+
 svn export http://svn.openlayers.org/tags/openlayers/release-$VERSION OpenLayers-$VERSION
 cd OpenLayers-$VERSION/build
 ./build.py full
+cp OpenLayers.js ..
+
+cd ..
+
+mkdir doc/devdocs
+mkdir doc/apidocs
+rm tools/*.pyc
+
 mkdir /www/openlayers/htdocs/api/$VERSION
 cp OpenLayers.js /www/openlayers/htdocs/api/$VERSION
-cd ..
 cp -a img/ /www/openlayers/htdocs/api/$VERSION
 cp -a theme/ /www/openlayers/htdocs/api/$VERSION
-rm tools/*.pyc
+
 cd ..
-tar -zvcf OpenLayers-$VERSION.tar.gz OpenLayers-$VERSION
-zip -9r OpenLayers-$VERSION.zip OpenLayers-$VERSION
-cp OpenLayers-$VERSION.{tar.gz,zip} /www/openlayers/htdocs/download/
+
+~/nd/NaturalDocs -i OpenLayers-$VERSION/lib -o HTML OpenLayers-$VERSION/doc/devdocs -p OpenLayers-$VERSION/doc_config -s Small OL
+~/nd/NaturalDocs -i OpenLayers-$VERSION/lib -o HTML OpenLayers-$VERSION/doc/apidocs -p OpenLayers-$VERSION/apidoc_config -s Small OL
+
+tar cvfz OpenLayers-$VERSION.tar.gz OpenLayers-$VERSION/
+zip -9r OpenLayers-$VERSION.zip OpenLayers-$VERSION/
+
+cp OpenLayers-$VERSION.* /www/openlayers/htdocs/download 
