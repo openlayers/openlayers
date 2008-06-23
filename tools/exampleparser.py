f7a798d810ddc037d29ed548583a0e3d16dde083
Tim Schaub <tim.schaub@gmail.com>
2008-06-23 18:29:42 +0000

    Looks like google doesn't like the relative link with a nested sitemap.
    git-svn-id: http://svn.openlayers.org/trunk/openlayers@7434 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

--- tools/exampleparser.py
+++ tools/exampleparser.py
@@ -100,7 +100,7 @@ def createFeed(examples):
         entry.appendChild(title)
         
         link = doc.createElementNS("http://www.w3.org/2005/Atom", "link")
-        link.setAttribute("href", example["example"])
+        link.setAttribute("href", "/dev/examples/" % example["example"])
         entry.appendChild(link)
     
         summary = doc.createElementNS("http://www.w3.org/2005/Atom", "summary")
