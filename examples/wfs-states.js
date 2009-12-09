e0be3f2e658273b5bd9bace5211c841a4ad36f84
Tim Schaub <tim.schaub@gmail.com>
2009-12-09 04:04:52 +0000

    Updating the wfs-states example.
    git-svn-id: http://svn.openlayers.org/trunk/openlayers@9863 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

---
+++ examples/wfs-states.js
@@ -0,0 +1,24 @@
+var map;
+OpenLayers.ProxyHost = "/proxy/?url=";
+
+function init() {
+    map = new OpenLayers.Map("map");
+
+    var base = new OpenLayers.Layer.WMS("OpenLayers WMS",
+        "http://labs.metacarta.com/wms-c/Basic.py",
+        {layers: "basic"} 
+    );
+    map.addLayer(base);
+
+    var wfs = new OpenLayers.Layer.Vector("States", {
+        strategies: [new OpenLayers.Strategy.BBOX()],
+        protocol: new OpenLayers.Protocol.WFS({
+            url: "http://demo.opengeo.org/geoserver/wfs",
+            featureType: "states",
+            featureNS: "http://www.openplans.org/topp"
+        })
+    });
+    map.addLayer(wfs);
+
+    map.zoomToExtent(new OpenLayers.Bounds(-140.4, 25.1, -44.4, 50.5));
+}
