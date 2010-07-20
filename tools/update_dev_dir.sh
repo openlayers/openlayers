6adffe5be28cd504acf71f0a08b733fd533ad48d
crschmidt <crschmidt@crschmidt.net>
2010-07-20 15:27:13 +0000

    Add updating sandbox to script
    git-svn-id: http://svn.openlayers.org/trunk/openlayers@10502 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

--- tools/update_dev_dir.sh
+++ tools/update_dev_dir.sh
@@ -2,10 +2,10 @@
 
 # Used to update http://openlayers.org/dev/ 
 
-svn up /www/openlayers/docs/dev; 
+svn up /osgeo/openlayers/docs/dev; 
 
 # Get current 'Last Changed Rev'
-REV=`svn info /www/openlayers/docs/dev/ | grep 'Last Changed Rev' | awk '{print $4}'`
+REV=`svn info /osgeo/openlayers/docs/dev/ | grep 'Last Changed Rev' | awk '{print $4}'`
 
 # Get the last svn rev
 touch /tmp/ol_svn_rev
@@ -14,32 +14,33 @@ OLD_REV="o`cat /tmp/ol_svn_rev`"
 # If they're not equal, do some work.
 if [ ! o$REV = $OLD_REV ]; then
 
-    cd /www/openlayers/docs/dev/tools/ 
+    cd /osgeo/openlayers/docs/dev/tools/ 
     python exampleparser.py
-    cd /www/openlayers/docs/dev/build
+    cd /osgeo/openlayers/docs/dev/build
     ./build.py
     
     cp OpenLayers.js ..
     cd ..
     
     sed -i -e 's!../lib/OpenLayers.js!../OpenLayers.js!' examples/*.html
-    perl /home/crschmidt/NaturalDocs -i /www/openlayers/docs/dev/lib -o HTML /www/openlayers/dev/apidocs -p /www/openlayers/docs/dev/apidoc_config -s Default OL >/dev/null
-    perl /home/crschmidt/NaturalDocs -i /www/openlayers/docs/dev/lib -o HTML /www/openlayers/dev/docs -p /www/openlayers/docs/dev/doc_config -s Default OL >/dev/null
-
+    naturaldocs -i /osgeo/openlayers/docs/dev/lib -o HTML /osgeo/openlayers/dev/apidocs -p /osgeo/openlayers/docs/dev/apidoc_config -s Default OL >/dev/null
+    naturaldocs -i /osgeo/openlayers/docs/dev/lib -o HTML /osgeo/openlayers/dev/docs -p /osgeo/openlayers/docs/dev/doc_config -s Default OL >/dev/null
+    
+    svn up /osgeo/openlayers/dev/sandbox/
     # Record the revision
     echo -n $REV > /tmp/ol_svn_rev
 fi    
    
-svn up /www/openlayers/documentation-checkout
-REV=`svn info /www/openlayers/documentation-checkout | grep 'Last Changed Rev' | awk '{print $4}'`
+svn up /osgeo/openlayers/documentation-checkout
+REV=`svn info /osgeo/openlayers/documentation-checkout | grep 'Last Changed Rev' | awk '{print $4}'`
 # Get the last svn rev
 touch /tmp/ol_doc_rev
 OLD_REV="o`cat /tmp/ol_doc_rev`"
 # If they're not equal, do some work.
 if [ ! o$REV = $OLD_REV ]; then
-    cd /www/openlayers/documentation-checkout
+    cd /osgeo/openlayers/documentation-checkout
     make html > /dev/null
-    cp -r _build/html/*  /www/openlayers/documentation
+    cp -r _build/html/*  /osgeo/openlayers/documentation
     
     echo -n $REV > /tmp/ol_doc_rev
 fi    
