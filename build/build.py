da14451dbddf234d9f977eeac56be6523ef55e11
crschmidt <crschmidt@crschmidt.net>
2010-08-21 16:06:06 +0000

    Fix from tim_abell to make build.py callable from another function, (Closes #2468)
    git-svn-id: http://svn.openlayers.org/trunk/openlayers@10664 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

--- build/build.py
+++ build/build.py
@@ -4,47 +4,51 @@ import sys
 sys.path.append("../tools")
 import mergejs
 
-have_compressor = None
-try:
-    import jsmin
-    have_compressor = "jsmin"
-except ImportError:
+def build():
+    have_compressor = None
     try:
-        import minimize
-        have_compressor = "minimize"
-    except Exception, E:
-        print E
-        pass
+        import jsmin
+        have_compressor = "jsmin"
+    except ImportError:
+        try:
+            import minimize
+            have_compressor = "minimize"
+        except Exception, E:
+            print E
+            pass
 
-sourceDirectory = "../lib"
-configFilename = "full.cfg"
-outputFilename = "OpenLayers.js"
+    sourceDirectory = "../lib"
+    configFilename = "full.cfg"
+    outputFilename = "OpenLayers.js"
 
-if len(sys.argv) > 1:
-    configFilename = sys.argv[1]
-    extension = configFilename[-4:]
+    if len(sys.argv) > 1:
+        configFilename = sys.argv[1]
+        extension = configFilename[-4:]
 
-    if extension  != ".cfg":
-        configFilename = sys.argv[1] + ".cfg"
+        if extension  != ".cfg":
+            configFilename = sys.argv[1] + ".cfg"
 
-if len(sys.argv) > 2:
-    outputFilename = sys.argv[2]
+    if len(sys.argv) > 2:
+        outputFilename = sys.argv[2]
 
-print "Merging libraries."
-merged = mergejs.run(sourceDirectory, None, configFilename)
-if have_compressor == "jsmin":
-    print "Compressing using jsmin."
-    minimized = jsmin.jsmin(merged)
-elif have_compressor == "minimize":
-    print "Compressing using minimize."
-    minimized = minimize.minimize(merged)
-else: # fallback
-    print "Not compressing."
-    minimized = merged 
-print "Adding license file."
-minimized = file("license.txt").read() + minimized
+    print "Merging libraries."
+    merged = mergejs.run(sourceDirectory, None, configFilename)
+    if have_compressor == "jsmin":
+        print "Compressing using jsmin."
+        minimized = jsmin.jsmin(merged)
+    elif have_compressor == "minimize":
+        print "Compressing using minimize."
+        minimized = minimize.minimize(merged)
+    else: # fallback
+        print "Not compressing."
+        minimized = merged 
+    print "Adding license file."
+    minimized = file("license.txt").read() + minimized
 
-print "Writing to %s." % outputFilename
-file(outputFilename, "w").write(minimized)
+    print "Writing to %s." % outputFilename
+    file(outputFilename, "w").write(minimized)
 
-print "Done."
+    print "Done."
+
+if __name__ == '__main__':
+  build()
