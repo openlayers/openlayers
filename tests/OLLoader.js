941ebc78ce027c813cc63e6f0e93294ba83519e4
pgiraud <pierre.giraud@camptocamp.com>
2011-02-21 11:32:37 +0000

    Now we rely on relative paths + minor changes
    git-svn-id: http://svn.openlayers.org/trunk/openlayers@11167 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

--- tests/OLLoader.js
+++ tests/OLLoader.js
@@ -1,11 +1,25 @@
 // Adding a mode parameter with "build" as value in the run-tests.html will 
 // make usage of the build version of the library.
-var regexS = "[\\?&]mode=([^&#]*)";
-var regex = new RegExp( regexS );
+// get the OLLoader.js script location
+
+var r = new RegExp("(^|(.*?\\/))(" + "OLLoader.js" + ")(\\?|$)"),
+    s = document.getElementsByTagName('script'),
+    src, m, l = "";
+for(var i=0, len=s.length; i<len; i++) {
+    src = s[i].getAttribute('src');
+    if(src) {
+        var m = src.match(r);
+        if(m) {
+            l = m[1];
+            break;
+        }
+    }
+}
+
+var regex = new RegExp( "[\\?&]mode=([^&#]*)" );
 var href = window.parent.location.href;
-var url = href.substring(0, href.lastIndexOf('/') + 1);
 var results = regex.exec( href );
-url += (results && results[1] == 'build') ? 
+l += (results && results[1] == 'build') ? 
     "../build/OpenLayers.js" : "../lib/OpenLayers.js"; 
-scriptTag = "<script src='" + url + "'></script>"; 
+scriptTag = "<script src='" + l + "'></script>"; 
 document.write(scriptTag);
