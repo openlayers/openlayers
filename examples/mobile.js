c9d459c7fe91834680cc9449d75fa1a90be57748
Stéphane Brunner <stephane.brunner@camptocamp.com>
2011-03-31 09:45:50 +0000

    fix Don't let the browser to zoom or select the map on feature selection, r=erilem (closes #3212)
    git-svn-id: http://svn.openlayers.org/trunk/openlayers@11829 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

--- examples/mobile.js
+++ examples/mobile.js
@@ -1,34 +1,14 @@
 // initialize map when page ready
 var map;
 
-// Get rid of address bar on iphone/ipod
-var fixSize = function() {
-    window.scrollTo(0,0);
-    document.body.style.height = '100%';
-    if (!(/(iphone|ipod)/.test(navigator.userAgent.toLowerCase()))) {
-        if (document.body.parentNode) {
-            document.body.parentNode.style.height = '100%';
-        }
-    }
-};
-setTimeout(fixSize, 700);
-setTimeout(fixSize, 1500);
 
+ 
 var init = function () {
     // create map
     map = new OpenLayers.Map({
         div: "map",
         theme: null,
-        controls: [
-            new OpenLayers.Control.Attribution(),
-            new OpenLayers.Control.TouchNavigation({
-                dragPanOptions: {
-                    interval: 100,
-                    enableKinetic: true
-                }
-            }),
-            new OpenLayers.Control.ZoomPanel()
-        ],
+        controls: [],
         layers: [
             new OpenLayers.Layer.OSM("OpenStreetMap", null, {
                 transitionEffect: 'resize'
