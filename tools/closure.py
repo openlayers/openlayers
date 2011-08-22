bb41c995241030fddb9a4d20a787cd6ee449c245
bartvde <bartvde@osgis.nl>
2011-08-22 10:10:46 +0000

    fix closure build on Windows, p=jorix, r=me, thanks to mprins and scaddenp, I've verified that the build still works on OSX (closes #3158)
    git-svn-id: http://svn.openlayers.org/trunk/openlayers@12268 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

--- tools/closure.py
+++ tools/closure.py
@@ -2,20 +2,21 @@ import sys
 import os
 import tempfile
 
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), "closure-compiler.jar"))
+path = "../tools/closure-compiler.jar"
 if not os.path.exists(path):
     raise Exception("No closure-compiler.jar at %s; read README.txt!" % path)
 
 def minimize(code):
-    ntf = tempfile.NamedTemporaryFile()
+    ntf = tempfile.NamedTemporaryFile(delete=False)
     ntf.write(code)
     ntf.flush()
 
-    ntf2 = tempfile.NamedTemporaryFile()
-
-    os.system("java -jar %s --js %s --js_output_file %s" % (path, ntf.name, ntf2.name))
-    ntf2.seek(0)
-    data = ntf2.read()
+    ntf2 = tempfile.NamedTemporaryFile(delete=False)
     ntf.close()
     ntf2.close()
+
+    os.system("java -jar %s --js %s --js_output_file %s" % (path, ntf.name, ntf2.name))
+    data = open(ntf2.name).read()
+    os.unlink(ntf.name)
+    os.unlink(ntf2.name)
     return data
