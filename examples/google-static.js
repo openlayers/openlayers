6b8ba43e20cb85d09a72e4e4bce8547c41e46e75
ahocevar <andreas.hocevar@gmail.com>
2012-02-15 22:00:54 +0100

    Adding a scaled layer.

--- examples/google-static.js
+++ examples/google-static.js
@@ -40,7 +40,23 @@ var map = new OpenLayers.Map({
             "Google Satellite",
             "http://maps.googleapis.com/maps/api/staticmap?sensor=false&maptype=satellite", 
             null, options
-        )
+        ),
+        // the same layer again, but scaled to allow map sizes up to 1280x1280 pixels
+        new OpenLayers.Layer.Grid(
+            "Google Satellite (scale=2)",
+            "http://maps.googleapis.com/maps/api/staticmap?sensor=false&maptype=satellite&scale=2", 
+            null, OpenLayers.Util.applyDefaults({
+                getURL: function() {
+                    var center = this.map.getCenter().transform("EPSG:3857", "EPSG:4326"),
+                        size = this.map.getSize();
+                    return [
+                        this.url, "&center=", center.lat, ",", center.lon,
+                        "&zoom=", (this.map.getZoom() - 1),
+                        "&size=", Math.floor(size.w / 2), "x", Math.floor(size.h / 2)
+                    ].join("");
+                }
+            }, options)
+        ),
     ],
     center: new OpenLayers.LonLat(10.2, 48.9).transform("EPSG:4326", "EPSG:3857"),
     zoom: 5
