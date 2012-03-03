c6650087cc5758749c1282e2d822cfdc66deb66b
Tim Schaub <tim.schaub@gmail.com>
2012-03-02 23:51:39 -0700

    Update zoom example to demonstrate custom style.

--- examples/zoom.js
+++ examples/zoom.js
@@ -7,7 +7,27 @@ var map = new OpenLayers.Map({
                 enableKinetic: true
             }
         }),
+        new OpenLayers.Control.Attribution(),
         new OpenLayers.Control.Zoom()
+    ],
+    center: [0, 0],
+    zoom: 1
+});
+
+var map2 = new OpenLayers.Map({
+    div: "map2",
+    layers: [new OpenLayers.Layer.OSM()],
+    controls: [
+        new OpenLayers.Control.Navigation({
+            dragPanOptions: {
+                enableKinetic: true
+            }
+        }),
+        new OpenLayers.Control.Attribution(),
+        new OpenLayers.Control.Zoom({
+            zoomInId: "customZoomIn",
+            zoomOutId: "customZoomOut"
+        })
     ],
     center: [0, 0],
     zoom: 1
