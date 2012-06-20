5f557e6c6ce29fc725a2f1423bf12500eeef5b8f
Éric Lemoine <eric.lemoine@gmail.com>
2012-06-20 08:48:12 +0200

    the TileSet constructor takes 4 args

--- src/ol/TileSet.js
+++ src/ol/TileSet.js
@@ -5,36 +5,38 @@ goog.provide('ol.TileSet');
  * tiles. Tiles of a TileSet have the same resolution, width and
  * height.
  * @constructor
+ * @params {Array.<Array.<ol.Tile>>} tiles
+ * @params {number} tileWidth
+ * @params {number} tileHeight
+ * @params {number} resolution
  */
-ol.TileSet = function() {
+ol.TileSet = function(tiles, tileWidth, tileHeight, resolution) {
+
+    /**
+     * @private
+     * @type {Array.<Array.<ol.Tile>>}
+     */
+    this.tiles_ = tiles;
 
     /**
      * @private
      * @type {number|undefined}
      */
-    this.resolution_ = undefined;
+    this.tileWidth_ = tileWidth;
 
     /**
      * @private
      * @type {number|undefined}
      */
-    this.bounds_ = undefined;
+    this.tileHeight_ = tileHeight;
 
     /**
      * @private
      * @type {number|undefined}
      */
-    this.tileWidth_ = undefined;
+    this.resolution_ = resolution;
+};
 
-    /**
-     * @private
-     * @type {number|undefined}
-     */
-    this.tileHeight_ = undefined;
-
-    /**
-     * @private
-     * @type {Array.<ol.Tile>|undefined}
-     */
-    this.tiles_ = undefined;
+ol.TileSet.prototype.getTiles = function() {
+    return this.tiles_;
 };
