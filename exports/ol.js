61038cde5f853bf937cf5c4bb089273435e13a03
Tom Payne <twpayne@gmail.com>
2012-07-05 13:35:17 +0200

    Move ol.js and ol.export.js into exports/ol.js

--- exports/ol.js
+++ exports/ol.js
@@ -1,4 +1,34 @@
-goog.require("ol");
+goog.require('ol.base');
+goog.require('ol.bounds');
+goog.require('ol.control.Attribution');
+goog.require('ol.control.Navigation');
+goog.require('ol.control.Zoom');
+goog.require('ol.event.Events');
+goog.require('ol.event.Scroll');
+goog.require('ol.handler.Drag');
+goog.require("ol.map");
+goog.require("ol.loc");
+goog.require("ol.feature");
+goog.require("ol.projection");
+goog.require("ol.layer.xyz");
+goog.require("ol.layer.osm");
+goog.require("ol.layer.wms");
+goog.require("ol.popup");
+goog.require("ol.Tile");
+goog.require("ol.TileSet");
+goog.require("ol.TileCache");
+goog.require("ol.geom.geometry");
+goog.require("ol.geom.point");
+goog.require("ol.geom.multipoint");
+goog.require("ol.geom.linestring");
+goog.require("ol.geom.multilinestring");
+goog.require("ol.geom.collection");
+goog.require('ol.layer.XYZ');
+goog.require('ol.layer.OSM');
+goog.require('ol.layer.WMS');
+goog.require('ol.renderer.Composite');
+goog.require('ol.renderer.TileLayerRenderer');
+goog.require('ol.renderer.WebGL');
 
 // ol.map
 goog.exportSymbol('ol.map', ol.map );
