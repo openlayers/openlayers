10f8f1198b55bc48fb809ea53b77247a81695576
Tom Payne <twpayne@gmail.com>
2012-07-09 14:22:25 +0200

    Add ol.TileGrid.createOpenStreetMap

--- src/ol/tilegrid.js
+++ src/ol/tilegrid.js
@@ -4,6 +4,7 @@ goog.require('goog.array');
 goog.require('goog.asserts');
 goog.require('goog.math.Size');
 goog.require('ol.Extent');
+goog.require('ol.Projection');
 goog.require('ol.TileBounds');
 goog.require('ol.TileCoord');
 
@@ -81,6 +82,28 @@ ol.TileGrid =
    */
   this.tileSize_ = goog.isDef(opt_tileSize) ?
       opt_tileSize : new goog.math.Size(256, 256);
+
+};
+
+
+/**
+ * @param {number} maxZoom Maximum zoom.
+ * @return {ol.TileGrid} Tile grid.
+ */
+ol.TileGrid.createOpenStreetMap = function(maxZoom) {
+
+  var resolutions = new Array(maxZoom + 1);
+  var z;
+  for (z = 0; z <= maxZoom; ++z) {
+    resolutions[z] = ol.Projection.EPSG_3857_HALF_SIZE / (128 << z);
+  }
+
+  var extent = ol.Projection.EPSG_3857_EXTENT;
+  var origin = new goog.math.Coordinate(
+      -ol.Projection.EPSG_3857_HALF_SIZE, ol.Projection.EPSG_3857_HALF_SIZE);
+  var tileSize = new goog.math.Size(256, 256);
+
+  return new ol.TileGrid(resolutions, extent, origin, true, true, tileSize);
 
 };
 
