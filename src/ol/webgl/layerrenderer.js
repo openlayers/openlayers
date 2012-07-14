669a83ff784f9360983cadfebce332604c559c5f
Tom Payne <twpayne@gmail.com>
2012-07-14 21:50:35 +0200

    Add ol.webgl.LayerRenderer

---
+++ src/ol/webgl/layerrenderer.js
@@ -0,0 +1,25 @@
+goog.provide('ol.webgl.LayerRenderer');
+
+goog.require('ol.LayerRenderer');
+
+
+
+/**
+ * @constructor
+ * @extends {ol.LayerRenderer}
+ * @param {ol.webgl.Map} map Map.
+ * @param {ol.Layer} layer Layer.
+ */
+ol.webgl.LayerRenderer = function(map, layer) {
+  goog.base(this, map, layer);
+};
+goog.inherits(ol.webgl.LayerRenderer, ol.LayerRenderer);
+
+
+/**
+ * @return {WebGLRenderingContext} GL.
+ */
+ol.webgl.LayerRenderer.prototype.getGL = function() {
+  var map = /** @type {ol.webgl.Map} */ this.getMap();
+  return map.getGL();
+};
