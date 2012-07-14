5aab7cb1423f9fa2ca05a19ad907eec777eac2e5
Tom Payne <twpayne@gmail.com>
2012-07-14 21:52:02 +0200

    Add ol.TileLayer

--- src/ol/tilelayer.js
+++ src/ol/tilelayer.js
@@ -4,11 +4,24 @@ goog.require('ol.Layer');
 goog.require('ol.TileStore');
 
 
+
+/**
+ * @constructor
+ * @extends {ol.Layer}
+ * @param {ol.TileStore} tileStore Tile store.
+ * @param {Object.<string, *>=} opt_values Values.
+ */
+ol.TileLayer = function(tileStore, opt_values) {
+  goog.base(this, tileStore, opt_values);
+};
+goog.inherits(ol.TileLayer, ol.Layer);
+
+
 /**
  * @param {Object.<string, *>=} opt_values Values.
  * @return {ol.Layer} Layer.
  */
 ol.TileLayer.createOpenStreetMap = function(opt_values) {
   var store = ol.TileStore.createOpenStreetMap();
-  return new ol.Layer(store, opt_values);
+  return new ol.TileLayer(store, opt_values);
 };
