b53e5a3fda85c671d05008aad46508fcc31b8cba
Éric Lemoine <eric.lemoine@gmail.com>
2012-07-18 16:32:16 +0200

    two maps in the skeleton example

--- skeleton.js
+++ skeleton.js
@@ -4,18 +4,35 @@ goog.require('ol.RendererHint');
 goog.require('ol.createMap');
 goog.require('ol.tilelayer.createOpenStreetMap');
 
+var twoMaps = true;
 
-var target = /** @type {!HTMLDivElement} */ document.getElementById('map');
-var map = ol.createMap(target, undefined, ol.RendererHint.DOM);
+var target;
+var map, map1, map2;
+var layer;
 
-var layer = ol.tilelayer.createOpenStreetMap({
+target = /** @type {!HTMLDivElement} */ document.getElementById('map1');
+map = map1 = ol.createMap(target, undefined, ol.RendererHint.DOM);
+layer = ol.tilelayer.createOpenStreetMap({
   'opacity': 0.5
 });
 map.getLayers().push(layer);
 
 var resolutions = layer.getStore().getResolutions();
-map.setCenter(new goog.math.Coordinate(0, 0));
-map.setResolution(resolutions[0]);
+map1.setCenter(new goog.math.Coordinate(0, 0));
+map1.setResolution(resolutions[0]);
+
+if (twoMaps) {
+  target = /** @type {!HTMLDivElement} */ document.getElementById('map2');
+  map2 = ol.createMap(target, undefined, ol.RendererHint.DOM);
+  layer = ol.tilelayer.createOpenStreetMap({
+    'opacity': 0.5
+  });
+  map2.getLayers().push(layer);
+  map2.bindTo('center', map1);
+  map2.bindTo('resolution', map1);
+}
 
 goog.exportSymbol('layer', layer);
 goog.exportSymbol('map', map);
+goog.exportSymbol('map1', map1);
+goog.exportSymbol('map2', map2);
