d83235b5dc02c0f44983a04a9354d61d6a671439
Tom Payne <twpayne@gmail.com>
2012-07-19 00:29:15 +0200

    Include drag zoom control in default map

--- src/ol/createmap.js
+++ src/ol/createmap.js
@@ -8,6 +8,7 @@ goog.require('ol.MapProperty');
 goog.require('ol.Projection');
 goog.require('ol.control.DblClickZoom');
 goog.require('ol.control.DragPan');
+goog.require('ol.control.DragZoom');
 goog.require('ol.control.MouseWheelZoom');
 goog.require('ol.dom');
 goog.require('ol.dom.Map');
@@ -68,6 +69,7 @@ ol.createMap = function(target, opt_values, opt_rendererHints) {
   if (!goog.object.containsKey(values, ol.MapProperty.CONTROLS)) {
     var controls = new ol.Array();
     controls.push(new ol.control.DblClickZoom());
+    controls.push(new ol.control.DragZoom());
     controls.push(new ol.control.DragPan());
     controls.push(new ol.control.MouseWheelZoom());
     values[ol.MapProperty.CONTROLS] = controls;
