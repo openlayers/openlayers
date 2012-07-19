45c067dfdaf60b0131020216cfeb80f65b6ef00b
Éric Lemoine <eric.lemoine@gmail.com>
2012-07-19 17:47:34 +0200

    add ol.PixelBounds

---
+++ src/ol/pixelbounds.js
@@ -0,0 +1,19 @@
+goog.provide('ol.PixelBounds');
+
+goog.require('goog.asserts');
+goog.require('ol.Rectangle');
+
+
+
+/**
+ * @constructor
+ * @extends {ol.Rectangle}
+ * @param {number} minX Minimum X.
+ * @param {number} minY Minimum Y.
+ * @param {number} maxX Maximum X.
+ * @param {number} maxY Maximum Y.
+ */
+ol.PixelBounds = function(minX, minY, maxX, maxY) {
+  goog.base(this, minX, minY, maxX, maxY);
+};
+goog.inherits(ol.PixelBounds, ol.Rectangle);
