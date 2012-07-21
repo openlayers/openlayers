f46c9e12fb5c5983de1fb13a6061ff0a293725c3
Tom Payne <twpayne@gmail.com>
2012-07-22 01:13:26 +0200

    Add ol.TileBounds.containsTileBounds

--- src/ol/tilebounds.js
+++ src/ol/tilebounds.js
@@ -50,6 +50,16 @@ ol.TileBounds.prototype.clone = function() {
 
 
 /**
+ * @param {ol.TileBounds} tileBounds Tile bounds.
+ * @return {boolean} Contains.
+ */
+ol.TileBounds.prototype.contains = function(tileBounds) {
+  return this.minX <= tileBounds.minX && tileBounds.maxX <= this.maxX &&
+      this.minY <= tileBounds.minY && tileBounds.minY <= this.minY;
+};
+
+
+/**
  * @param {number} z Z.
  * @param {function(this: T, ol.TileCoord)} f Callback.
  * @param {T=} opt_obj The object to be used for the value of 'this' within f.
