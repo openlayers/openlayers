1afea5ca0ccee6c85f660617e88f1451206dd210
Tom Payne <twpayne@gmail.com>
2012-07-26 01:52:19 +0200

    Update tests

--- src/ol/tile/xyz_test.js
+++ src/ol/tile/xyz_test.js
@@ -1,42 +1,44 @@
 goog.require('goog.testing.jsunit');
 goog.require('ol.Coordinate');
 goog.require('ol.TileCoord');
-goog.require('ol.tilestore.createXYZ');
+goog.require('ol.TileUrlFunction');
+goog.require('ol.tilestore.XYZ');
 
 
 function testXYZ() {
 
-  var tileStore = ol.tilestore.createXYZ(6, ['{z}/{x}/{y}']);
-  var tileGrid = tileStore.getTileGrid();
+  var xyzTileStore = new ol.tilestore.XYZ(
+      6, ol.TileUrlFunction.createFromTemplate('{z}/{x}/{y}'));
+  var tileGrid = xyzTileStore.getTileGrid();
 
   var coordinate = new ol.Coordinate(829330.2064098881, 5933916.615134273);
   var tileUrl;
 
-  tileUrl = tileStore.getTileCoordUrl(
+  tileUrl = xyzTileStore.getTileCoordUrl(
       tileGrid.getTileCoordForCoordAndZ(coordinate, 0));
   assertEquals('0/0/0', tileUrl);
 
-  tileUrl = tileStore.getTileCoordUrl(
+  tileUrl = xyzTileStore.getTileCoordUrl(
       tileGrid.getTileCoordForCoordAndZ(coordinate, 1));
   assertEquals('1/1/0', tileUrl);
 
-  tileUrl = tileStore.getTileCoordUrl(
+  tileUrl = xyzTileStore.getTileCoordUrl(
       tileGrid.getTileCoordForCoordAndZ(coordinate, 2));
   assertEquals('2/2/1', tileUrl);
 
-  tileUrl = tileStore.getTileCoordUrl(
+  tileUrl = xyzTileStore.getTileCoordUrl(
       tileGrid.getTileCoordForCoordAndZ(coordinate, 3));
   assertEquals('3/4/2', tileUrl);
 
-  tileUrl = tileStore.getTileCoordUrl(
+  tileUrl = xyzTileStore.getTileCoordUrl(
       tileGrid.getTileCoordForCoordAndZ(coordinate, 4));
   assertEquals('4/8/5', tileUrl);
 
-  tileUrl = tileStore.getTileCoordUrl(
+  tileUrl = xyzTileStore.getTileCoordUrl(
       tileGrid.getTileCoordForCoordAndZ(coordinate, 5));
   assertEquals('5/16/11', tileUrl);
 
-  tileUrl = tileStore.getTileCoordUrl(
+  tileUrl = xyzTileStore.getTileCoordUrl(
       tileGrid.getTileCoordForCoordAndZ(coordinate, 6));
   assertEquals('6/33/22', tileUrl);
 
@@ -45,15 +47,16 @@ function testXYZ() {
 
 function testXYZWrapX() {
 
-  var tileStore = ol.tilestore.createXYZ(6, ['{z}/{x}/{y}']);
+  var xyzTileStore = new ol.tilestore.XYZ(
+      6, ol.TileUrlFunction.createFromTemplate('{z}/{x}/{y}'));
 
-  tileUrl = tileStore.getTileCoordUrl(new ol.TileCoord(6, -31, -23));
+  tileUrl = xyzTileStore.getTileCoordUrl(new ol.TileCoord(6, -31, -23));
   assertEquals('6/33/22', tileUrl);
 
-  tileUrl = tileStore.getTileCoordUrl(new ol.TileCoord(6, 33, -23));
+  tileUrl = xyzTileStore.getTileCoordUrl(new ol.TileCoord(6, 33, -23));
   assertEquals('6/33/22', tileUrl);
 
-  tileUrl = tileStore.getTileCoordUrl(new ol.TileCoord(6, 97, -23));
+  tileUrl = xyzTileStore.getTileCoordUrl(new ol.TileCoord(6, 97, -23));
   assertEquals('6/33/22', tileUrl);
 
 }
@@ -61,15 +64,16 @@ function testXYZWrapX() {
 
 function testXYZCropY() {
 
-  var tileStore = ol.tilestore.createXYZ(6, ['{z}/{x}/{y}']);
+  var xyzTileStore = new ol.tilestore.XYZ(
+      6, ol.TileUrlFunction.createFromTemplate('{z}/{x}/{y}'));
 
-  tileUrl = tileStore.getTileCoordUrl(new ol.TileCoord(6, 33, -87));
+  tileUrl = xyzTileStore.getTileCoordUrl(new ol.TileCoord(6, 33, -87));
   assertUndefined(tileUrl);
 
-  tileUrl = tileStore.getTileCoordUrl(new ol.TileCoord(6, 33, -23));
+  tileUrl = xyzTileStore.getTileCoordUrl(new ol.TileCoord(6, 33, -23));
   assertEquals('6/33/22', tileUrl);
 
-  tileUrl = tileStore.getTileCoordUrl(new ol.TileCoord(6, 33, 41));
+  tileUrl = xyzTileStore.getTileCoordUrl(new ol.TileCoord(6, 33, 41));
   assertUndefined(tileUrl);
 
 }
