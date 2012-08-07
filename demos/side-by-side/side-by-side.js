afe85dd6207f7dc5b81535d882fd3c452e3c3bc4
Tom Payne <twpayne@gmail.com>
2012-08-07 14:55:49 +0200

    Add keyboard controls to side-by-side demo

--- demos/side-by-side/side-by-side.js
+++ demos/side-by-side/side-by-side.js
@@ -1,4 +1,5 @@
 goog.require('ol.RendererHint');
+goog.require('ol.control.Keyboard');
 goog.require('ol.createMap');
 goog.require('ol.layer.MapQuestOpenAerial');
 goog.require('ol.view.Attribution');
@@ -28,6 +29,50 @@ webglMap.bindTo('rotation', domMap);
 var attributionView = new ol.view.Attribution(domMap);
 document.getElementById('attribution').appendChild(
     attributionView.getElement());
+
+var keyboardControl = new ol.control.Keyboard();
+keyboardControl.addCallback('0', function() {
+  layer.setBrightness(0);
+  layer.setContrast(0);
+  layer.setHue(0);
+  layer.setSaturation(0);
+  layer.setOpacity(1);
+  layer.setVisible(true);
+});
+keyboardControl.addCallback('b', function() {
+  layer.setBrightness(layer.getBrightness() - 0.1);
+});
+keyboardControl.addCallback('B', function() {
+  layer.setBrightness(layer.getBrightness() + 0.1);
+});
+keyboardControl.addCallback('c', function() {
+  layer.setContrast(layer.getContrast() - 0.1);
+});
+keyboardControl.addCallback('C', function() {
+  layer.setContrast(layer.getContrast() + 0.1);
+});
+keyboardControl.addCallback('h', function() {
+  layer.setHue(layer.getHue() - 0.1);
+});
+keyboardControl.addCallback('H', function() {
+  layer.setHue(layer.getHue() + 0.1);
+});
+keyboardControl.addCallback('o', function() {
+  layer.setOpacity(layer.getOpacity() - 0.1);
+});
+keyboardControl.addCallback('O', function() {
+  layer.setOpacity(layer.getOpacity() + 0.1);
+});
+keyboardControl.addCallback('s', function() {
+  layer.setSaturation(layer.getSaturation() - 0.1);
+});
+keyboardControl.addCallback('S', function() {
+  layer.setSaturation(layer.getSaturation() + 0.1);
+});
+keyboardControl.addCallback('vV', function() {
+  layer.setVisible(!layer.getVisible());
+});
+domMap.getControls().push(keyboardControl);
 
 goog.exportSymbol('layer', layer);
 goog.exportSymbol('layers', layers);
