07c3cd3d8d32ef40967402562f1befd6e4c0f6cc
Tom Payne <twpayne@gmail.com>
2012-08-13 18:02:05 +0200

    Fix drag pan for rotated maps

--- src/ol/interaction/dragpan.js
+++ src/ol/interaction/dragpan.js
@@ -1,5 +1,3 @@
-// FIXME cope with rotation
-
 goog.provide('ol.interaction.DragPan');
 
 goog.require('ol.Coordinate');
@@ -26,8 +24,12 @@ goog.inherits(ol.interaction.DragPan, ol.interaction.Drag);
 ol.interaction.DragPan.prototype.handleDrag = function(mapBrowserEvent) {
   var map = mapBrowserEvent.map;
   var resolution = map.getResolution();
+  var rotation = map.getRotation();
   var delta =
       new ol.Coordinate(-resolution * this.deltaX, resolution * this.deltaY);
+  if (map.canRotate() && goog.isDef(rotation)) {
+    delta.rotate(rotation);
+  }
   this.pan(map, delta, this.startCenter);
 };
 
