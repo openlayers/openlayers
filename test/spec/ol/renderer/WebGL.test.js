baeddfac129a2c42401c27095ff7c74fd1c93b21
Éric Lemoine <eric.lemoine@gmail.com>
2012-09-24 18:12:51 +0200

    remove the old tests

--- test/spec/ol/renderer/WebGL.test.js
+++
@@ -1,9 +0,0 @@
-describe("ol.renderer.WebGL", function() {
-
-    it("is supported in this environment", function() {
-        // this will not always be true, but for now we expect it to be so
-        expect(ol.renderer.WebGL.isSupported()).toBe(true);
-    });
-    
-});
-
