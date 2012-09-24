1f0d53342af1ac99e932e25aba01fe13b4909f32
Éric Lemoine <eric.lemoine@gmail.com>
2012-09-25 00:29:23 +0200

    Jasmine tests for ol.array

---
+++ test/spec/ol/array.test.js
@@ -0,0 +1,48 @@
+goog.require('ol.array');
+
+describe('ol.array', function() {
+
+  describe('binaryFindNearest', function() {
+    it('returns expected value', function() {
+      var arr = [1000, 500, 100];
+
+      expect(ol.array.binaryFindNearest(arr, 10000)).toEqual(0);
+      expect(ol.array.binaryFindNearest(arr, 1000)).toEqual(0);
+      expect(ol.array.binaryFindNearest(arr, 900)).toEqual(0);
+
+      expect(ol.array.binaryFindNearest(arr, 750)).toEqual(1);
+
+      expect(ol.array.binaryFindNearest(arr, 550)).toEqual(1);
+      expect(ol.array.binaryFindNearest(arr, 500)).toEqual(1);
+      expect(ol.array.binaryFindNearest(arr, 450)).toEqual(1);
+
+      expect(ol.array.binaryFindNearest(arr, 300)).toEqual(2);
+
+      expect(ol.array.binaryFindNearest(arr, 200)).toEqual(2);
+      expect(ol.array.binaryFindNearest(arr, 100)).toEqual(2);
+      expect(ol.array.binaryFindNearest(arr, 50)).toEqual(2);
+    });
+  });
+
+  describe('linearFindNearest', function() {
+    it('returns expected value', function() {
+      var arr = [1000, 500, 100];
+
+      expect(ol.array.linearFindNearest(arr, 10000)).toEqual(0);
+      expect(ol.array.linearFindNearest(arr, 1000)).toEqual(0);
+      expect(ol.array.linearFindNearest(arr, 900)).toEqual(0);
+
+      expect(ol.array.linearFindNearest(arr, 750)).toEqual(1);
+
+      expect(ol.array.linearFindNearest(arr, 550)).toEqual(1);
+      expect(ol.array.linearFindNearest(arr, 500)).toEqual(1);
+      expect(ol.array.linearFindNearest(arr, 450)).toEqual(1);
+
+      expect(ol.array.linearFindNearest(arr, 300)).toEqual(2);
+
+      expect(ol.array.linearFindNearest(arr, 200)).toEqual(2);
+      expect(ol.array.linearFindNearest(arr, 100)).toEqual(2);
+      expect(ol.array.linearFindNearest(arr, 50)).toEqual(2);
+    });
+  });
+});
