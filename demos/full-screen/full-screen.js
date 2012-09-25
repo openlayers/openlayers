1bb2c3c451ab35f4cf839a20c207f1e299327319
Tom Payne <twpayne@gmail.com>
2012-09-25 12:53:09 +0200

    Enable debugging in full screen demo

--- demos/full-screen/full-screen.js
+++ demos/full-screen/full-screen.js
@@ -1,7 +1,15 @@
+goog.require('goog.debug.Console');
+goog.require('goog.debug.Logger');
+goog.require('goog.debug.Logger.Level');
 goog.require('ol.RendererHint');
 goog.require('ol.createMap');
 goog.require('ol.layer.MapQuestOpenAerial');
 
+
+if (goog.DEBUG) {
+  goog.debug.Console.autoInstall();
+  goog.debug.Logger.getLogger('ol').setLevel(goog.debug.Logger.Level.INFO);
+}
 
 var rendererHint = [ol.RendererHint.WEBGL, ol.RendererHint.DOM];
 var map = ol.createMap(document.getElementById('map'), {}, rendererHint);
