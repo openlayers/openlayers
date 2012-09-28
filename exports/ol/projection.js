91d582e76edd15f5504aa6f71c09018aa5b0fb6d
Éric Lemoine <eric.lemoine@gmail.com>
2012-09-28 15:29:12 +0200

    export ol.Projection.transformWithCodes

--- exports/ol/projection.js
+++ exports/ol/projection.js
@@ -9,6 +9,8 @@ goog.exportProperty(ol.Projection,
     'getTransformFromCodes', ol.Projection.getTransformFromCodes);
 goog.exportProperty(ol.Projection,
     'transform', ol.Projection.transform);
+goog.exportProperty(ol.Projection,
+    'transformWithCodes', ol.Projection.transformWithCodes);
 goog.exportProperty(ol.Projection.prototype,
     'getCode', ol.Projection.prototype.getCode);
 goog.exportProperty(ol.Projection.prototype,
