47ef8b37e9d059a6862edcad10f0f567a09e0734
Tim Schaub <tim.schaub@gmail.com>
2012-10-01 22:27:46 -0600

    Using a CSS fitler to deal with hue, sat, etc

--- src/ol/renderer/dom/layer.js
+++ src/ol/renderer/dom/layer.js
@@ -1,5 +1,7 @@
 goog.provide('ol.renderer.dom.Layer');
 
+goog.require('goog.math');
+
 goog.require('ol.Coordinate');
 goog.require('ol.renderer.Layer');
 
@@ -48,6 +50,72 @@ ol.renderer.dom.Layer.prototype.getMapRenderer = function() {
 /**
  * @inheritDoc
  */
+ol.renderer.dom.Layer.prototype.handleLayerBrightnessChange = function() {
+  this.applyHSBCFilter_();
+};
+
+
+/**
+ * @inheritDoc
+ */
+ol.renderer.dom.Layer.prototype.handleLayerContrastChange = function() {
+  this.applyHSBCFilter_();
+};
+
+
+/**
+ * @inheritDoc
+ */
+ol.renderer.dom.Layer.prototype.handleLayerHueChange = function() {
+  this.applyHSBCFilter_();
+};
+
+
+/**
+ * Create a CSS filter to match the current hue, saturation, brightness, and
+ * contrast values.
+ *
+ * @private
+ */
+ol.renderer.dom.Layer.prototype.applyHSBCFilter_ = function() {
+  var layer = this.getLayer();
+
+  /**
+   * Hue currently has no restrictions in range, but we map 1 to 180 and -1 to
+   * -180.  The hue-rotate function has a domain of 0 to 360 degrees.
+   */
+  var hue = Math.round((360 + (180 * layer.getHue())) % 360);
+
+  /**
+   * Saturation has a range of -1 to 1.  We linearly map -1 to 0% and 1 to 200%
+   * saturation change.
+   */
+  var saturation = Math.round(100 * (layer.getSaturation() + 1));
+
+  /**
+   * Brightness has a range of -1 to 1, and we use it directly.
+   */
+  var brightness = layer.getBrightness().toFixed(6);
+
+  /**
+   * Contrast has a range of -1 to 1.  We linearly map -1 to 0 and 1 to 200%
+   * contrast change.
+   */
+  var contrast = Math.round(100 * (layer.getContrast() + 1));
+
+  var filter = 'hue-rotate(' + hue + 'deg) ' +
+      'saturate(' + saturation + '%)' +
+      'brightness(' + brightness + ')' +
+      'contrast(' + contrast + '%)';
+
+  var style = this.target.style;
+  style.WebkitFilter = filter;
+};
+
+
+/**
+ * @inheritDoc
+ */
 ol.renderer.dom.Layer.prototype.handleLayerLoad = function() {
   this.getMap().render();
 };
@@ -58,6 +126,14 @@ ol.renderer.dom.Layer.prototype.handleLayerLoad = function() {
  */
 ol.renderer.dom.Layer.prototype.handleLayerOpacityChange = function() {
   goog.style.setOpacity(this.target, this.getLayer().getOpacity());
+};
+
+
+/**
+ * @inheritDoc
+ */
+ol.renderer.dom.Layer.prototype.handleLayerSaturationChange = function() {
+  this.applyHSBCFilter_();
 };
 
 
