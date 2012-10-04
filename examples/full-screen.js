21f8c8f3bd30902bec7946cc890f5b1a0e4d7b2a
Tom Payne <twpayne@gmail.com>
2012-10-04 11:14:12 +0200

    Enable debugging in full screen example

--- examples/full-screen.js
+++ examples/full-screen.js
@@ -1,7 +1,16 @@
+goog.require('goog.debug.Console');
+goog.require('goog.debug.Logger');
+goog.require('goog.debug.Logger.Level');
 goog.require('ol.Collection');
 goog.require('ol.Coordinate');
 goog.require('ol.Map');
 goog.require('ol.source.MapQuestOpenAerial');
+
+
+if (goog.DEBUG) {
+  goog.debug.Console.autoInstall();
+  goog.debug.Logger.getLogger('ol').setLevel(goog.debug.Logger.Level.INFO);
+}
 
 
 var layer = new ol.layer.TileLayer({
