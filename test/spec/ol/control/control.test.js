2a76a23b6f244af016038ad4c0abe4733fd4655f
Éric Lemoine <eric.lemoine@gmail.com>
2012-10-11 17:46:47 +0200

    Add tests for ol.control.Control

---
+++ test/spec/ol/control/control.test.js
@@ -0,0 +1,27 @@
+goog.require('ol.Map');
+goog.require('ol.control.Control');
+goog.require('goog.dom');
+
+describe('ol.control.Control', function() {
+  var map, control;
+
+  beforeEach(function() {
+    map = new ol.Map({
+      target: document.getElementById('map')
+    });
+    var element = goog.dom.createDom(goog.dom.TagName.DIV);
+    control = new ol.control.Control({element:element});
+    control.setMap(map);
+  });
+
+  afterEach(function() {
+    map.dispose();
+  });
+
+  describe('dispose', function() {
+    it('removes the control element from its parent', function() {
+      control.dispose();
+      expect(goog.dom.getParentElement(control.element)).toBeNull();
+    });
+  });
+});
