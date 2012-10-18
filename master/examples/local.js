1f8c98db8cfa5d21374e7f7b375dcefa86c6c6a3
Tom Payne <twpayne@gmail.com>
2012-10-18 11:50:40 +0200

    Update

---
+++ master/examples/local.js
@@ -0,0 +1,27 @@
+goog.require('goog.debug.Console');
+goog.require('goog.debug.Logger');
+goog.require('goog.debug.Logger.Level');
+goog.require('ol.Collection');
+goog.require('ol.Coordinate');
+goog.require('ol.Map');
+goog.require('ol.source.MapQuestOpenAerial');
+
+
+if (goog.DEBUG) {
+  goog.debug.Console.autoInstall();
+  goog.debug.Logger.getLogger('ol').setLevel(goog.debug.Logger.Level.INFO);
+}
+
+
+var layer = new ol.layer.TileLayer({
+  source: new ol.source.XYZ({
+    maxZoom: 8,
+    url: 'http://localhost:8080/tiles/0/tiles/{z}/{x}/{y}'
+  })
+});
+var map = new ol.Map({
+  center: new ol.Coordinate(0, 0),
+  layers: new ol.Collection([layer]),
+  target: 'map',
+  zoom: 2
+});
