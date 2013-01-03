18fdb6643729dcad6d9b696e144e121f8500a689
Tom Payne <twpayne@gmail.com>
2013-01-03 12:15:30 +0100

    Update jsdoc template to be compatible with latest version of jsdoc3

--- doc/template/publish.js
+++ doc/template/publish.js
@@ -71,7 +71,7 @@ function generate(title, docs, filename) {
     
     html = helper.resolveLinks(html); // turn {@link foo} into <a href="foodoc.html">foo</a>
     
-    fs.writeFileSync(outpath, html);
+    fs.writeFileSync(outpath, html, 'utf8');
 }
 
 /**
@@ -378,7 +378,7 @@ exports.publish = function(taffyData, opts, tutorials) {
         // yes, you can use {@link} in tutorials too!
         html = helper.resolveLinks(html); // turn {@link foo} into <a href="foodoc.html">foo</a>
         
-        fs.writeFileSync(tutorialPath, html);
+        fs.writeFileSync(tutorialPath, html, 'utf8');
     }
     
     // tutorials can have only one parent so there is no risk for loops
