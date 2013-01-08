bbc73a21d865b8e6756ae2e759bec5e0c2742f42
Tim Schaub <tim.schaub@gmail.com>
2013-01-07 22:01:16 -0500

    Update copyright date

--- tools/mergejs.py
+++ tools/mergejs.py
@@ -31,7 +31,7 @@
 #
 # Note: This is a very rough initial version of this code.
 #
-# -- Copyright 2005-2012 OpenLayers contributors / OpenLayers project --
+# -- Copyright 2005-2013 OpenLayers contributors / OpenLayers project --
 #
 
 # TODO: Allow files to be excluded. e.g. `Crossbrowser/DebugMode.js`?
