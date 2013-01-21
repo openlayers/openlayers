c6a8e5ef7202777bdbb2baa8e07f9757906732b0
Éric Lemoine <eric.lemoine@gmail.com>
2013-01-21 12:29:31 +0100

    Remove unused Jasmine matchers

--- test/jasmine-extensions.js
+++ test/jasmine-extensions.js
@@ -4,12 +4,6 @@ beforeEach(function() {
         toBeA: function(type) {
             return this.actual instanceof type;
         },
-        toBeGreaterThanOrEqualTo: function(other) {
-            return this.actual >= other;
-        },
-        toBeLessThanOrEqualTo: function(other) {
-            return this.actual <= other;
-        },
         toRoughlyEqual: function(other, tol) {
             return Math.abs(this.actual - other) <= tol;
         }
