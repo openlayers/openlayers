0f33561af2e15a67a0ea55bdf4b420e2d5f98090
Tim Schaub <tim.schaub@gmail.com>
2013-01-21 18:02:28 -0700

    Skeleton canvas renderer for vector layer

---
+++ src/ol/renderer/canvas/canvasvectorlayerrenderer.js
@@ -0,0 +1,75 @@
+goog.provide('ol.renderer.canvas.VectorLayer');
+
+goog.require('goog.vec.Mat4');
+goog.require('ol.layer.Vector');
+
+
+
+/**
+ * @constructor
+ * @extends {ol.renderer.canvas.Layer}
+ * @param {ol.renderer.Map} mapRenderer Map renderer.
+ * @param {ol.layer.Vector} layer Vector layer.
+ */
+ol.renderer.canvas.VectorLayer = function(mapRenderer, layer) {
+
+  goog.base(this, mapRenderer, layer);
+
+  /**
+   * @private
+   * @type {HTMLCanvasElement}
+   */
+  this.canvas_ = null;
+
+  /**
+   * @private
+   * @type {ol.Size}
+   */
+  this.canvasSize_ = null;
+
+  /**
+   * @private
+   * @type {CanvasRenderingContext2D}
+   */
+  this.context_ = null;
+
+  /**
+   * @private
+   * @type {!goog.vec.Mat4.Number}
+   */
+  this.transform_ = goog.vec.Mat4.createNumber();
+
+};
+goog.inherits(ol.renderer.canvas.VectorLayer, ol.renderer.canvas.Layer);
+
+
+/**
+ * @inheritDoc
+ */
+ol.renderer.canvas.VectorLayer.prototype.getImage = function() {
+  return this.canvas_;
+};
+
+
+/**
+ * @return {ol.layer.Vector} Vector layer.
+ */
+ol.renderer.canvas.VectorLayer.prototype.getVectorLayer = function() {
+  return /** @type {ol.layer.Vector} */ (this.getLayer());
+};
+
+
+/**
+ * @inheritDoc
+ */
+ol.renderer.canvas.VectorLayer.prototype.getTransform = function() {
+  return this.transform_;
+};
+
+
+/**
+ * @inheritDoc
+ */
+ol.renderer.canvas.VectorLayer.prototype.renderFrame =
+    function(frameState, layerState) {
+};
