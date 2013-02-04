06bb26a085cbec9021e4825afa44fbf04616feee
Tim Schaub <tim.schaub@gmail.com>
2013-02-04 16:49:51 -0700

    Using testacular

---
+++ test/testacular.conf.js
@@ -0,0 +1,12 @@
+basePath = '../';
+
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'test/jasmine-extensions.js',
+  'http://localhost:9810/compile?id=test&mode=RAW'
+];
+
+autoWatch = true;
+
+browsers = ['Chrome'];
