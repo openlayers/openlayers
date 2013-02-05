24031753c372ac3eeee333fb19be5345dd37d14f
Tim Schaub <tim.schaub@gmail.com>
2013-02-04 17:16:05 -0700

    Watch test and source files
    Testacular 0.5.2 provides the ability to watch files without serving.  This is required because we are using Plovr to serve and Testacular wouldn't otherwise know to watch.

--- test/testacular.conf.js
+++ test/testacular.conf.js
@@ -4,7 +4,9 @@ files = [
   JASMINE,
   JASMINE_ADAPTER,
   'test/jasmine-extensions.js',
-  'http://localhost:9810/compile?id=test&mode=RAW'
+  'http://localhost:9810/compile?id=test&mode=RAW',
+  {pattern: 'test/**/*.test.js', watched: true, included: false, served: false},
+  {pattern: 'src/**/*.js', watched: true, included: false, served: false}
 ];
 
 autoWatch = true;
