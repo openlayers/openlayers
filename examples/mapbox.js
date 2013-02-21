f1a79135b4e2214a5f106ec6c2516520fd70d6a2
Tim Schaub <tim.schaub@gmail.com>
2013-02-21 11:31:41 -0700

    Chasing tiles

--- examples/mapbox.js
+++ examples/mapbox.js
@@ -1,35 +1,21 @@
-var streets = new OpenLayers.Layer.XYZ(
-    "MapBox Streets",
+var earth = new OpenLayers.Layer.XYZ(
+    "Natural Earth",
     [
-        "http://a.tiles.mapbox.com/v3/mapbox.mapbox-streets/${z}/${x}/${y}.png",
-        "http://b.tiles.mapbox.com/v3/mapbox.mapbox-streets/${z}/${x}/${y}.png",
-        "http://c.tiles.mapbox.com/v3/mapbox.mapbox-streets/${z}/${x}/${y}.png",
-        "http://d.tiles.mapbox.com/v3/mapbox.mapbox-streets/${z}/${x}/${y}.png"
+        "http://a.tiles.mapbox.com/v3/mapbox.natural-earth-hypso-bathy/${z}/${x}/${y}.png",
+        "http://b.tiles.mapbox.com/v3/mapbox.natural-earth-hypso-bathy/${z}/${x}/${y}.png",
+        "http://c.tiles.mapbox.com/v3/mapbox.natural-earth-hypso-bathy/${z}/${x}/${y}.png",
+        "http://d.tiles.mapbox.com/v3/mapbox.natural-earth-hypso-bathy/${z}/${x}/${y}.png"
     ], {
-        attribution: "Tiles &copy; <a href='http://mapbox.com/'>MapBox</a> | " + 
-            "Data &copy; <a href='http://www.openstreetmap.org/'>OpenStreetMap</a> " +
-            "and contributors, CC-BY-SA",
+        attribution: "Tiles &copy; <a href='http://mapbox.com/'>MapBox</a>",
         sphericalMercator: true,
         wrapDateLine: true,
-        transitionEffect: "resize",
-        buffer: 1,
-        numZoomLevels: 17
+        numZoomLevels: 5
     }
 );
 
 var map = new OpenLayers.Map({
     div: "map",
-    layers: [streets],
-    controls: [
-        new OpenLayers.Control.Attribution(),
-        new OpenLayers.Control.Navigation({
-            dragPanOptions: {
-                enableKinetic: true
-            }
-        }),
-        new OpenLayers.Control.Zoom(),
-        new OpenLayers.Control.Permalink({anchor: true})
-    ],
+    layers: [earth],
     center: [0, 0],
     zoom: 1
 });
