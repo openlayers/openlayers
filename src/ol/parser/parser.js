4af32560cec009d23247fdd9afdf53c2b4db5be3
Tim Schaub <tim.schaub@gmail.com>
2013-03-03 15:49:53 +0100

    Removing abstract methods for now
    Later we'll discuss having more specific FeatureParser and readFeatures type methods.

--- src/ol/parser/parser.js
+++ src/ol/parser/parser.js
@@ -6,16 +6,3 @@ goog.provide('ol.parser.Parser');
  * @constructor
  */
 ol.parser.Parser = function() {};
-
-/**
- * @param {*} data Data to deserialize.
- * @return {*} Parsed data.
- */
-ol.parser.Parser.prototype.read = goog.abstractMethod;
-
-
-/**
- * @param {*} obj Object to serialize.
- * @return {*} Serialized object.
- */
-ol.parser.Parser.prototype.write = goog.abstractMethod;
