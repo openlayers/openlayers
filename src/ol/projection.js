1803e7b28dc527badfcb9b15a52991f9ab3b1edb
Bruno Binet <bruno.binet@gmail.com>
2013-03-06 10:27:50 +0100

    Add getMetersPerUnit instance method to ol.Projection

--- src/ol/projection.js
+++ src/ol/projection.js
@@ -33,6 +33,15 @@ ol.ProjectionUnits = {
 };
 
 
+/**
+ * @const {Object.<ol.ProjectionUnits, number>} Meters per unit lookup table.
+ */
+ol.METERS_PER_UNIT = {};
+ol.METERS_PER_UNIT[ol.ProjectionUnits.DEGREES] = 2 * Math.PI * 6378000 / 360;
+ol.METERS_PER_UNIT[ol.ProjectionUnits.FEET] = 0.02540005080010160020;
+ol.METERS_PER_UNIT[ol.ProjectionUnits.METERS] = 1;
+
+
 
 /**
  * @constructor
@@ -106,6 +115,14 @@ ol.Projection.prototype.getPointResolution = goog.abstractMethod;
  */
 ol.Projection.prototype.getUnits = function() {
   return this.units_;
+};
+
+
+/**
+ * @return {number} Meters.
+ */
+ol.Projection.prototype.getMetersPerUnit = function() {
+  return ol.METERS_PER_UNIT[this.units_];
 };
 
 
