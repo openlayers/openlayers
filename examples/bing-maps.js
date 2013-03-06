3e7cd5fffb9e1021553db2859ffcd4aab28eef51
Tom Payne <twpayne@gmail.com>
2013-03-06 20:13:45 +0100

    Add Bing Maps example

---
+++ examples/bing-maps.js
@@ -0,0 +1,26 @@
+goog.require('ol.Coordinate');
+goog.require('ol.Map');
+goog.require('ol.RendererHints');
+goog.require('ol.View2D');
+goog.require('ol.layer.TileLayer');
+goog.require('ol.projection');
+goog.require('ol.source.BingMaps');
+
+
+var map = new ol.Map({
+  layers: [
+    new ol.layer.TileLayer({
+      source: new ol.source.BingMaps({
+        key: 'AgtFlPYDnymLEe9zJ5PCkghbNiFZE9aAtTy3mPaEnEBXqLHtFuTcKoZ-miMC3w7R',
+        style: 'AerialWithLabels'
+      })
+    })
+  ],
+  renderers: ol.RendererHints.createFromQueryData(),
+  target: 'map',
+  view: new ol.View2D({
+    center: ol.projection.transformWithCodes(
+        new ol.Coordinate(-123.1, 49.25), 'EPSG:4326', 'EPSG:3857'),
+    zoom: 8
+  })
+});
