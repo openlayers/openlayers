194edc7f26dae4b3998c64b360e1643093fb9e01
Tom Payne <twpayne@gmail.com>
2013-03-09 02:19:35 +0100

    Sort functions alphabetically

--- src/ol/easing.js
+++ src/ol/easing.js
@@ -4,6 +4,33 @@ goog.require('goog.fx.easing');
 
 
 /**
+ * from https://raw.github.com/DmitryBaranovskiy/raphael/master/raphael.js
+ * @param {number} t Input between 0 and 1.
+ * @return {number} Output between 0 and 1.
+ */
+ol.easing.bounce = function(t) {
+  var s = 7.5625, p = 2.75, l;
+  if (t < (1 / p)) {
+    l = s * t * t;
+  } else {
+    if (t < (2 / p)) {
+      t -= (1.5 / p);
+      l = s * t * t + 0.75;
+    } else {
+      if (t < (2.5 / p)) {
+        t -= (2.25 / p);
+        l = s * t * t + 0.9375;
+      } else {
+        t -= (2.625 / p);
+        l = s * t * t + 0.984375;
+      }
+    }
+  }
+  return l;
+};
+
+
+/**
  * @param {number} t Input between 0 and 1.
  * @return {number} Output between 0 and 1.
  */
@@ -15,6 +42,16 @@ ol.easing.easeIn = goog.fx.easing.easeIn;
  * @return {number} Output between 0 and 1.
  */
 ol.easing.easeOut = goog.fx.easing.easeOut;
+
+
+/**
+ * from https://raw.github.com/DmitryBaranovskiy/raphael/master/raphael.js
+ * @param {number} t Input between 0 and 1.
+ * @return {number} Output between 0 and 1.
+ */
+ol.easing.elastic = function(t) {
+  return Math.pow(2, -10 * t) * Math.sin((t - 0.075) * (2 * Math.PI) / 0.3) + 1;
+};
 
 
 /**
@@ -43,41 +80,4 @@ ol.easing.upAndDown = function(t) {
   } else {
     return 1 - ol.easing.inAndOut(2 * (t - 0.5));
   }
-};
-
-
-/**
- * from https://raw.github.com/DmitryBaranovskiy/raphael/master/raphael.js
- * @param {number} t Input between 0 and 1.
- * @return {number} Output between 0 and 1.
- */
-ol.easing.elastic = function(t) {
-  return Math.pow(2, -10 * t) * Math.sin((t - 0.075) * (2 * Math.PI) / 0.3) + 1;
-};
-
-
-/**
- * from https://raw.github.com/DmitryBaranovskiy/raphael/master/raphael.js
- * @param {number} t Input between 0 and 1.
- * @return {number} Output between 0 and 1.
- */
-ol.easing.bounce = function(t) {
-  var s = 7.5625, p = 2.75, l;
-  if (t < (1 / p)) {
-    l = s * t * t;
-  } else {
-    if (t < (2 / p)) {
-      t -= (1.5 / p);
-      l = s * t * t + 0.75;
-    } else {
-      if (t < (2.5 / p)) {
-        t -= (2.25 / p);
-        l = s * t * t + 0.9375;
-      } else {
-        t -= (2.625 / p);
-        l = s * t * t + 0.984375;
-      }
-    }
-  }
-  return l;
 };
