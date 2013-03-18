ac561b2952e1d018729e57880722cdbedaa9accc
Tim Schaub <tim.schaub@gmail.com>
2013-03-18 17:44:21 -0600

    Use Mocha adapter
    Additional work needed to get tests passing (proxy data for tests that use xhr and get rid of markup dependency in ol.html).

--- test/testacular.conf.js
+++ test/testacular.conf.js
@@ -1,10 +1,17 @@
 basePath = '../';
+baseUrl = '/base/test';
 
 files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'test/jasmine-extensions.js',
+  // 'test/mocha-1.8.1/mocha.js',
+  MOCHA,
+  MOCHA_ADAPTER,
+  'test/expect-0.2.0-ol3/expect.js',
+  'test/sinon-1.6.0/sinon.js',
+  'test/test-extensions.js',
+  'build/proj4js/lib/proj4js-combined.js',
   'http://localhost:9810/compile?id=test&mode=RAW',
+  {pattern: 'test/**/*.json', watched: false, included: false, served: true},
+  {pattern: 'test/**/*.xml', watched: false, included: false, served: true},
   {pattern: 'test/**/*.test.js', watched: true, included: false, served: false},
   {pattern: 'src/**/*.js', watched: true, included: false, served: false}
 ];
