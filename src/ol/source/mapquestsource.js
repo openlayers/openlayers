0caf5efef6ad4600e2ac850f0339f73f8d83892d
Tom Payne <twpayne@gmail.com>
2013-04-05 18:13:00 +0200

    Configure logo for MapQuest sources

--- src/ol/source/mapquestsource.js
+++ src/ol/source/mapquestsource.js
@@ -15,8 +15,7 @@ ol.source.MapQuestOSM = function() {
   var attributions = [
     new ol.Attribution(
         'Tiles Courtesy of ' +
-        '<a href="http://www.mapquest.com/" target="_blank">MapQuest</a> ' +
-        '<img src="http://developer.mapquest.com/content/osm/mq_logo.png">'),
+        '<a href="http://www.mapquest.com/" target="_blank">MapQuest</a>'),
     new ol.Attribution(
         'Data &copy; ' +
         '<a href="http://www.openstreetmap.org">OpenStreetMap</a> ' +
@@ -27,6 +26,7 @@ ol.source.MapQuestOSM = function() {
   goog.base(this, {
     attributions: attributions,
     crossOrigin: 'anonymous',
+    logo: 'http://developer.mapquest.com/content/osm/mq_logo.png',
     opaque: true,
     maxZoom: 28,
     url: 'http://otile{1-4}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg'
@@ -46,8 +46,7 @@ ol.source.MapQuestOpenAerial = function() {
   var attributions = [
     new ol.Attribution(
         'Tiles Courtesy of ' +
-        '<a href="http://www.mapquest.com/" target="_blank">MapQuest</a> ' +
-        '<img src="http://developer.mapquest.com/content/osm/mq_logo.png">'),
+        '<a href="http://www.mapquest.com/" target="_blank">MapQuest</a>'),
     new ol.Attribution(
         'Portions Courtesy NASA/JPL-Caltech and ' +
         'U.S. Depart. of Agriculture, Farm Service Agency')
@@ -56,6 +55,7 @@ ol.source.MapQuestOpenAerial = function() {
   goog.base(this, {
     attributions: attributions,
     crossOrigin: 'anonymous',
+    logo: 'http://developer.mapquest.com/content/osm/mq_logo.png',
     maxZoom: 18,
     opaque: true,
     url: 'http://oatile{1-4}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg'
