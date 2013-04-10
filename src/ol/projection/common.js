4668b51b793ec4b091be5dc41f37eb1eab7d305a
Tom Payne <twpayne@gmail.com>
2013-04-10 17:13:12 +0200

    Add ol.toMap and ol.toUser convenience functions

--- src/ol/projection/common.js
+++ src/ol/projection/common.js
@@ -21,3 +21,41 @@ ol.projection.common.addProjections = function() {
       ol.projection.EPSG3857.fromEPSG4326,
       ol.projection.EPSG3857.toEPSG4326);
 };
+
+
+/**
+ * @param {ol.Coordinate} coordinate Coordinate.
+ * @param {ol.ProjectionLike=} opt_mapProjection Map projection.
+ * @param {ol.ProjectionLike=} opt_userProjection User projection.
+ * @return {ol.Coordinate} Coordinate.
+ */
+ol.toMap = function(coordinate, opt_mapProjection, opt_userProjection) {
+  var mapProjection = goog.isDef(opt_mapProjection) ?
+      opt_mapProjection : ol.projection.get(ol.DEFAULT_MAP_PROJECTION);
+  var userProjection = goog.isDef(opt_userProjection) ?
+      opt_userProjection : ol.projection.get(ol.DEFAULT_USER_PROJECTION);
+  if (userProjection.getAxisOrientation() !=
+      mapProjection.getAxisOrientation()) {
+    coordinate = [coordinate[1], coordinate[0]];
+  }
+  return ol.projection.transform(coordinate, userProjection, mapProjection);
+};
+
+
+/**
+ * @param {ol.Coordinate} coordinate Coordinate.
+ * @param {ol.ProjectionLike=} opt_mapProjection Map projection.
+ * @param {ol.ProjectionLike=} opt_userProjection User projection.
+ * @return {ol.Coordinate} Coordinate.
+ */
+ol.toUser = function(coordinate, opt_mapProjection, opt_userProjection) {
+  var mapProjection = goog.isDef(opt_mapProjection) ?
+      opt_mapProjection : ol.projection.get(ol.DEFAULT_MAP_PROJECTION);
+  var userProjection = goog.isDef(opt_userProjection) ?
+      opt_userProjection : ol.projection.get(ol.DEFAULT_USER_PROJECTION);
+  if (userProjection.getAxisOrientation() !=
+      mapProjection.getAxisOrientation()) {
+    coordinate = [coordinate[1], coordinate[0]];
+  }
+  return ol.projection.transform(coordinate, mapProjection, userProjection);
+};
