bed8c4485928d92baa22e4ba7a1dff434a7f25fd
Tom Payne <twpayne@gmail.com>
2013-04-23 18:32:48 +0200

    Add ol.layer.VectorLayer2

---
+++ src/ol/layer/vectorlayer2.js
@@ -0,0 +1,24 @@
+goog.provide('ol.layer.VectorLayer2');
+
+goog.require('ol.layer.Layer');
+goog.require('ol.source.VectorSource2');
+
+
+
+/**
+ * @constructor
+ * @extends {ol.layer.Layer}
+ * @param {ol.layer.LayerOptions} options Options.
+ */
+ol.layer.VectorLayer2 = function(options) {
+  goog.base(this, options);
+};
+goog.inherits(ol.layer.VectorLayer2, ol.layer.Layer);
+
+
+/**
+ * @return {ol.source.VectorSource2} Source.
+ */
+ol.layer.VectorLayer2.prototype.getVectorSource = function() {
+  return /** @type {ol.source.VectorSource2} */ (this.getSource());
+};
