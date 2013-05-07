2ad5dea737d12b5649477cc29cf28ccd4f5e684e
Tom Payne <twpayne@gmail.com>
2013-05-07 17:31:26 +0200

    Rename ol-mouseposition to ol-mouse-position

--- src/ol/control/mousepositioncontrol.js
+++ src/ol/control/mousepositioncontrol.js
@@ -28,7 +28,7 @@ ol.control.MousePosition = function(opt_options) {
   var options = goog.isDef(opt_options) ? opt_options : {};
 
   var className = goog.isDef(options.className) ?
-      options.className : 'ol-mouseposition';
+      options.className : 'ol-mouse-position';
 
   var element = goog.dom.createDom(goog.dom.TagName.DIV, {
     'class': className
