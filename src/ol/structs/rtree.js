d80ded31eeb083dfc5fe88b151b3dfa0bafdfc59
Tom Payne <twpayne@gmail.com>
2013-05-25 19:46:40 -0500

    Clean up some comments

--- src/ol/structs/rtree.js
+++ src/ol/structs/rtree.js
@@ -1,30 +1,28 @@
-/******************************************************************************
-  rtree.js - General-Purpose Non-Recursive Javascript R-Tree Library
-  Version 0.6.2, December 5st 2009
-
-  Copyright (c) 2009 Jon-Carlos Rivera
-
-  Permission is hereby granted, free of charge, to any person obtaining
-  a copy of this software and associated documentation files (the
-  "Software"), to deal in the Software without restriction, including
-  without limitation the rights to use, copy, modify, merge, publish,
-  distribute, sublicense, and/or sell copies of the Software, and to
-  permit persons to whom the Software is furnished to do so, subject to
-  the following conditions:
-
-  The above copyright notice and this permission notice shall be
-  included in all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-  Jon-Carlos Rivera - imbcmdth@hotmail.com
-******************************************************************************/
+// rtree.js - General-Purpose Non-Recursive Javascript R-Tree Library
+// Version 0.6.2, December 5st 2009
+//
+// Copyright (c) 2009 Jon-Carlos Rivera
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// Jon-Carlos Rivera - imbcmdth@hotmail.com
 
 
 goog.provide('ol.structs.RTree');
@@ -34,17 +32,19 @@ goog.require('ol.extent');
 
 
 /**
- * @typedef {{extent: ol.Extent, leaf: (Object|undefined),
- *     nodes: (Array.<ol.structs.RTreeNode>|undefined),
- *     target: (Object|undefined), type: (string|undefined)}}
+ * @typedef {{extent: ol.Extent,
+ *            leaf: (Object|undefined),
+ *            nodes: (Array.<ol.structs.RTreeNode>|undefined),
+ *            target: (Object|undefined),
+ *            type: (string|undefined)}}
  */
 ol.structs.RTreeNode;
 
 
 
 /**
- * @param {number=} opt_maxWidth Width before a node is split. Default is 6.
  * @constructor
+ * @param {number=} opt_maxWidth Width before a node is split. Default is 6.
  */
 ol.structs.RTree = function(opt_maxWidth) {
 
@@ -70,7 +70,6 @@ ol.structs.RTree = function(opt_maxWidth) {
   this.rootTree_ = /** @type {ol.structs.RTreeNode} */
       ({extent: [0, 0, 0, 0], nodes: []});
 
-  //End of RTree
 };
 
 
