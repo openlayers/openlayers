poohc/ol3

Add ol.DeviceOrientation

build.py
@@ -311,6 +311,7 @@ def examples_star_json(name, match):
                 '../externs/geojson.js',
                 '../externs/proj4js.js',
                 '../externs/tilejson.js',
+                '../externs/w3c_deviceorientation.js',
             ],
         })
         with open(t.name, 'w') as f:

openlayers/openlayers

build.py
@@ -312,6 +312,7 @@ def examples_star_json(name, match):
                 '../externs/oli.js',
                 '../externs/proj4js.js',
                 '../externs/tilejson.js',
+                '../externs/w3c_device_sensor_event.js',
             ],
         })
         with open(t.name, 'w') as f: