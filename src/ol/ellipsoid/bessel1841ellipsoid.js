826556775ce5d9681ca3679eb1a5751575641d6c
Tom Payne <twpayne@gmail.com>
2013-06-12 11:20:20 +0200

    Add ol.ellipsoid.BESSEL1841

---
+++ src/ol/ellipsoid/bessel1841ellipsoid.js
@@ -0,0 +1,10 @@
+goog.provide('ol.ellipsoid.BESSEL1841');
+
+goog.require('ol.Ellipsoid');
+
+
+/**
+ * @const
+ * @type {ol.Ellipsoid}
+ */
+ol.ellipsoid.BESSEL1841 = new ol.Ellipsoid(6377397.155, 1 / 299.15281285);
