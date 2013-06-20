e76f079a56440504c5621260ce3a60e8fd7b0e54
Alexandre Dube <adube@mapgears.com>
2013-06-20 15:58:50 -0400

    Update OverviewMap control, box added

--- src/ol/control/overviewmapcontrol.js
+++ src/ol/control/overviewmapcontrol.js
@@ -5,8 +5,11 @@ goog.provide('ol.control.OverviewMap');
 goog.require('goog.dom');
 goog.require('goog.dom.TagName');
 goog.require('goog.events');
+goog.require('goog.math.Size');
+goog.require('goog.style');
 goog.require('ol.Map');
 goog.require('ol.Object');
+goog.require('ol.Overlay');
 goog.require('ol.View2DProperty');
 goog.require('ol.control.Control');
 goog.require('ol.css');
@@ -45,6 +48,8 @@ ol.control.OverviewMap = function(opt_options) {
   var cssClasses = className + ' ' + ol.css.CLASS_UNSELECTABLE;
   var element = goog.dom.createDom(goog.dom.TagName.DIV, cssClasses);
 
+  var box = goog.dom.createDom(goog.dom.TagName.DIV, 'ol-overviewmap-box');
+
   goog.base(this, {
     element: element,
     map: options.map,
@@ -61,6 +66,17 @@ ol.control.OverviewMap = function(opt_options) {
   });
 
   /**
+   * @type {ol.Overlay}
+   * @private
+   */
+  this.boxOverlay_ = new ol.Overlay({
+    map: this.ovmap_,
+    position: [0, 0],
+    element: box
+  });
+
+
+  /**
    * @type {number}
    * @private
    */
@@ -71,6 +87,7 @@ ol.control.OverviewMap = function(opt_options) {
    * @private
    */
   this.minRatio_ = options.minRatio || ol.control.OVERVIEWMAP_MIN_RATIO;
+
 
   // FIXME - check if layers in options, otherwise we add the map layers later
   // ...
@@ -100,8 +117,13 @@ ol.control.OverviewMap.prototype.setMap = function(map) {
         view, ol.Object.getChangeEventType(ol.View2DProperty.RESOLUTION),
         this.handleResolutionChanged_, false, this);
 
+    goog.events.listen(
+        map, ol.Object.getChangeEventType(ol.MapProperty.SIZE),
+        this.handleSizeChanged_, false, this);
+
     this.ovmap_.updateSize();
     this.resetExtent_();
+    this.updateBox_();
   }
 };
 
@@ -124,18 +146,40 @@ ol.control.OverviewMap.prototype.handleCenterChanged_ = function(event) {
   if (!ol.extent.containsExtent(ovmapViewExtent, mapViewExtent)) {
     this.resetExtent_();
   }
+
+  this.updateBox_();
 };
 
 
 /**
- * Called on main map view resolution change. If the main extent size
- * (width or height is less than the size of the overview
- * extent times minRatio or is greater than the size of the overview
- * extent times maxRatio, reset the overview map.
+ * Called on main map view resolution change.
  * @param {goog.events.Event} event Event.
  * @private
  */
 ol.control.OverviewMap.prototype.handleResolutionChanged_ = function(event) {
+  this.validateExtent_();
+  this.updateBox_();
+};
+
+
+/**
+ * Called on main map size change.
+ * @param {goog.events.Event} event Event.
+ * @private
+ */
+ol.control.OverviewMap.prototype.handleSizeChanged_ = function(event) {
+  this.validateExtent_();
+  this.updateBox_();
+};
+
+
+/**
+ * Reset the overview map extent if the main extent size (width or
+ * height is less than the size of the overview extent times minRatio
+ * or is greater than the size of the overview extent times maxRatio.
+ * @private
+ */
+ol.control.OverviewMap.prototype.validateExtent_ = function() {
   var map = this.getMap();
   var view = map.getView();
   var mapViewExtent = view.calculateExtent(map.getSize());
@@ -166,4 +210,33 @@ ol.control.OverviewMap.prototype.resetExtent_ = function() {
   // FIXME - set ratio as property
   ol.extent.scaleFromCenter(extent, 1.5);
   this.ovmap_.getView().fitExtent(extent, this.ovmap_.getSize());
+};
+
+
+/**
+ * Update the box using the main map extent
+ * @private
+ */
+ol.control.OverviewMap.prototype.updateBox_ = function() {
+  var map = this.getMap();
+  var ovmap = this.ovmap_;
+  var overlay = this.boxOverlay_;
+  var box = this.boxOverlay_.getElement();
+  var extent = map.getView().calculateExtent(map.getSize());
+
+  // set position using bottom left coordinates
+  var bottomLeft = ol.extent.getBottomLeft(extent);
+  overlay.setPosition(bottomLeft);
+
+  // set box size using top left and bottom right pixels
+  if (goog.isDefAndNotNull(box)) {
+    var topLeftPixel =
+        ovmap.getPixelFromCoordinate(ol.extent.getTopLeft(extent));
+    var bottomRightPixel =
+        ovmap.getPixelFromCoordinate(ol.extent.getBottomRight(extent));
+    goog.style.setBorderBoxSize(box, new goog.math.Size(
+        Math.abs(topLeftPixel[0] - bottomRightPixel[0]),
+        Math.abs(topLeftPixel[1] - bottomRightPixel[1])));
+  }
+
 };
