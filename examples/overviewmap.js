c212505bdc27ca6d1d5989fdafb491a3ab0e97ae
Alexandre Dube <adube@mapgears.com>
2013-07-15 08:55:59 -0400

    Add OverviewMap control rotation support
    Rotation of the overview map only, no 'box' rotation support yet.
    This new feature reveals an issue: the extent of the map when rotated
    is not what is currently viewed by the user. The extent remains
    un-rotated, so the box gets wider as we appproach 45 degrees of
    rotation.

--- examples/overviewmap.js
+++ examples/overviewmap.js
@@ -2,6 +2,8 @@ goog.require('ol.Map');
 goog.require('ol.RendererHints');
 goog.require('ol.View2D');
 goog.require('ol.control.OverviewMap');
+goog.require('ol.interaction.DragRotateAndZoom');
+goog.require('ol.interaction.defaults');
 goog.require('ol.layer.TileLayer');
 goog.require('ol.source.MapQuestOpenAerial');
 goog.require('ol.source.OSM');
@@ -11,10 +13,13 @@ goog.require('ol.source.OSM');
  * Helper method for map-creation.
  *
  * @param {string} divId The id of the div for the map.
+ * @param {boolean} rotate Whether to add a DragRotateAndZoom interaction
+ *     or not.
  * @return {ol.Map} The ol.Map instance.
  */
-var createMap = function(divId) {
-  return new ol.Map({
+var createMap = function(divId, rotate) {
+  rotate = rotate || false;
+  var mapOptions = {
     layers: [
       new ol.layer.TileLayer({
         source: new ol.source.OSM()
@@ -26,21 +31,27 @@ var createMap = function(divId) {
       center: [0, 0],
       zoom: 2
     })
-  });
+  };
+  if (rotate) {
+    mapOptions.interactions = ol.interaction.defaults({}, [
+      new ol.interaction.DragRotateAndZoom()
+    ]);
+  }
+  return new ol.Map(mapOptions);
 };
 
-var map1 = createMap('map1');
+var map1 = createMap('map1', false);
 var overview1 = new ol.control.OverviewMap({
   map: map1
 });
 
-var map2 = createMap('map2');
+var map2 = createMap('map2', false);
 var overview2 = new ol.control.OverviewMap({
   map: map2,
   maximized: true
 });
 
-var map3 = createMap('map3');
+var map3 = createMap('map3', true);
 var overview3 = new ol.control.OverviewMap({
   layers: [
     new ol.layer.TileLayer({
