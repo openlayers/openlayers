99a5da79fc8c4f2389c0f2ae297cf02188638607
Frederic Junod <frederic.junod@camptocamp.com>
2013-07-22 17:35:00 +0200

    Custom button text for the ZoomToExtent control

--- examples/custom-controls.js
+++ examples/custom-controls.js
@@ -3,6 +3,7 @@ goog.require('ol.Map');
 goog.require('ol.RendererHints');
 goog.require('ol.View2D');
 goog.require('ol.control.Control');
+goog.require('ol.control.ZoomToExtent');
 goog.require('ol.control.defaults');
 goog.require('ol.layer.TileLayer');
 goog.require('ol.source.OSM');
