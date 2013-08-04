3db93bfbcab926123f1c296a54da7f6ba7238923
Thomas <thomas_gratier@yahoo.fr>
2013-08-04 02:26:22 +0200

    Add Acetate sample layer with code

---
+++ examples/acetate.js
@@ -0,0 +1,29 @@
+goog.require('ol.Map');
+goog.require('ol.RendererHints');
+goog.require('ol.View2D');
+goog.require('ol.layer.TileLayer');
+goog.require('ol.proj');
+goog.require('ol.source.Acetate');
+
+
+var map = new ol.Map({
+  layers: [
+    new ol.layer.TileLayer({
+      source: new ol.source.Acetate({
+        layer: 'terrain'
+      })
+    }),
+    new ol.layer.TileLayer({
+      source: new ol.source.Acetate({
+        layer: 'acetate-labels'
+      })
+    })
+  ],
+  renderer: ol.RendererHint.CANVAS,
+  target: 'map',
+  view: new ol.View2D({
+    center: ol.proj.transform(
+        [-117.19565, 34.056766], 'EPSG:4326', 'EPSG:3857'),
+    zoom: 9
+  })
+});
