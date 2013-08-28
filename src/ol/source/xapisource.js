80567a468ac1b807807f96dd84403f66a19929c2
Jachym Cepicky <jachym.cepicky@gmail.com>
2013-08-28 17:13:30 +0200

    adding 'type' attribute to xapi source, reformating URL properly

--- src/ol/source/xapisource.js
+++ src/ol/source/xapisource.js
@@ -1,10 +1,9 @@
-goog.provide('ol.source.XAPI');
-
 goog.require('goog.net.XhrIo');
-goog.require('ol.proj');
 goog.require('ol.parser.OSM');
-goog.require('ol.source.Vector');
+goog.require('ol.proj');
 goog.require('ol.source.OSM');
+
+goog.provide('ol.source.XAPI');
 
 /**
  * @constructor
@@ -21,10 +20,16 @@ ol.source.XAPI = function(options) {
       options.url : 'http://xapi.openstreetmap.org/api/0.6/';
 
   /**
+   * type on of 'node', 'way', 'relation', or '*'
+   * @type {Object.<string>}
+   */
+  this.type = goog.isDef(options.type) ? options.type : '*';
+
+  /**
    * aditional parameters according to XAPI interface
    * @type {ol.source.XAPIParameters}
    */
-  this.params = goog.isDef(options.params) ?  options.params : {};
+  this.params = goog.isDef(options.params) ? options.params : {};
 
   var attributions;
   if (goog.isDef(options.attributions)) {
@@ -43,7 +48,6 @@ ol.source.XAPI = function(options) {
   });
 
 };
-
 goog.inherits(ol.source.XAPI, ol.source.Vector);
 
 /**
@@ -57,25 +61,44 @@ goog.inherits(ol.source.XAPI, ol.source.Vector);
 ol.source.XAPI.prototype.prepareFeatures = function(layer, extent, projection,
     opt_callback) {
 
+    // transform coordinates, if needed
+    if (projection && projection.getCode() !== 'EPSG:4326') {
+        extent = ol.proj.transform(extent, projection, 'EPSG:4326');
+    }
     // parepare this.url_ first, than run prepareFeatures from parent class
-    this.url_ = this.getUrl(this.url, this.params, extent);
-    return ol.source.Vector.superClass_.prepareFeatures.call(this,
-            layer,extent,projection,opt_callback);
+    this.url_ = this.getUrl(extent, this.params, this.type);
+    return ol.source.XAPI.superClass_.prepareFeatures.call(this,
+            layer, extent, projection, opt_callback);
 };
 
-ol.source.XAPI.prototype.getUrl = function(extent, params) {
+/**
+ * Creates url used for xapi query
+ * @param {ol.Extent} extent Extent that needs to be fetched.
+ * @param {Object.<string, string|number>} params Request parameters.
+ * @param {Object.<string>} type on of 'node', 'way', 'relation', or '*'
+ * @return {string} WMS GetMap request URL.
+ */
+ol.source.XAPI.prototype.getUrl = function(extent, params, type) {
 
     params = params || {};
-    var path = ""; 
+    type = type || this.type;
+    type = type || this.type;
+    var path;
     var baseParams = {};
     var bboxValues = [extent[0], extent[1], extent[2], extent[3]];
     var i;
+    var query_array = [];
+    var query;
 
     baseParams.bbox = bboxValues.join(',');
     goog.object.extend(baseParams, params);
 
     for (i in baseParams) {
-
+        query_array.push(
+                goog.string.buildString('[', i, '=', baseParams[i], ']'));
     }
-    return goog.uri.utils.appendParamsFromMap(this.url, baseParams);
+    query = goog.string.escapeString(
+            goog.string.buildString.apply(this, query_array));
+
+    return goog.string.buildString(this.url, type, query);
 };
