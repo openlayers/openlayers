5a7b982937ecbaee1458bd74cf8463e75d56a360
Jachym Cepicky <jachym.cepicky@gmail.com>
2013-08-28 17:11:56 +0200

    fixing xapi tests, adding 'type' parameter

--- test/spec/ol/source/xapisource.test.js
+++ test/spec/ol/source/xapisource.test.js
@@ -1,9 +1,11 @@
+goog.require('ol.source.Vector');
 goog.provide('ol.test.source.XAPI');
 goog.require('ol.source.XAPI');
 
 describe('ol.source.XAPI', function() {
 
-    var extent = [14.39811708927129,50.07493521431255,14.450817131995263,50.086633811917075];
+    var extent = [14.39811708927129, 50.07493521431255,
+                  14.450817131995263, 50.086633811917075];
 
     describe('ol.source.XAPI', function() {
         it('creates an instance', function() {
@@ -17,44 +19,56 @@ describe('ol.source.XAPI', function() {
         it('prepares xapi url based on given extent', function() {
             var source = new ol.source.XAPI({});
             var url = source.getUrl(extent);
-            expect(url).to.equal('http://xapi.openstreetmap.org/api/0.6/'+escape('[bbox='+extent.join(',')+']'));
+            expect(url).to.equal(
+                'http://xapi.openstreetmap.org/api/0.6/*[bbox=' +
+                extent.join(',') + ']');
         });
 
         it('prepares url based on different url then defeault', function() {
-            var source = new ol.source.XAPI({url:'http://api.openstreetmap.fr/xapi'});
+            var source = new ol.source.XAPI({
+                url: 'http://api.openstreetmap.fr/xapi?',
+                type: 'node'});
             var url = source.getUrl(extent);
-            expect(url).to.equal('http://api.openstreetmap.fr/xapi/'+escape('[bbox='+extent.join(',')+']'));
+            expect(url).to.equal(
+                'http://api.openstreetmap.fr/xapi?node[bbox=' +
+                extent.join(',') + ']');
         });
 
         it('adds different parameters to url', function() {
-            var source = new ol.source.XAPI({url:'http://api.openstreetmap.fr/xapi/node/'});
-            var url = source.getUrl(extent, {
-                man_made:'surveillance'
+            var source = new ol.source.XAPI({
+                url: 'http://api.openstreetmap.fr/xapi?'
             });
-            expect(url).to.equal('http://api.openstreetmap.fr/xapi/node/'+escape('[man_made=surveillance][bbox='+extent.join(',')+']'));
+            var url = source.getUrl(extent, {
+                man_made: 'surveillance'
+            });
+            expect(url).to.equal(
+                'http://api.openstreetmap.fr/xapi?*[bbox=' +
+                extent.join(',') + '][man_made=surveillance]');
         });
     });
 
 
-    //describe('#prepareFeatures', function() {
-    //    it('loads and parses data from url', function(done) {
+    /* can't test remote resvers
+     * works by hand
+    describe('#prepareFeatures', function() {
+        it('loads and parses data from url', function() {
 
-    //        var source = new ol.source.XAPI({
-    //            url: "http://api.openstreetmap.fr/xapi"
-    //        }); 
+            var source = new ol.source.XAPI({
+                url: 'http://api.openstreetmap.fr/xapi/?',
+                type: 'node'
+            });
 
-    //        var layer = new ol.layer.Vector({
-    //            source: source
-    //        });
+            var layer = new ol.layer.Vector({
+                source: source
+            });
 
-    //        source.prepareFeatures(layer,extent,
-    //            function() {
-    //                expect(source.loadState_).to.be(ol.source.VectorLoadState.LOADED);
-    //                expect(goog.object.getCount(
-    //                    layer.featureCache_.getFeaturesObject())).to.be(179);
-    //                done();
-    //            });
-    //        });
-    //    });
+            source.prepareFeatures(layer, extent, undefined,
+                function() {
+                    expect(source.loadState_).to.be(ol.source.VectorLoadState.LOADED);
+                    expect(goog.object.getCount(
+                        layer.featureCache_.getFeaturesObject())).to.be(179);
+                });
+            });
+        });
+    */
 });
-
