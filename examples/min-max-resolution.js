97556f234fccf945142a2e37342f0cd10b7bb210
Bruno Binet <bruno.binet@gmail.com>
2013-09-17 15:45:19 +0200

    Add min-max-resolution example
    Show/hide layers depending on current map resolution.

---
+++ examples/min-max-resolution.js
@@ -0,0 +1,33 @@
+goog.require('ol.Map');
+goog.require('ol.View2D');
+goog.require('ol.layer.Tile');
+goog.require('ol.source.OSM');
+goog.require('ol.source.TileJSON');
+
+
+/**
+ * Create the map.
+ */
+var map = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM(),
+      minResolution: 200,
+      maxResolution: 2000
+    }),
+    new ol.layer.Tile({
+      source: new ol.source.TileJSON({
+        url: 'http://api.tiles.mapbox.com/v3/' +
+            'mapbox.natural-earth-hypso-bathy.jsonp',
+        crossOrigin: 'anonymous'
+      }),
+      minResolution: 2000,
+      maxResolution: 20000
+    })
+  ],
+  target: 'map',
+  view: new ol.View2D({
+    center: [653600, 5723680],
+    zoom: 5
+  })
+});
