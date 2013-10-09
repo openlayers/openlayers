11693464697c49d4c0920bd35683751a16d27751
Éric Lemoine <eric.lemoine@gmail.com>
2013-10-09 11:23:32 +0200

    Use singletap in getfeatureinfo example

--- examples/getfeatureinfo.js
+++ examples/getfeatureinfo.js
@@ -46,7 +46,7 @@ var map = new ol.Map({
   })
 });
 
-map.on('singleclick', function(evt) {
+map.on(['singleclick', 'singletap'], function(evt) {
   map.getFeatureInfo({
     pixel: evt.getPixel(),
     success: function(featureInfoByLayer) {
