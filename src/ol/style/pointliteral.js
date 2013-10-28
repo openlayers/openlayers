b4e0c2bf3035962956f93ba91f3ca61644f627e9
Olivier Terral <olivier.terral@swisstopo.ch>
2013-10-28 14:49:06 +0100

    Remove PointLiteral equals function

--- src/ol/style/pointliteral.js
+++ src/ol/style/pointliteral.js
@@ -12,17 +12,3 @@ ol.style.PointLiteral = function() {
   goog.base(this);
 };
 goog.inherits(ol.style.PointLiteral, ol.style.Literal);
-
-
-/**
- * @inheritDoc
- */
-ol.style.PointLiteral.prototype.equals = function(other) {
-  return other instanceof ol.style.PointLiteral &&
-      this.fillColor == other.fillColor &&
-      this.fillOpacity == other.fillOpacity &&
-      this.strokeColor == other.strokeColor &&
-      this.strokeOpacity == other.strokeOpacity &&
-      this.strokeWidth == other.strokeWidth &&
-      this.zIndex == other.zIndex;
-};
