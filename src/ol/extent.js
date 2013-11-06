cf0fdff4e25b42725f2f9f6e2f87870451f7f53d
Tom Payne <twpayne@gmail.com>
2013-11-06 12:06:32 +0100

    Add ol.extent.extendXY

--- src/ol/extent.js
+++ src/ol/extent.js
@@ -203,6 +203,19 @@ ol.extent.extendCoordinate = function(extent, coordinate) {
 
 /**
  * @param {ol.Extent} extent Extent.
+ * @param {number} x X.
+ * @param {number} y Y.
+ */
+ol.extent.extendXY = function(extent, x, y) {
+  extent[0] = Math.min(extent[0], x);
+  extent[1] = Math.min(extent[1], y);
+  extent[2] = Math.max(extent[2], x);
+  extent[3] = Math.max(extent[3], y);
+};
+
+
+/**
+ * @param {ol.Extent} extent Extent.
  * @return {ol.Coordinate} Bottom left coordinate.
  * @todo stability experimental
  */
