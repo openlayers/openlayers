e403f10de522aa33d8edcaa82cada035286fb3a2
Tom Payne <twpayne@gmail.com>
2013-11-07 15:07:11 +0100

    Add GeoJSON example

---
+++ examples/geojson.js
@@ -0,0 +1,54 @@
+goog.require('ol.Map');
+goog.require('ol.RendererHint');
+goog.require('ol.View2D');
+goog.require('ol.layer.Tile');
+goog.require('ol.layer.Vector');
+goog.require('ol.source.GeoJSON');
+goog.require('ol.source.OSM');
+
+var geoJSONSource = new ol.source.GeoJSON(
+    /** @type {ol.source.GeoJSONOptions} */ ({
+      geoJSON: {
+        'type': 'FeatureCollection',
+        features: [
+          {
+            'type': 'Feature',
+            'geometry': {
+              'type': 'Point',
+              'coordinates': [0, 0]
+            }
+          },
+          {
+            'type': 'Feature',
+            'geometry': {
+              'type': 'LineString',
+              'coordinates': [[-1e7, -1e7], [1e7, 1e7]]
+            }
+          },
+          {
+            'type': 'Feature',
+            'geometry': {
+              'type': 'LineString',
+              'coordinates': [[-1e7, 1e7], [1e7, -1e7]]
+            }
+          }
+        ]
+      }
+    }));
+
+var map = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    }),
+    new ol.layer.Vector({
+      source: geoJSONSource
+    })
+  ],
+  renderer: ol.RendererHint.CANVAS,
+  target: 'map',
+  view: new ol.View2D({
+    center: [0, 0],
+    zoom: 2
+  })
+});
