f0b4a5a35e4ad0fb70da245e07ae7ff617d1a4ab
Tom Payne <twpayne@gmail.com>
2013-11-07 15:06:57 +0100

    Activate canvas vector layer renderer

--- src/ol/renderer/canvas/canvasmaprenderer.js
+++ src/ol/renderer/canvas/canvasmaprenderer.js
@@ -9,9 +9,11 @@ goog.require('goog.style');
 goog.require('ol.css');
 goog.require('ol.layer.Image');
 goog.require('ol.layer.Tile');
+goog.require('ol.layer.Vector');
 goog.require('ol.renderer.Map');
 goog.require('ol.renderer.canvas.ImageLayer');
 goog.require('ol.renderer.canvas.TileLayer');
+goog.require('ol.renderer.canvas.VectorLayer');
 goog.require('ol.source.State');
 
 
@@ -60,6 +62,8 @@ ol.renderer.canvas.Map.prototype.createLayerRenderer = function(layer) {
     return new ol.renderer.canvas.ImageLayer(this, layer);
   } else if (layer instanceof ol.layer.Tile) {
     return new ol.renderer.canvas.TileLayer(this, layer);
+  } else if (layer instanceof ol.layer.Vector) {
+    return new ol.renderer.canvas.VectorLayer(this, layer);
   } else {
     goog.asserts.fail();
     return null;
