51d4d164b1710559bebdaea81b559e61d44a0203
Tom Payne <twpayne@gmail.com>
2013-11-10 13:47:24 +0100

    Add ol.render.transformGeometry

--- src/ol/render/render.js
+++ src/ol/render/render.js
@@ -1,6 +1,7 @@
 goog.provide('ol.render');
 
 goog.require('goog.vec.Mat4');
+goog.require('ol.geom.Geometry');
 
 
 /**
@@ -31,4 +32,18 @@ ol.render.transformFlatCoordinates =
     dest.length = i;
   }
   return dest;
+};
+
+
+/**
+ * @param {ol.geom.Geometry} geometry Geometry.
+ * @param {goog.vec.Mat4.AnyType} transform Transform.
+ * @param {Array.<number>=} opt_dest Destination.
+ * @return {Array.<number>} Transformed flat coordinates.
+ */
+ol.render.transformGeometry = function(geometry, transform, opt_dest) {
+  var flatCoordinates = geometry.getFlatCoordinates();
+  var stride = geometry.getStride();
+  return ol.render.transformFlatCoordinates(
+      flatCoordinates, stride, transform, opt_dest);
 };
