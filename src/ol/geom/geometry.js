72f64bd37a300d160498bf2727330aaa0658256c
Tom Payne <twpayne@gmail.com>
2013-11-11 20:14:53 +0100

    Add ol.geom.reverseFlatCoordinates

--- src/ol/geom/geometry.js
+++ src/ol/geom/geometry.js
@@ -486,3 +486,24 @@ ol.geom.inflateCoordinatesss =
   coordinatesss.length = i;
   return coordinatesss;
 };
+
+
+/**
+ * @param {Array.<number>} flatCoordinates Flat coordinates.
+ * @param {number} offset Offset.
+ * @param {number} end End.
+ * @param {number} stride Stride.
+ */
+ol.geom.reverseFlatCoordinates =
+    function(flatCoordinates, offset, end, stride) {
+  while (offset < end - stride) {
+    var i;
+    for (i = 0; i < stride; ++i) {
+      var tmp = flatCoordinates[offset + i];
+      flatCoordinates[offset + i] = flatCoordinates[end - stride + i];
+      flatCoordinates[end - stride + i] = tmp;
+    }
+    offset += stride;
+    end -= stride;
+  }
+};
