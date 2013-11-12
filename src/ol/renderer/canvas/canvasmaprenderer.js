900bc176ae6f69a74e1e171c5219bc121b1d4a8f
Tom Payne <twpayne@gmail.com>
2013-11-12 01:26:41 +0100

    Avoid clearing canvas twice

--- src/ol/renderer/canvas/canvasmaprenderer.js
+++ src/ol/renderer/canvas/canvasmaprenderer.js
@@ -95,14 +95,15 @@ ol.renderer.canvas.Map.prototype.renderFrame = function(frameState) {
     return;
   }
 
+  var context = this.context_;
+
   var size = frameState.size;
   if (this.canvas_.width != size[0] || this.canvas_.height != size[1]) {
     this.canvas_.width = size[0];
     this.canvas_.height = size[1];
+  } else {
+    context.clearRect(0, 0, this.canvas_.width, this.canvas_.height);
   }
-
-  var context = this.context_;
-  context.clearRect(0, 0, this.canvas_.width, this.canvas_.height);
 
   this.calculateMatrices2D(frameState);
 
