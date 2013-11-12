77b7abb96abce9cb640e7b479be56d8fe5dc6204
Frederic Junod <frederic.junod@camptocamp.com>
2013-11-12 15:42:23 +0100

    Don't rely on goog.net.XhrIo in vector-layer example

--- examples/vector-layer.js
+++ examples/vector-layer.js
@@ -1,8 +1,6 @@
 // NOCOMPILE
 // FIXME don't rely on goog.* functions
-goog.require('goog.asserts');
 goog.require('goog.functions');
-goog.require('goog.net.XhrIo');
 goog.require('ol.Map');
 goog.require('ol.RendererHint');
 goog.require('ol.View2D');
@@ -29,31 +27,26 @@ var map = new ol.Map({
 
 var vectorSource = new ol.source.Vector();
 
-goog.net.XhrIo.send('data/countries.geojson', function(event) {
-  var xhrIo = /** @type {goog.net.XhrIo} */ (event.target);
-  if (xhrIo.isSuccess()) {
-    var format = new ol.format.GeoJSON();
-    var object = xhrIo.getResponseJson();
-    goog.asserts.assert(goog.isDefAndNotNull(object));
-    var transformFn = ol.proj.getTransform('EPSG:4326', 'EPSG:3857');
-    format.readObject(object, function(feature) {
-      var geometry = feature.getGeometry();
-      geometry.transform(transformFn);
-      feature.setGeometry(geometry);
-      vectorSource.addFeature(feature);
-    });
-    map.getLayers().push(new ol.layer.Vector({
-      source: vectorSource,
-      styleFunction: goog.functions.constant({
-        fill: {
-          color: 'rgba(255, 255, 255, 0.6)'
-        },
-        stroke: {
-          color: '#319FD3'
-        }
-      })
-    }));
-  }
+$.get('data/countries.geojson', function(data) {
+  var format = new ol.format.GeoJSON();
+  var transformFn = ol.proj.getTransform('EPSG:4326', 'EPSG:3857');
+  format.readString(data, function(feature) {
+    var geometry = feature.getGeometry();
+    geometry.transform(transformFn);
+    feature.setGeometry(geometry);
+    vectorSource.addFeature(feature);
+  });
+  map.getLayers().push(new ol.layer.Vector({
+    source: vectorSource,
+    styleFunction: goog.functions.constant({
+      fill: {
+        color: 'rgba(255, 255, 255, 0.6)'
+      },
+      stroke: {
+        color: '#319FD3'
+      }
+    })
+  }));
 });
 
 var displayFeatureInfo = function(pixel) {
