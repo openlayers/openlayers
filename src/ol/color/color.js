02604745caf239215e8825865d19c5c9ce643a0d
Tom Payne <twpayne@gmail.com>
2013-11-13 21:40:54 +0100

    Add ol.color.equals

--- src/ol/color/color.js
+++ src/ol/color/color.js
@@ -49,6 +49,18 @@ ol.color.rgbaColorRe_ =
 
 
 /**
+ * @param {ol.Color} color1 Color1.
+ * @param {ol.Color} color2 Color2.
+ * @return {boolean} Equals.
+ */
+ol.color.equals = function(color1, color2) {
+  return color1 === color2 || (
+      color1[0] == color2[0] && color1[1] == color2[1] &&
+      color1[2] == color2[2] && color1[3] == color2[3]);
+};
+
+
+/**
  * @param {string} s String.
  * @param {ol.Color=} opt_color Color.
  * @return {ol.Color} Color.
