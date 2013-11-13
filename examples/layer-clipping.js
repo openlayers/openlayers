aabd8541c24a5d2ac01c0a536cf8074edf225909
Tom Payne <twpayne@gmail.com>
2013-11-13 13:50:28 +0100

    Fix goog.require in layer clipping example

--- examples/layer-clipping.js
+++ examples/layer-clipping.js
@@ -1,5 +1,5 @@
 goog.require('ol.Map');
-goog.require('ol.RendererHints');
+goog.require('ol.RendererHint');
 goog.require('ol.View2D');
 goog.require('ol.layer.Tile');
 goog.require('ol.source.OSM');
