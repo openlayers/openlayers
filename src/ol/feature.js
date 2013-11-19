8d03fa1197d9465db74f31e4e5fcda181e2c5330
Bart van den Eijnden <bartvde@boundlessgeo.com>
2013-11-19 15:39:22 +0100

    add an optional argument to getAttributes so we can get a feature's attributes except for the geometry ones

--- src/ol/feature.js
+++ src/ol/feature.js
@@ -68,17 +68,23 @@ goog.inherits(ol.Feature, ol.Object);
 
 /**
  * Gets a copy of the attributes of this feature.
+ * @param {boolean=} opt_nonGeometry Don't include any geometry attributes
+ *     (by default geometry attributes are returned).
  * @return {Object.<string, *>} Attributes object.
  * @todo stability experimental
  */
-ol.Feature.prototype.getAttributes = function() {
+ol.Feature.prototype.getAttributes = function(opt_nonGeometry) {
   var keys = this.getKeys(),
       len = keys.length,
       attributes = {},
       i, key;
   for (i = 0; i < len; ++ i) {
     key = keys[i];
-    attributes[key] = this.get(key);
+    var value = this.get(key);
+    if (!goog.isDef(opt_nonGeometry) || opt_nonGeometry == false ||
+        (opt_nonGeometry === true && !(value instanceof ol.geom.Geometry))) {
+      attributes[key] = value;
+    }
   }
   return attributes;
 };
