d3ad1a750625db3d2dfb090bcc278bff7d346bf9
Éric Lemoine <eric.lemoine@gmail.com>
2013-11-21 14:41:07 +0100

    Add canvas style default values

---
+++ src/ol/render/canvas/canvas.js
@@ -0,0 +1,19 @@
+goog.provide('ol.render.canvas');
+
+
+/**
+ * @const {string}
+ */
+ol.render.canvas.defaultFillStyle = 'black';
+
+
+/**
+ * @const {string}
+ */
+ol.render.canvas.defaultStrokeStyle = 'black';
+
+
+/**
+ * @const {number}
+ */
+ol.render.canvas.defaultLineWidth = 1;
