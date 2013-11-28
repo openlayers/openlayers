06d873666f006b8e4c0f50cd7f03d237703726fb
Frederic Junod <frederic.junod@camptocamp.com>
2013-11-28 10:02:30 +0100

    Remove unused ol.style.Fill.equals

--- src/ol/style/fillstyle.js
+++ src/ol/style/fillstyle.js
@@ -15,26 +15,3 @@ ol.style.Fill = function(options) {
    */
   this.color = goog.isDef(options.color) ? options.color : null;
 };
-
-
-/**
- * @param {ol.style.Fill} fillStyle1 Fill style 1.
- * @param {ol.style.Fill} fillStyle2 Fill style 2.
- * @return {boolean} Equals.
- */
-ol.style.Fill.equals = function(fillStyle1, fillStyle2) {
-  if (!goog.isNull(fillStyle1)) {
-    if (!goog.isNull(fillStyle2)) {
-      return fillStyle1 === fillStyle2 ||
-          ol.color.stringOrColorEquals(fillStyle1.color, fillStyle2.color);
-    } else {
-      return false;
-    }
-  } else {
-    if (!goog.isNull(fillStyle2)) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-};
