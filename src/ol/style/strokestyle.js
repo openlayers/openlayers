b8dbac77bc87958c131eb198eb91a98569d3fa20
Frederic Junod <frederic.junod@camptocamp.com>
2013-11-28 10:03:07 +0100

    Remove unused ol.style.Stroke.equals

--- src/ol/style/strokestyle.js
+++ src/ol/style/strokestyle.js
@@ -25,28 +25,3 @@ ol.style.Stroke = function(options) {
    */
   this.width = options.width;
 };
-
-
-/**
- * @param {ol.style.Stroke} strokeStyle1 Stroke style 1.
- * @param {ol.style.Stroke} strokeStyle2 Stroke style 2.
- * @return {boolean} Equals.
- */
-ol.style.Stroke.equals = function(strokeStyle1, strokeStyle2) {
-  if (!goog.isNull(strokeStyle1)) {
-    if (!goog.isNull(strokeStyle2)) {
-      return strokeStyle1 === strokeStyle2 ||
-          (ol.color.stringOrColorEquals(strokeStyle1.color,
-                                        strokeStyle2.color) &&
-           strokeStyle1.width == strokeStyle2.width);
-    } else {
-      return false;
-    }
-  } else {
-    if (!goog.isNull(strokeStyle2)) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-};
