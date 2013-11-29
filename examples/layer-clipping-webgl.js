e2d9c77e08557d3da9769222cddf2f20bdfa737e
Frederic Junod <frederic.junod@camptocamp.com>
2013-11-29 10:43:02 +0100

    Notify if WebGL is not supported

--- examples/layer-clipping-webgl.js
+++ examples/layer-clipping-webgl.js
@@ -4,90 +4,99 @@ goog.require('ol.View2D');
 goog.require('ol.layer.Tile');
 goog.require('ol.source.OSM');
 
-var osm = new ol.layer.Tile({
-  source: new ol.source.OSM()
-});
+if (!ol.webgl.SUPPORTED) {
+  var info = document.getElementById('no-webgl');
+  /**
+   * display error message
+   */
+  info.style.display = '';
+} else {
 
-var map = new ol.Map({
-  layers: [osm],
-  renderer: ol.RendererHint.WEBGL,
-  target: 'map',
-  view: new ol.View2D({
-    center: [0, 0],
-    zoom: 2
-  })
-});
+  var osm = new ol.layer.Tile({
+    source: new ol.source.OSM()
+  });
 
-var fragmentShaderSource = [
-  'precision mediump float;',
-  'void main() {',
-  '}'
-].join('');
+  var map = new ol.Map({
+    layers: [osm],
+    renderer: ol.RendererHint.WEBGL,
+    target: 'map',
+    view: new ol.View2D({
+      center: [0, 0],
+      zoom: 2
+    })
+  });
 
-var vertexShaderSource = [
-  'attribute vec2 a_position;',
-  'void main() {',
-  'gl_Position = vec4(a_position, 0, 1);',
-  '}'
-].join('');
+  var fragmentShaderSource = [
+    'precision mediump float;',
+    'void main() {',
+    '}'
+  ].join('');
 
-osm.on('precompose', function(event) {
-  var context = event.getGlContext();
+  var vertexShaderSource = [
+    'attribute vec2 a_position;',
+    'void main() {',
+    '  gl_Position = vec4(a_position, 0, 1);',
+    '}'
+  ].join('');
 
-  var gl = context.getGL();
-  var program = gl.createProgram();
+  osm.on('precompose', function(event) {
+    var context = event.getGlContext();
 
-  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertexShader, vertexShaderSource);
-  gl.compileShader(vertexShader);
-  gl.attachShader(program, vertexShader);
+    var gl = context.getGL();
+    var program = gl.createProgram();
 
-  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragmentShader, fragmentShaderSource);
-  gl.compileShader(fragmentShader);
-  gl.attachShader(program, fragmentShader);
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderSource);
+    gl.compileShader(vertexShader);
+    gl.attachShader(program, vertexShader);
 
-  gl.linkProgram(program);
-  context.useProgram(program);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderSource);
+    gl.compileShader(fragmentShader);
+    gl.attachShader(program, fragmentShader);
 
-  var positionLocation = gl.getAttribLocation(program, 'a_position');
+    gl.linkProgram(program);
+    context.useProgram(program);
 
-  gl.enable(gl.STENCIL_TEST);
-  gl.colorMask(false, false, false, false);
-  gl.stencilOp(gl.KEEP, gl.KEEP, gl.REPLACE);
-  gl.stencilFunc(gl.ALWAYS, 1, 0xff);
+    var positionLocation = gl.getAttribLocation(program, 'a_position');
 
-  var buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    // first band
-    -1.0, -1.0, -0.75, -1.0, -1.0, 1.0,
-    -1.0, 1.0, -0.75, -1.0, -0.75, 1.0,
-    // second band
-    -0.5, -1.0, -0.25, -1.0, -0.5, 1.0,
-    -0.5, 1.0, -0.25, -1.0, -0.25, 1.0,
-    // third band
-    0.0, -1.0, 0.25, -1.0, 0.0, 1.0,
-    0.0, 1.0, 0.25, -1.0, 0.25, 1.0,
-    // forth band
-    0.5, -1.0, 0.75, -1.0, 0.5, 1.0,
-    0.5, 1.0, 0.75, -1.0, 0.75, 1.0
-  ]), gl.STATIC_DRAW);
+    gl.enable(gl.STENCIL_TEST);
+    gl.colorMask(false, false, false, false);
+    gl.stencilOp(gl.KEEP, gl.KEEP, gl.REPLACE);
+    gl.stencilFunc(gl.ALWAYS, 1, 0xff);
 
-  gl.enableVertexAttribArray(positionLocation);
-  gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-  gl.drawArrays(gl.TRIANGLES, 0, 24);
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+      // first band
+      -1.0, -1.0, -0.75, -1.0, -1.0, 1.0,
+      -1.0, 1.0, -0.75, -1.0, -0.75, 1.0,
+      // second band
+      -0.5, -1.0, -0.25, -1.0, -0.5, 1.0,
+      -0.5, 1.0, -0.25, -1.0, -0.25, 1.0,
+      // third band
+      0.0, -1.0, 0.25, -1.0, 0.0, 1.0,
+      0.0, 1.0, 0.25, -1.0, 0.25, 1.0,
+      // forth band
+      0.5, -1.0, 0.75, -1.0, 0.5, 1.0,
+      0.5, 1.0, 0.75, -1.0, 0.75, 1.0
+    ]), gl.STATIC_DRAW);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  gl.deleteBuffer(buffer);
+    gl.enableVertexAttribArray(positionLocation);
+    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, 24);
 
-  gl.colorMask(true, true, true, true);
-  gl.stencilFunc(gl.NOTEQUAL, 0, 0xff);
-  gl.stencilOp(gl.KEEP, gl.KEEP, gl.KEEP);
-});
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.deleteBuffer(buffer);
 
-osm.on('postcompose', function(event) {
-  var context = event.getGlContext();
-  var gl = context.getGL();
-  gl.disable(gl.STENCIL_TEST);
-});
+    gl.colorMask(true, true, true, true);
+    gl.stencilFunc(gl.NOTEQUAL, 0, 0xff);
+    gl.stencilOp(gl.KEEP, gl.KEEP, gl.KEEP);
+  });
+
+  osm.on('postcompose', function(event) {
+    var context = event.getGlContext();
+    var gl = context.getGL();
+    gl.disable(gl.STENCIL_TEST);
+  });
+}
