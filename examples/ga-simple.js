a8238b6d5e76925eb43d826d366d475bfae61c75
Cédric Moullet <cedric.moullet@gmail.com>
2013-11-29 06:50:09 +0100

    Support layer group

--- examples/ga-simple.js
+++ examples/ga-simple.js
@@ -12,7 +12,7 @@ var map = new ga.Map({
     // 10 means that one pixel is 10m width and height
     // List of resolution of the WMTS layers:
     // 650, 500, 250, 100, 50, 20, 10, 5, 2.5, 2, 1, 0.5, 0.25, 0.1
-    resolution: 5,
+    resolution: 50,
     // Define a coordinate CH1903 for the center of the view
     center: [561666.5, 185569.5]
   })
@@ -23,7 +23,9 @@ var map = new ga.Map({
 //var lyr = ga.layer.create('ch.bfs.gebaeude_wohnungs_register_wmts');
 //var lyr1 = ga.layer.create('ch.swisstopo.swissimage');
 //var lyr = ga.layer.create('ch.bfs.gebaeude_wohnungs_register_wms');
-var lyr = ga.layer.create('ch.kantone.cadastralwebmap-farbe');
+//var lyr = ga.layer.create('ch.kantone.cadastralwebmap-farbe');
+//var lyr = ga.layer.create('ch.swisstopo-vd.geometa-gemeinde');
+var lyr = ga.layer.create('ch.swisstopo.fixpunkte-hfp1');
 
 //map.addLayer(lyr1);
 map.addLayer(lyr);
