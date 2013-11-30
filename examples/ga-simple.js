9124ea46a8f4ff2c0859a0d7f19abbe283282cbe
Cédric Moullet <cedric.moullet@gmail.com>
2013-11-30 03:19:22 +0100

    Fix style according to CDBund

--- examples/ga-simple.js
+++ examples/ga-simple.js
@@ -26,9 +26,9 @@ var map = new ga.Map({
 //var lyr = ga.layer.create('ch.kantone.cadastralwebmap-farbe');
 //var lyr = ga.layer.create('ch.swisstopo-vd.geometa-gemeinde');
 //var lyr = ga.layer.create('ch.swisstopo.fixpunkte-hfp1');
-//var lyr = ga.layer.create('ch.swisstopo.fixpunkte-agnes');
-var lyr = ga.layer.create('ch.swisstopo.pixelkarte-farbe');
+var lyr = ga.layer.create('ch.swisstopo.fixpunkte-agnes');
+var lyr1 = ga.layer.create('ch.swisstopo.pixelkarte-farbe');
 
-//map.addLayer(lyr1);
+map.addLayer(lyr1);
 map.addLayer(lyr);
 
