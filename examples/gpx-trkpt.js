029c7ea7f3b598aa833a9bf3abc2712919878223
Bart van den Eijnden <bartvde@boundlessgeo.com>
2013-12-02 09:48:59 +0100

    add missing requires

--- examples/gpx-trkpt.js
+++ examples/gpx-trkpt.js
@@ -4,8 +4,11 @@ goog.require('ol.View2D');
 goog.require('ol.layer.Tile');
 goog.require('ol.layer.Vector');
 goog.require('ol.parser.GPX');
+goog.require('ol.proj');
 goog.require('ol.source.OSM');
 goog.require('ol.source.Vector');
+goog.require('ol.style.Stroke');
+goog.require('ol.style.Style');
 
 var raster = new ol.layer.Tile({
   source: new ol.source.OSM()
