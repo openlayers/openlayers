b3473c3cba8797854e84d2e2cd3b2a9688d34387
Tom Payne <twpayne@gmail.com>
2013-12-02 09:07:55 +0100

    Add ol.geom.flat.flipXY

--- test/spec/ol/geom/flatgeom.test.js
+++ test/spec/ol/geom/flatgeom.test.js
@@ -44,6 +44,37 @@ describe('ol.geom.flat', function() {
 
   });
 
+  describe('ol.geom.flat.flipXY', function() {
+
+    it('can flip XY coordinates', function() {
+      var flatCoordinates = ol.geom.flat.flipXY([1, 2, 3, 4], 0, 4, 2);
+      expect(flatCoordinates).to.eql([2, 1, 4, 3]);
+    });
+
+    it('can flip XY coordinates while preserving other dimensions', function() {
+      var flatCoordinates = ol.geom.flat.flipXY(
+          [1, 2, 3, 4, 5, 6, 7, 8], 0, 8, 4);
+      expect(flatCoordinates).to.eql([2, 1, 3, 4, 6, 5, 7, 8]);
+    });
+
+    it('can flip XY coordinates in place', function() {
+      var flatCoordinates = [1, 2, 3, 4];
+      expect(ol.geom.flat.flipXY(flatCoordinates, 0, 4, 2, flatCoordinates)).
+          to.be(flatCoordinates);
+      expect(flatCoordinates).to.eql([2, 1, 4, 3]);
+    });
+
+    it('can flip XY coordinates in place while preserving other dimensions',
+        function() {
+          var flatCoordinates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+          expect(ol.geom.flat.flipXY(
+              flatCoordinates, 0, 9, 3, flatCoordinates)).
+              to.be(flatCoordinates);
+          expect(flatCoordinates).to.eql([2, 1, 3, 5, 4, 6, 8, 7, 9]);
+        });
+
+  });
+
   describe('ol.geom.flat.inflateCoordinatess', function() {
 
     it('inflates arrays of coordinates', function() {
