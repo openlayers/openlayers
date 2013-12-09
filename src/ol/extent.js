66f1826358de97e1a8a1af03fb661eecc2d7a86a
Tom Payne <twpayne@gmail.com>
2013-12-09 11:30:28 +0000

    Add ol.extent.closestSquaredDistanceXY

--- src/ol/extent.js
+++ src/ol/extent.js
@@ -85,6 +85,32 @@ ol.extent.clone = function(extent, opt_extent) {
 
 
 /**
+ * @param {ol.Extent} extent Extent.
+ * @param {number} x X.
+ * @param {number} y Y.
+ * @return {number} Closest squared distance.
+ */
+ol.extent.closestSquaredDistanceXY = function(extent, x, y) {
+  var dx, dy;
+  if (x < extent[0]) {
+    dx = extent[0] - x;
+  } else if (extent[2] < x) {
+    dx = x - extent[2];
+  } else {
+    dx = 0;
+  }
+  if (y < extent[1]) {
+    dy = extent[1] - y;
+  } else if (extent[3] < y) {
+    dy = y - extent[3];
+  } else {
+    dy = 0;
+  }
+  return dx * dx + dy * dy;
+};
+
+
+/**
  * Checks if the passed coordinate is contained or on the edge of the extent.
  *
  * @param {ol.Extent} extent Extent.
