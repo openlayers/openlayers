828c35d2390491dd4b381a822ab94445d0bfb00c
Tom Payne <twpayne@gmail.com>
2013-12-18 20:12:33 +0100

    Use more interesting data in KML example

--- examples/kml.js
+++ examples/kml.js
@@ -3,25 +3,20 @@ goog.require('ol.RendererHint');
 goog.require('ol.View2D');
 goog.require('ol.layer.Tile');
 goog.require('ol.layer.Vector');
+goog.require('ol.source.BingMaps');
 goog.require('ol.source.KML');
-goog.require('ol.source.TileWMS');
 
 var raster = new ol.layer.Tile({
-  source: new ol.source.TileWMS({
-    url: 'http://vmap0.tiles.osgeo.org/wms/vmap0',
-    crossOrigin: null,
-    params: {
-      'LAYERS': 'basic',
-      'VERSION': '1.1.1',
-      'FORMAT': 'image/jpeg'
-    }
+  source: new ol.source.BingMaps({
+    imagerySet: 'Aerial',
+    key: 'Ak-dzM4wZjSqTlzveKz5u0d4IQ4bRzVI309GxmkgSVr1ewS6iPSrOvOKhA-CJlm3'
   })
 });
 
 var vector = new ol.layer.Vector({
   source: new ol.source.KML({
-    reprojectTo: 'EPSG:4326',
-    url: 'data/kml/lines.kml'
+    reprojectTo: 'EPSG:3857',
+    url: 'data/kml/2012-02-10.kml'
   })
 });
 
@@ -30,9 +25,8 @@ var map = new ol.Map({
   renderer: ol.RendererHint.CANVAS,
   target: 'map',
   view: new ol.View2D({
-    projection: 'EPSG:4326',
-    center: [-112.169, 36.099],
-    zoom: 11
+    center: [876970.8463461736, 5859807.853963373],
+    zoom: 10
   })
 });
 
