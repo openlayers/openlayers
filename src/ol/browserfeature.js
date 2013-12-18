73f24cf7b0da7a35e566316b65fadd69f0581143
Éric Lemoine <eric.lemoine@gmail.com>
2013-12-18 08:16:13 +0100

    goog.require creates a namespace object already

--- src/ol/browserfeature.js
+++ src/ol/browserfeature.js
@@ -8,22 +8,22 @@ ol.ASSUME_TOUCH = false;
 
 
 /**
- * @type {Object}
- */
-ol.BrowserFeature = {
-  /**
-   * The ratio between physical pixels and device-independent pixels
-   * (dips) on the device (`window.devicePixelRatio`).
-   * @type {number}
-   */
-  DEVICE_PIXEL_RATIO: goog.global.devicePixelRatio || 1,
+* The ratio between physical pixels and device-independent pixels
+* (dips) on the device (`window.devicePixelRatio`).
+* @const
+* @type {number}
+* @todo stability experimental
+*/
+ol.BrowserFeature.DEVICE_PIXEL_RATIO = goog.global.devicePixelRatio || 1;
 
-  /**
-   * True if browser supports touch events.
-   * @type {boolean}
-   */
-  HAS_TOUCH: ol.ASSUME_TOUCH ||
-      (goog.global.document &&
-      'ontouchstart' in goog.global.document.documentElement) ||
-      !!(goog.global.navigator.msPointerEnabled)
-};
+
+/**
+* True if browser supports touch events.
+* @const
+* @type {boolean}
+ * @todo stability experimental
+*/
+ol.BrowserFeature.HAS_TOUCH = ol.ASSUME_TOUCH ||
+    (goog.global.document &&
+    'ontouchstart' in goog.global.document.documentElement) ||
+    !!(goog.global.navigator.msPointerEnabled);
