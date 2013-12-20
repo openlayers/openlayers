0bf34a52ff05cf545c2e32888798d9139c580708
Tom Payne <twpayne@gmail.com>
2013-12-20 16:33:26 +0100

    Add ol.geom.MultiLineString#setLineStrings

--- test/spec/ol/geom/multilinestring.test.js
+++ test/spec/ol/geom/multilinestring.test.js
@@ -168,8 +168,25 @@ describe('ol.geom.MultiLineString', function() {
 
   });
 
+  describe('#setLineStrings', function() {
+
+    it('sets the line strings', function() {
+      var multiLineString = new ol.geom.MultiLineString(null);
+      var lineString1 = new ol.geom.LineString([[1, 2], [3, 4]]);
+      var lineString2 = new ol.geom.LineString([[5, 6], [7, 8]]);
+      multiLineString.setLineStrings([lineString1, lineString2]);
+      expect(multiLineString.getFlatCoordinates()).to.eql(
+          [1, 2, 3, 4, 5, 6, 7, 8]);
+      expect(multiLineString.getEnds()).to.eql([4, 8]);
+      var coordinates = multiLineString.getCoordinates();
+      expect(coordinates[0]).to.eql(lineString1.getCoordinates());
+      expect(coordinates[1]).to.eql(lineString2.getCoordinates());
+    });
+  });
+
 });
 
 
 goog.require('ol.extent');
+goog.require('ol.geom.LineString');
 goog.require('ol.geom.MultiLineString');
