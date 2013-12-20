72ea0a10fce8e9fe4226b85ef2f18967e4c9b6d8
Éric Lemoine <eric.lemoine@gmail.com>
2013-12-20 15:45:07 +0100

    Use ol.interaction.defaults in dragzoom example

--- examples/dragzoom.js
+++ examples/dragzoom.js
@@ -1,12 +1,16 @@
 goog.require('ol.Map');
 goog.require('ol.RendererHint');
 goog.require('ol.View2D');
+goog.require('ol.interaction');
 goog.require('ol.interaction.DragZoom');
 goog.require('ol.layer.Tile');
 goog.require('ol.source.OSM');
 
 
 var map = new ol.Map({
+  interactions: ol.interaction.defaults().extend([
+    new ol.interaction.DragZoom()
+  ]),
   layers: [
     new ol.layer.Tile({
       source: new ol.source.OSM()
@@ -19,6 +23,3 @@ var map = new ol.Map({
     zoom: 2
   })
 });
-
-var dragzoom = new ol.interaction.DragZoom();
-map.addInteraction(dragzoom);
