19b9b2dce2969e57f2467ba188e9090a91511d83
cedricmoullet <cedric.moullet@gmail.com>
2013-12-21 11:21:32 +0100

    Missing space

--- src/ga/translation.js
+++ src/ga/translation.js
@@ -2,18 +2,18 @@ goog.provide('ga.Translate');
 
 ga.Translate = {
 	'en' : {
-		'geocoding_results': 'Geocoding results'
+		'geocoding_results': 'Several locations with this name:'
 	},
 	'de' : {
-		'geocoding_results': 'Geocoding Ergebnisse'
+		'geocoding_results': 'Mehrere Orte mit diese Name:'
 	},
 	'fr' : {
 		'geocoding_results': 'Plusieurs lieux avec ce noms:'
 	},
 	'it' : {
-		'geocoding_results': 'Geocoding results'
+		'geocoding_results': 'Several locations with this name:'
 	},
 	'rm' : {
-		'geocoding_results': 'Geocoding Ergebnisse'
+		'geocoding_results': 'Mehrere Orte mit diese Name:'
 	}
 }
