22d7b6d7039bad8ca592722590b0c54df811b5cb
cedricmoullet <cedric.moullet@gmail.com>
2013-12-21 11:19:33 +0100

    Support translation (very minimal, but make tje job)

---
+++ src/ga/translation.js
@@ -0,0 +1,19 @@
+goog.provide('ga.Translate');
+
+ga.Translate = {
+	'en' : {
+		'geocoding_results': 'Geocoding results'
+	},
+	'de' : {
+		'geocoding_results': 'Geocoding Ergebnisse'
+	},
+	'fr' : {
+		'geocoding_results': 'Plusieurs lieux avec ce noms:'
+	},
+	'it' : {
+		'geocoding_results': 'Geocoding results'
+	},
+	'rm' : {
+		'geocoding_results': 'Geocoding Ergebnisse'
+	}
+}
