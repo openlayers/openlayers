541fb177732b1cd1b5d893062dc57d09e050ee55
Tom Payne <twpayne@gmail.com>
2014-01-02 15:26:36 +0100

    Use prettier image in zoomify example

--- examples/zoomify.js
+++ examples/zoomify.js
@@ -8,13 +8,16 @@ goog.require('ol.proj.Projection');
 goog.require('ol.proj.Units');
 goog.require('ol.source.Zoomify');
 
-//var imgWidth = 8001;
-//var imgHeight = 6943;
-//var url = 'http://mapy.mzk.cz/AA22/0103/'
-var imgWidth = 9911;
-var imgHeight = 6100;
-var url = 'http://vips.vtech.fr/cgi-bin/iipsrv.fcgi?zoomify=' +
-        '/mnt/MD1/AD00/plan_CHU-4HD-01/FOND.TIF/';
+var imgWidth = 8001;
+var imgHeight = 6943;
+var url = 'http://mapy.mzk.cz/AA22/0103/';
+var crossOrigin = undefined;
+
+//var imgWidth = 9911;
+//var imgHeight = 6100;
+//var url = 'http://vips.vtech.fr/cgi-bin/iipsrv.fcgi?zoomify=' +
+//        '/mnt/MD1/AD00/plan_CHU-4HD-01/FOND.TIF/';
+//var crossOrigin = 'anonymous';
 
 var imgCenter = [imgWidth / 2, - imgHeight / 2];
 var proj = new ol.proj.Projection({
@@ -26,7 +29,7 @@ var proj = new ol.proj.Projection({
 var source = new ol.source.Zoomify({
   url: url,
   size: [imgWidth, imgHeight],
-  crossOrigin: 'anonymous'
+  crossOrigin: crossOrigin
 });
 
 var map = new ol.Map({
