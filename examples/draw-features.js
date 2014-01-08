e3810b5027db6be3f3f919fbbd91b3bd818c847c
oterral <oterral@gmail.com>
2014-01-08 15:46:08 +0100

    Add draw features example

---
+++ examples/draw-features.js
@@ -0,0 +1,74 @@
+goog.require('ol.Map');
+goog.require('ol.RendererHint');
+goog.require('ol.View2D');
+goog.require('ol.interaction');
+goog.require('ol.interaction.Draw');
+goog.require('ol.layer.Tile');
+goog.require('ol.layer.Vector');
+goog.require('ol.source.MapQuestOpenAerial');
+goog.require('ol.source.Vector');
+goog.require('ol.style.Circle');
+goog.require('ol.style.Fill');
+goog.require('ol.style.Stroke');
+goog.require('ol.style.Style');
+
+var raster = new ol.layer.Tile({
+  source: new ol.source.MapQuestOpenAerial()
+});
+
+var styleArray = [new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(255, 255, 255, 0.2)'
+  }),
+  stroke: new ol.style.Stroke({
+    color: '#ffcc33',
+    width: 2
+  }),
+  image: new ol.style.Circle({
+    radius: 7,
+    fill: new ol.style.Fill({
+      color: '#ffcc33'
+    })
+  })
+})];
+
+var vector = new ol.layer.Vector({
+  source: new ol.source.Vector(),
+  styleFunction: function(feature, resolution) {
+    return styleArray;
+  }
+});
+
+var map = new ol.Map({
+  layers: [raster, vector],
+  renderer: ol.RendererHint.CANVAS,
+  target: 'map',
+  view: new ol.View2D({
+    center: [-11000000, 4600000],
+    zoom: 4
+  })
+});
+
+var typeSelect = document.getElementById('type');
+
+var draw; // global so we can remove it later
+function addInteraction() {
+  draw = new ol.interaction.Draw({
+    layer: vector,
+    type: /** @type {ol.geom.GeometryType} */
+        (typeSelect.options[typeSelect.selectedIndex].value)
+  });
+  map.addInteraction(draw);
+}
+
+
+/**
+ * Let user change the geometry type.
+ * @param {Event} e Change event.
+ */
+typeSelect.onchange = function(e) {
+  map.removeInteraction(draw);
+  addInteraction();
+};
+
+addInteraction();
