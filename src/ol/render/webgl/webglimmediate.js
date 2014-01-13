54e1fd3a41e86a6a8d902d77e643a337e63eefe5
Tom Payne <twpayne@gmail.com>
2014-01-13 18:11:06 +0100

    Use @inheritDoc in ol.render.webgl.Immediate

--- src/ol/render/webgl/webglimmediate.js
+++ src/ol/render/webgl/webglimmediate.js
@@ -21,17 +21,14 @@ ol.render.webgl.Immediate.prototype.drawAsync = function(zIndex, callback) {
 
 
 /**
- * @param {ol.Feature} feature Feature.
- * @param {ol.style.Style} style Style.
+ * @inheritDoc
  */
 ol.render.webgl.Immediate.prototype.drawFeature = function(feature, style) {
 };
 
 
 /**
- * @param {ol.geom.GeometryCollection} geometryCollectionGeometry Geometry
- *     collection.
- * @param {Object} data Opaque data object.
+ * @inheritDoc
  */
 ol.render.webgl.Immediate.prototype.drawGeometryCollectionGeometry =
     function(geometryCollectionGeometry, data) {
@@ -39,8 +36,7 @@ ol.render.webgl.Immediate.prototype.drawGeometryCollectionGeometry =
 
 
 /**
- * @param {ol.geom.Point} pointGeometry Point geometry.
- * @param {Object} data Opaque data object.
+ * @inheritDoc
  */
 ol.render.webgl.Immediate.prototype.drawPointGeometry =
     function(pointGeometry, data) {
@@ -48,8 +44,7 @@ ol.render.webgl.Immediate.prototype.drawPointGeometry =
 
 
 /**
- * @param {ol.geom.LineString} lineStringGeometry Line string geometry.
- * @param {Object} data Opaque data object.
+ * @inheritDoc
  */
 ol.render.webgl.Immediate.prototype.drawLineStringGeometry =
     function(lineStringGeometry, data) {
@@ -57,9 +52,7 @@ ol.render.webgl.Immediate.prototype.drawLineStringGeometry =
 
 
 /**
- * @param {ol.geom.MultiLineString} multiLineStringGeometry
- *     MultiLineString geometry.
- * @param {Object} data Opaque data object.
+ * @inheritDoc
  */
 ol.render.webgl.Immediate.prototype.drawMultiLineStringGeometry =
     function(multiLineStringGeometry, data) {
@@ -67,8 +60,7 @@ ol.render.webgl.Immediate.prototype.drawMultiLineStringGeometry =
 
 
 /**
- * @param {ol.geom.MultiPoint} multiPointGeometry MultiPoint geometry.
- * @param {Object} data Opaque data object.
+ * @inheritDoc
  */
 ol.render.webgl.Immediate.prototype.drawMultiPointGeometry =
     function(multiPointGeometry, data) {
@@ -76,8 +68,7 @@ ol.render.webgl.Immediate.prototype.drawMultiPointGeometry =
 
 
 /**
- * @param {ol.geom.MultiPolygon} multiPolygonGeometry MultiPolygon geometry.
- * @param {Object} data Opaque data object.
+ * @inheritDoc
  */
 ol.render.webgl.Immediate.prototype.drawMultiPolygonGeometry =
     function(multiPolygonGeometry, data) {
@@ -85,8 +76,7 @@ ol.render.webgl.Immediate.prototype.drawMultiPolygonGeometry =
 
 
 /**
- * @param {ol.geom.Polygon} polygonGeometry Polygon geometry.
- * @param {Object} data Opaque data object.
+ * @inheritDoc
  */
 ol.render.webgl.Immediate.prototype.drawPolygonGeometry =
     function(polygonGeometry, data) {
@@ -94,8 +84,7 @@ ol.render.webgl.Immediate.prototype.drawPolygonGeometry =
 
 
 /**
- * @param {ol.style.Fill} fillStyle Fill style.
- * @param {ol.style.Stroke} strokeStyle Stroke style.
+ * @inheritDoc
  */
 ol.render.webgl.Immediate.prototype.setFillStrokeStyle =
     function(fillStyle, strokeStyle) {
@@ -103,14 +92,14 @@ ol.render.webgl.Immediate.prototype.setFillStrokeStyle =
 
 
 /**
- * @param {ol.style.Image} imageStyle Image style.
+ * @inheritDoc
  */
 ol.render.webgl.Immediate.prototype.setImageStyle = function(imageStyle) {
 };
 
 
 /**
- * @param {ol.style.Text} textStyle Text style.
+ * @inheritDoc
  */
 ol.render.webgl.Immediate.prototype.setTextStyle = function(textStyle) {
 };
