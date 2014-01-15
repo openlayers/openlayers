4fd320f34aba8a22f2dd2b68d79395b7272f08d5
Frederic Junod <frederic.junod@camptocamp.com>
2014-01-15 09:09:47 +0100

    snapshot

--- examples/geolocation.js
+++ examples/geolocation.js
@@ -2,15 +2,19 @@
 // FIXME this circle will need to compensate for the pointResolution of the
 // FIXME EPSG:3857 projection
 
+goog.require('ol.Feature');
 goog.require('ol.Geolocation');
 goog.require('ol.Map');
-goog.require('ol.Overlay');
-goog.require('ol.OverlayPositioning');
-goog.require('ol.RendererHints');
+goog.require('ol.RendererHint');
 goog.require('ol.View2D');
 goog.require('ol.dom.Input');
 goog.require('ol.layer.Tile');
+goog.require('ol.render.FeaturesOverlay');
 goog.require('ol.source.OSM');
+goog.require('ol.style.Circle');
+goog.require('ol.style.Fill');
+goog.require('ol.style.Stroke');
+goog.require('ol.style.Style');
 
 
 var view = new ol.View2D({
@@ -24,7 +28,7 @@ var map = new ol.Map({
       source: new ol.source.OSM()
     })
   ],
-  renderers: ol.RendererHints.createFromQueryData(),
+  renderer: ol.RendererHint.CANVAS,
   target: 'map',
   view: view
 });
@@ -43,22 +47,41 @@ geolocation.on('propertychange', function() {
   $('#speed').text(geolocation.getSpeed() + ' [m/s]');
 });
 
-var marker = new ol.Overlay({
-  element: /** @type {Element} */ ($('<i/>').addClass('icon-flag').get(0)),
-  positioning: ol.OverlayPositioning.BOTTOM_LEFT,
-  stopEvent: false
-});
-map.addOverlay(marker);
-// bind the marker position to the device location.
-marker.bindTo('position', geolocation);
-
-geolocation.on('change:accuracy', function() {
-  $(marker.getElement()).tooltip({
-    title: this.getAccuracy() + 'm from this point'
-  });
-});
 geolocation.on('error', function(error) {
   var info = document.getElementById('info');
   info.innerHTML = error.message;
   info.style.display = '';
+});
+
+
+var styleArray = [
+  new ol.style.Style({
+    image: new ol.style.Circle({
+      radius: 5,
+      stroke: null,
+      fill: new ol.style.Fill({
+        color: '#319FD3'
+      })
+    }),
+    fill: new ol.style.Fill({
+      color: 'rgba(255, 255, 255, 0.6)'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#319FD3',
+      width: 1
+    })
+  })
+];
+
+var accuracyFeature = new ol.Feature();
+accuracyFeature.bindTo('geometry', geolocation, 'accuracyGeometry');
+var positionFeature = new ol.Feature();
+positionFeature.bindTo('geometry', geolocation, 'positionGeometry');
+
+var featuresOverlay = new ol.render.FeaturesOverlay({
+  map: map,
+  features: [accuracyFeature, positionFeature],
+  styleFunction: function(feature, resolution) {
+    return styleArray;
+  }
 });
