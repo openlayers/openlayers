a1f8000a0e3b7cf87ba4e27b6e27b5269ff2fea8
cedricmoullet <cedric.moullet@gmail.com>
2014-01-22 21:18:21 +0100

    Fix examples

--- examples/ga-side-by-side.js
+++ examples/ga-side-by-side.js
@@ -2,7 +2,6 @@ goog.require('ol.Map');
 goog.require('ol.RendererHint');
 goog.require('ol.View2D');
 goog.require('ol.layer.Tile');
-goog.require('ol.source.MapQuestOpenAerial');
 goog.require('ol.webgl');
 
 goog.require('ga.Map');
