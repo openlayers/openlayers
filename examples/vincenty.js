7ffe71b5015fd91342729f2dd05ba0386a00af34
schedul-xor <schedul.xor@gmail.com>
2014-01-26 18:46:46 +0900

    Fixed example

--- examples/vincenty.js
+++ examples/vincenty.js
@@ -4,80 +4,76 @@ goog.require('ol.View2D');
 goog.require('ol.ellipsoid.WGS84');
 goog.require('ol.layer.Tile');
 goog.require('ol.layer.Vector');
-goog.require('ol.parser.GeoJSON');
 goog.require('ol.proj');
 goog.require('ol.source.OSM');
 goog.require('ol.source.Vector');
+goog.require('ol.style.Circle');
 goog.require('ol.style.Fill');
-goog.require('ol.style.Rule');
-goog.require('ol.style.Shape');
+goog.require('ol.style.Stroke');
 goog.require('ol.style.Style');
-goog.require('ol.style.Text');
 
 
-
-var style = new ol.style.Style({
-  rules: [new ol.style.Rule({
-    filter: 'geometryType("point")',
-    symbolizers: [
-      new ol.style.Shape({
-        size: 40,
-        fill: new ol.style.Fill({'color': '#ffffff'})
-      }),
-      new ol.style.Text({
-        color: '#333333',
-        text: ol.expr.parse('name'),
-        fontFamily: 'Calibri,sans-serif',
-        fontSize: 10
-      })
-    ]
-  })]
-});
 var cityFeatures = [{
-  'type': 'Feature',
-  'properties': {'name': 'London'},
-  'geometry': {
-    'type': 'Point',
-    'coordinates': [-0.1275, 51.507222]
-  }
+  'name': 'London',
+  'coordinates': [-0.1275, 51.507222]
 },{
-  'type': 'Feature',
-  'properties': {'name': 'Beijing'},
-  'geometry': {
-    'type': 'Point',
-    'coordinates': [116.391667, 39.913889]
-  }
+  'name': 'Beijing',
+  'coordinates': [116.391667, 39.913889]
 }];
-var v = ol.ellipsoid.WGS84.vincenty(cityFeatures[0].
-    geometry.coordinates, cityFeatures[1].geometry.coordinates);
+
+var styles = [new ol.style.Style({
+  image: new ol.style.Circle({
+    fill: new ol.style.Fill({
+      color: '#ffffff'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#000',
+      width: 1
+    }),
+    radius: 10
+  })
+})];
+
+var tr4326to3857 = ol.proj.getTransform('EPSG:4326', 'EPSG:3857');
+
+var features = [];
+goog.array.forEach(cityFeatures, function(cityFeature, index) {
+  var feature = new ol.Feature({
+    geometry: new ol.geom.Point(tr4326to3857(cityFeature.coordinates)),
+    name: cityFeature.name
+  });
+  features.push(feature);
+},this);
+
+var v = ol.ellipsoid.WGS84.vincenty(
+    cityFeatures[0].coordinates,
+    cityFeatures[1].coordinates);
 var distance = v.distance;
 var initialBearing = v.initialBearing;
 var divisionCount = 20;
 for (var i = 1; i < divisionCount; i++) {
-  var pd = ol.ellipsoid.WGS84.vincentyDirect(cityFeatures[0].
-      geometry.coordinates, distance / divisionCount * i, initialBearing);
-  cityFeatures.push({
-    'type': 'Feature',
-    'properties': {'name': i + ''},
-    'geometry': {
-      'type': 'Point',
-      'coordinates': pd
-    }
+  var pd = ol.ellipsoid.WGS84.vincentyDirect(
+      cityFeatures[0].coordinates,
+      distance / divisionCount * i,
+      initialBearing);
+  var feature = new ol.Feature({
+    geometry: new ol.geom.Point(tr4326to3857(pd)),
+    name: i + ''
   });
+  features.push(feature);
 }
 
-var citySource = new ol.source.Vector({
-  parser: new ol.parser.GeoJSON(),
-  projection: ol.proj.get('EPSG:4326'),
-  data: {'type': 'FeatureCollection',
-    'features': cityFeatures
-  }
-});
-var cityLayer = new ol.layer.Vector({
-  style: style,
-  source: citySource
+var vectorSource = new ol.source.Vector({
+  features: features,
+  projection: ol.proj.get('EPSG:3857')
 });
 
+var cityLayer = new ol.layer.Vector({
+  source: vectorSource,
+  styleFunction: function(feature, resolution) {
+    return styles;
+  }
+});
 
 
 var map = new ol.Map({
@@ -88,7 +84,7 @@ var map = new ol.Map({
     cityLayer
   ],
   renderer: ol.RendererHint.CANVAS,
-  target: 'map',
+  target: document.getElementById('map'),
   view: new ol.View2D({
     center: [6000000, 6000000],
     zoom: 2
