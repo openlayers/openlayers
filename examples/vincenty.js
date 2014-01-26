31f4bd848727ecb45d7b3e442313226e9c263b4e
schedul-xor <schedul.xor@gmail.com>
2014-01-26 17:31:10 +0900

    Removed ol.expr

--- examples/vincenty.js
+++ examples/vincenty.js
@@ -2,7 +2,6 @@ goog.require('ol.Map');
 goog.require('ol.RendererHint');
 goog.require('ol.View2D');
 goog.require('ol.ellipsoid.WGS84');
-goog.require('ol.expr');
 goog.require('ol.layer.Tile');
 goog.require('ol.layer.Vector');
 goog.require('ol.parser.GeoJSON');
