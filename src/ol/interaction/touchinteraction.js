63bcf684f73c843d46987e8da63170af07bef611
Éric Lemoine <eric.lemoine@gmail.com>
2014-01-30 09:13:03 +0100

    Fix the touch interactions
    This fixes a bug introduced by https://github.com/openlayers/ol3/commit/318fc6815901380bdc7c9d1921cb5a9cc1e6e2ff.  Thanks @gjn for reporting the problem.

--- src/ol/interaction/touchinteraction.js
+++ src/ol/interaction/touchinteraction.js
@@ -81,7 +81,7 @@ ol.interaction.Touch.isTouchEvent_ = function(mapBrowserEvent) {
 ol.interaction.Touch.prototype.updateTrackedTouches_ =
     function(mapBrowserEvent) {
   if (ol.interaction.Touch.isTouchEvent_(mapBrowserEvent)) {
-    var event = mapBrowserEvent.browserEvent;
+    var event = mapBrowserEvent.originalEvent;
     if (goog.isDef(event.targetTouches)) {
       // W3C touch events
       this.targetTouches = event.targetTouches;
