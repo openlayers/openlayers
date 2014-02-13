a178712f6b25c70e8f1370984ad3eb8be6fd94a8
Tom Payne <tom.payne@camptocamp.com>
2014-02-13 14:47:47 +0100

    Only replay replay group if it is not empty

--- src/ol/renderer/canvas/canvasvectorlayerrenderer.js
+++ src/ol/renderer/canvas/canvasvectorlayerrenderer.js
@@ -69,7 +69,7 @@ ol.renderer.canvas.VectorLayer.prototype.composeFrame =
   this.dispatchPreComposeEvent(context, frameState, transform);
 
   var replayGroup = this.replayGroup_;
-  if (!goog.isNull(replayGroup)) {
+  if (!goog.isNull(replayGroup) && !replayGroup.isEmpty()) {
     var renderGeometryFunction = this.getRenderGeometryFunction_();
     goog.asserts.assert(goog.isFunction(renderGeometryFunction));
     context.globalAlpha = layerState.opacity;
