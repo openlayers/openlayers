3227ce4a0697a68e9ec388c1b1e6a7c8f11d3079
ahocevar <andreas.hocevar@gmail.com>
2014-03-25 18:24:57 +0100

    Document ol.ObjectEvent events

--- src/ol/object.js
+++ src/ol/object.js
@@ -99,6 +99,7 @@ ol.ObjectAccessor.prototype.transform = function(from, to) {
  * @constructor
  * @extends {ol.Observable}
  * @param {Object.<string, *>=} opt_values Values.
+ * @fires ol.ObjectEvent
  * @todo stability experimental
  */
 ol.Object = function(opt_values) {
