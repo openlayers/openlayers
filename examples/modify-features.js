1e0d2af0f0aac7f87420cbd40e725669af6b87de
Tim Schaub <tim.schaub@gmail.com>
2014-03-27 11:01:12 -0600

    Add a style for selected features

--- examples/modify-features.js
+++ examples/modify-features.js
@@ -8,6 +8,9 @@ goog.require('ol.layer.Tile');
 goog.require('ol.layer.Vector');
 goog.require('ol.source.GeoJSON');
 goog.require('ol.source.MapQuest');
+goog.require('ol.style.Fill');
+goog.require('ol.style.Stroke');
+goog.require('ol.style.Style');
 
 
 var raster = new ol.layer.Tile({
@@ -26,7 +29,17 @@ var vector = new ol.layer.Vector({
   source: source
 });
 
-var select = new ol.interaction.Select({});
+var select = new ol.interaction.Select({
+  style: new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: '#3399CC',
+      width: 2.5
+    }),
+    fill: new ol.style.Fill({
+      color: 'rgba(255,255,255,0.6)'
+    })
+  })
+});
 
 var modify = new ol.interaction.Modify({
   features: select.getFeatures()
