02a7913c0f8ca400c563d4dd6e874d2dc0ad4884
Tim Schaub <tim.schaub@gmail.com>
2014-03-27 11:07:06 -0600

    Add condition for selection

--- examples/modify-features.js
+++ examples/modify-features.js
@@ -1,6 +1,7 @@
 goog.require('ol.Collection');
 goog.require('ol.Map');
 goog.require('ol.View2D');
+goog.require('ol.events.condition');
 goog.require('ol.interaction');
 goog.require('ol.interaction.Modify');
 goog.require('ol.interaction.Select');
@@ -30,6 +31,7 @@ var vector = new ol.layer.Vector({
 });
 
 var select = new ol.interaction.Select({
+  addCondition: ol.events.condition.shiftKeyOnly,
   style: new ol.style.Style({
     stroke: new ol.style.Stroke({
       color: '#3399CC',
