44c97450661f6c71c4db48d408b36a7b3b970d42
Marc Monnerat <procrastinatio@gmail.com>
2014-04-28 16:28:17 +0200

    simpliest webserver with proxy using only standard modules

--- proxy.py
+++ proxy.py
@@ -1,41 +1,94 @@
 #!/usr/bin/env python
 
+""" Minimal web server with proxy
 
-import os.path
+    Everything but '/proxy?url=' is considered a static file
+"""
+
+import os
+import mimetypes
 import urllib2
-from httplib2 import Http
-from bottle import request, response,route, run, abort
-from bottle import static_file, HTTPError
 
-@route('/proxy')
-def proxy():
-    url = urllib2.unquote(request.GET.get('url'))
-    http = Http(disable_ssl_certificate_validation=True)
-    h = dict(request.headers)
-    h.pop("Host", h)
-    try:
-        resp, content = http.request(url, headers=h)
-    except:
-        raise HTTPError(502, "Bad Gateway")
-    
-    if resp.status != 200:
-        abort(resp.status)
+from urlparse import parse_qs
+from wsgiref.simple_server import make_server
+
+def static(environ, start_response, path):
+
+        filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), path[1:])
+
+        if os.path.exists(filepath):
+            mime = mimetypes.guess_type(filepath)
+            response_body = open(filepath).read()
+            status = '200 OK'
+            headers = [('Content-type', mime[0]),
+                       ('Content-Length', str(len(response_body)))]
+
+        else:
+            headers = []
+            status = '404 Not found'
+            response_body = 'File not found'
+
+        start_response(status, headers)
+
+        return [response_body]
+
    
-    response.content_type = resp['content-type']
-    response.set_header('Content-Length', resp['content-length'])
+
+def proxy(environ, start_response):
+    qs =  environ['QUERY_STRING']
+    params = parse_qs(qs)
+
+
+    if 'url' in params:
+        print params['url']
+        url = urllib2.unquote(params['url'][0])
+
+        headers = {'Referer' : environ.get('HTTP_REFERER','')}
+                
+        request = urllib2.Request(url, None, headers)
+
+        response = urllib2.urlopen(request)
+
+        if response.code == 200:
+            headers = response.headers
+            print headers,dir(headers), headers.keys()
+            content =  response.read()
+
+            headers = [('Content-Type',headers.get('content-type')), 
+                       ('Content-Length', headers.get('content-length'))]
+
+            start_response("200 OK", headers)
+            return content
+
+
+
+def app(environ, start_response):
+    path    = environ['PATH_INFO']
+    method  = environ['REQUEST_METHOD']
     
-    return content
+    if method == 'POST':
+            status = '405 Method Not Allowed'
+            headers = [('Content-type', 'text/plain')]
+            start_response(status, headers)
+            return ['POST is not supported']
+    else:
+
+      if path.startswith('/proxy'):
+          return proxy(environ, start_response)
+      else:
+          return static(environ, start_response, path)
+
+      
+      start_response("200 OK", [])
+      return ["Some content"]
 
 
-@route('/<filepath:path>')
-def server_static(filepath):
-    print filepath
-    root = os.path.join(os.path.dirname(os.path.realpath(__file__)), '')
 
-    return static_file(filepath, root= root)
+if __name__ == '__main__':
 
+    PORT = 9000
+    httpd = make_server('', PORT, app)
+    print "Serving on port %d..." % PORT
 
-
-run(host='localhost', port=9001, reloader=True, debug=True)
-
-
+    # Serve until process is killed
+    httpd.serve_forever()
