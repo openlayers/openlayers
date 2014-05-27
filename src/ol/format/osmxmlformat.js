4c9463cdccf772dc35dcb4fcc912037ab8e4c0e0
Frederic Junod <frederic.junod@camptocamp.com>
2014-05-27 14:13:55 +0200

    Export ol.format.OSMXML#readFeatures

--- src/ol/format/osmxmlformat.js
+++ src/ol/format/osmxmlformat.js
@@ -181,6 +181,17 @@ ol.format.OSMXML.NODE_PARSERS_ = ol.xml.makeParsersNS(
 
 
 /**
+ * Read all features from an OSM source.
+ *
+ * @function
+ * @param {ArrayBuffer|Document|Node|Object|string} source Source.
+ * @return {Array.<ol.Feature>} Features.
+ * @todo api
+ */
+ol.format.OSMXML.prototype.readFeatures;
+
+
+/**
  * @inheritDoc
  */
 ol.format.OSMXML.prototype.readFeaturesFromNode = function(node) {
