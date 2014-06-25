7fb3e5842d7f9ba15655a18adcbf7dd8d6aba601
Éric Lemoine <eric.lemoine@gmail.com>
2014-06-25 14:19:10 +0200

    Add an exampleNS.strToBase64 function
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Base64_encoding_and_decoding

--- externs/example.js
+++ externs/example.js
@@ -7,3 +7,10 @@ var exampleNS;
 
 /** @interface */
 exampleNS.getRendererFromQueryString = function() {};
+
+
+/**
+ * @param {string} str String.
+ * @return {string} Base64 string.
+ */
+exampleNS.strToBase64 = function(str) {};
