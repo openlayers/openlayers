628a3012ce29f0a82c3b0a73685c4155d70e3a46
Petr Pridal <petr.pridal@klokantech.com>
2014-07-06 01:59:42 +0200

    Added missing ol.proj dependecy catched by Travis on xyz-retina example

--- examples/xyz-retina.js
+++ examples/xyz-retina.js
@@ -2,6 +2,7 @@ goog.require('ol.Attribution');
 goog.require('ol.Map');
 goog.require('ol.View');
 goog.require('ol.layer.Tile');
+goog.require('ol.proj');
 goog.require('ol.source.OSM');
 goog.require('ol.source.XYZ');
 
