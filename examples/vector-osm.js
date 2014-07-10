8e2a12bfc1df59f512f47e0dfe96e84c2783d730
Peter Robins <github@peterrobins.co.uk>
2014-07-10 11:07:39 +0000

    Change examples/vector-osm to use transformExtent

--- examples/vector-osm.js
+++ examples/vector-osm.js
@@ -90,8 +90,8 @@ var styles = {
 var vectorSource = new ol.source.ServerVector({
   format: new ol.format.OSMXML(),
   loader: function(extent, resolution, projection) {
-    var transform = ol.proj.getTransform(projection, 'EPSG:4326');
-    var epsg4326Extent = transform(extent, []);
+    var epsg4326Extent =
+        ol.proj.transformExtent(extent, projection, 'EPSG:4326');
     var url = 'http://overpass-api.de/api/xapi?map?bbox=' +
         epsg4326Extent.join(',');
     $.ajax(url).then(function(response) {
