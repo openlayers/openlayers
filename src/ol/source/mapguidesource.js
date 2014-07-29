5ba1ff9dde77b2caff13f3ad05e0f0a5b7d8c897
ChouGary <prestq@gmail.com>
2014-07-29 10:15:24 +0800

    Update mapguidesource.js

--- src/ol/source/mapguidesource.js
+++ src/ol/source/mapguidesource.js
@@ -58,7 +58,7 @@ ol.source.MapGuide = function(options) {
    * @private
    * @type {Object}
    */
-  this.params_ = goog.isDef(params) ? params : null;
+  this.params_ = goog.isDef(options.params) ? options.params : null;
 
   /**
    * @private
