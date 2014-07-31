bf3770a9da4453db7f67108ae0e728038c24e1ce
Peter Robins <github@peterrobins.co.uk>
2014-07-31 17:47:31 +0000

    Improve docs for style

--- src/ol/style/strokestyle.js
+++ src/ol/style/strokestyle.js
@@ -7,6 +7,9 @@ goog.require('ol.color');
 /**
  * @classdesc
  * Set stroke style for vector features.
+ * Note that the defaults given are the Canvas defaults, which will be used if
+ * option is not defined. The `get` functions return whatever was entered in
+ * the options; they will not return the default.
  *
  * @constructor
  * @param {olx.style.StrokeOptions=} opt_options Options.
