c7ba6f4c6efe175ed0befcbb5e3e43c93d918cba
Tim Schaub <tim.schaub@gmail.com>
2014-08-21 16:45:02 -0600

    Rename doc template from apidoc to api

--- build.py
+++ build.py
@@ -533,10 +533,10 @@ virtual('apidoc', 'build/jsdoc-%(BRANCH)s-timestamp' % vars(variables))
 
 
 @target('build/jsdoc-%(BRANCH)s-timestamp' % vars(variables), 'host-resources',
-        SRC, SHADER_SRC, ifind('buildcfg/jsdoc/apidoc/template'))
+        SRC, SHADER_SRC, ifind('buildcfg/jsdoc/api/template'))
 def jsdoc_BRANCH_timestamp(t):
-    t.run('%(JSDOC)s', 'buildcfg/jsdoc/apidoc/index.md',
-          '-c', 'buildcfg/jsdoc/apidoc/conf.json',
+    t.run('%(JSDOC)s', 'buildcfg/jsdoc/api/index.md',
+          '-c', 'buildcfg/jsdoc/api/conf.json',
           '-d', 'build/hosted/%(BRANCH)s/apidoc')
     t.touch()
 
