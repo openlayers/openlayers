db546a60ea2cf6047dca63a96f96ca33a6d9743e
Éric Lemoine <eric.lemoine@gmail.com>
2014-08-22 17:18:36 +0200

    Add missing "rotate" field to DefaultsOptions typedef

--- externs/olx.js
+++ externs/olx.js
@@ -873,8 +873,9 @@ olx.control.ControlOptions.prototype.target;
 /**
  * @typedef {{attribution: (boolean|undefined),
  *     attributionOptions: (olx.control.AttributionOptions|undefined),
- *     zoom: (boolean|undefined),
+ *     rotate: (boolean|undefined),
  *     rotateOptions: (olx.control.RotateOptions|undefined),
+ *     zoom: (boolean|undefined),
  *     zoomOptions: (olx.control.ZoomOptions|undefined)}}
  * @api
  */
