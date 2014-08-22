73df93969a28efe6e98459a38564ca77295afe62
Éric Lemoine <eric.lemoine@gmail.com>
2014-08-22 18:09:46 +0200

    No composeFrame if layer and view don't intersect
    ol.renderer.canvas.TileLayer#prepareFrame immediately returns false if the layer extent and the view extent do not intersect.

--- src/ol/renderer/canvas/canvastilelayerrenderer.js
+++ src/ol/renderer/canvas/canvastilelayerrenderer.js
@@ -197,6 +197,10 @@ ol.renderer.canvas.TileLayer.prototype.prepareFrame =
   if (goog.isDef(layerState.extent)) {
     extent = ol.extent.getIntersection(extent, layerState.extent);
   }
+  if (ol.extent.isEmpty(extent)) {
+    // Return false to prevent the rendering of the layer.
+    return false;
+  }
 
   var tileRange = tileGrid.getTileRangeForExtentAndResolution(
       extent, tileResolution);
