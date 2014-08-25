8107a0582a890c389e2a401530d35fedda8b5048
Bart van den Eijnden <bartvde@boundlessgeo.com>
2014-08-25 16:51:21 +0200

    Use extractStyles false in KML earthquakes example

--- examples/kml-earthquakes.js
+++ examples/kml-earthquakes.js
@@ -39,6 +39,7 @@ var styleFunction = function(feature, resolution) {
 
 var vector = new ol.layer.Vector({
   source: new ol.source.KML({
+    extractStyles: false,
     projection: 'EPSG:3857',
     url: 'data/kml/2012_Earthquakes_Mag5.kml'
   }),
