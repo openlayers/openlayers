f7f4c7b686a239ec769cae751eeee9fd54314ed9
Éric Lemoine <eric.lemoine@gmail.com>
2014-08-29 11:39:39 +0200

    Remove @api annotation from ol.sphere.WGS84

--- src/ol/sphere/wgs84sphere.js
+++ src/ol/sphere/wgs84sphere.js
@@ -7,6 +7,5 @@ goog.require('ol.Sphere');
  * A sphere with radius equal to the semi-major axis of the WGS84 ellipsoid.
  * @const
  * @type {ol.Sphere}
- * @api
  */
 ol.sphere.WGS84 = new ol.Sphere(6378137);
