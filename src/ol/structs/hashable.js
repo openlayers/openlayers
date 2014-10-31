46ec0785012c23bd1129bf4664358d613e5ce344
tsauerwein <tobias.sauerwein@camptocamp.com>
2014-10-31 11:17:03 +0100

    Add Hashable interface

---
+++ src/ol/structs/hashable.js
@@ -0,0 +1,16 @@
+goog.provide('ol.structs.IHashable');
+
+
+
+/**
+ * @interface
+ */
+ol.structs.IHashable = function() {
+};
+
+
+/**
+ * @return {number} The hash code.
+ */
+ol.structs.IHashable.prototype.hashCode = function() {
+};
