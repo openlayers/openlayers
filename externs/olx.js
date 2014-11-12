54772cfaccbf86cd19c9210e091b215dac40f156
Frederic Junod <frederic.junod@camptocamp.com>
2014-11-12 08:30:25 +0100

    Add missing properties in olx.control.AttributionOptions typedef

--- externs/olx.js
+++ externs/olx.js
@@ -797,6 +797,11 @@ olx.control;
 
 /**
  * @typedef {{className: (string|undefined),
+ *     collapsible: (boolean|undefined),
+ *     collapsed: (boolean|undefined),
+ *     tipLabel: (string|undefined),
+ *     label: (string|undefined),
+ *     collapseLabel: (string|undefined),
  *     tagName: (string|undefined),
  *     target: (Element|undefined)}}
  * @api
