a1c6662812684c6be5289b17eac2972cd01be67a
Bart van den Eijnden <bartvde@boundlessgeo.com>
2014-12-15 20:28:13 +0100

    Do not quote featureNS and featureType in options

--- src/ol/format/gml/gmlbaseformat.js
+++ src/ol/format/gml/gmlbaseformat.js
@@ -544,8 +544,8 @@ ol.format.GMLBase.prototype.readFeatures;
 ol.format.GMLBase.prototype.readFeaturesFromNode =
     function(node, opt_options) {
   var options = {
-    'featureType': this.featureType,
-    'featureNS': this.featureNS
+    featureType: this.featureType,
+    featureNS: this.featureNS
   };
   if (goog.isDef(opt_options)) {
     goog.object.extend(options, this.getReadOptions(node, opt_options));
