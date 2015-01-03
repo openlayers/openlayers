53d77f764dad36e1ab5942b559cbd1fac3898d7d
acanimal <asantiago@acuriousanimal.com>
2015-01-03 17:11:11 +0100

    Solved gslint issued on ol.source.GeoJSON

--- src/ol/source/geojsonsource.js
+++ src/ol/source/geojsonsource.js
@@ -7,11 +7,12 @@ goog.require('ol.source.StaticVector');
 
 /**
  * @classdesc
- * Static vector source in GeoJSON format.  
- * Note, although the {@link ol.format.GeoJSON} format class, used by this source,
- * allows to read/write features and geometries, the GeoJSON source limits this 
- * to only allow to read <em>features</em>, which follow the definition of source 
- * at {@link ol.source.Vector}). So next is a valid GeoJSON to pass to this source:
+ * Static vector source in GeoJSON format.
+ * Note, although the {@link ol.format.GeoJSON} format class, used by this
+ * source, allows to read/write features and geometries, the GeoJSON source
+ * limits this to only allow to read <em>features</em>, which follow the
+ * definition of source at {@link ol.source.Vector}). So next is a valid
+ * GeoJSON to pass to this source:
  *
  *     {
  *       "type": "Feature",
@@ -28,9 +29,9 @@ goog.require('ol.source.StaticVector');
  *       "coordinates": [0,0]
  *     }
  *
- * To read a GeoJSON formed of geometries you must read it using the 
- * {@link ol.format.GeoJSON} format class and, for each geometry, create a feature
- * and add it to a vector source.
+ * To read a GeoJSON formed of geometries you must read it using the
+ * {@link ol.format.GeoJSON} format class and, for each geometry, create a
+ * feature and add it to a vector source.
  *
  * @constructor
  * @extends {ol.source.StaticVector}
