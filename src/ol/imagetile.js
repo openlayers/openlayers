bf35b40b11790cf3e6dcd693bd663a0cdba4016b
Tim Schaub <tim.schaub@gmail.com>
2015-02-17 11:09:16 -0700

    Fire change event every time state changes

--- src/ol/imagetile.js
+++ src/ol/imagetile.js
@@ -136,6 +136,7 @@ ol.ImageTile.prototype.handleImageLoad_ = function() {
 ol.ImageTile.prototype.load = function() {
   if (this.state == ol.TileState.IDLE) {
     this.state = ol.TileState.LOADING;
+    this.changed();
     goog.asserts.assert(goog.isNull(this.imageListenerKeys_));
     this.imageListenerKeys_ = [
       goog.events.listenOnce(this.image_, goog.events.EventType.ERROR,
