de597a9744e0ff93df50597063b95a3b464edc69
Christopher S. Case <ccase@armt.com>
2015-02-18 11:34:24 -0600

    added unit tests.

--- examples_src/translate-features.js
+++ examples_src/translate-features.js
@@ -1,12 +1,12 @@
 goog.require('ol.Feature');
 goog.require('ol.Map');
 goog.require('ol.View');
+goog.require('ol.format.GeoJSON');
 goog.require('ol.geom.LineString');
 goog.require('ol.geom.Point');
 goog.require('ol.interaction');
 goog.require('ol.interaction.Select');
 goog.require('ol.interaction.Translate');
-goog.require('ol.format.GeoJSON');
 goog.require('ol.layer.Tile');
 goog.require('ol.layer.Vector');
 goog.require('ol.source.MapQuest');
