3e426ccedd5c23b71de147ce57e5455e8ad65e62
Andreas Hocevar <andreas.hocevar@gmail.com>
2015-03-04 22:11:16 +0100

    Fix HiDPI support for ArcGIS REST

--- src/ol/source/tilearcgisrestsource.js
+++ src/ol/source/tilearcgisrestsource.js
@@ -109,6 +109,7 @@ ol.source.TileArcGISRest.prototype.getRequestUrl_ =
   params['BBOX'] = tileExtent.join(',');
   params['BBOXSR'] = srid;
   params['IMAGESR'] = srid;
+  params['DPI'] = 90 * pixelRatio;
 
   var url;
   if (urls.length == 1) {
@@ -134,6 +135,23 @@ ol.source.TileArcGISRest.prototype.getRequestUrl_ =
   }
 
   return goog.uri.utils.appendParamsFromMap(url, params);
+};
+
+
+/**
+ * @param {number} z Z.
+ * @param {number} pixelRatio Pixel ratio.
+ * @param {ol.proj.Projection} projection Projection.
+ * @return {number} Size.
+ */
+ol.source.TileArcGISRest.prototype.getTilePixelSize =
+    function(z, pixelRatio, projection) {
+  var tileSize = goog.base(this, 'getTilePixelSize', z, pixelRatio, projection);
+  if (pixelRatio == 1) {
+    return tileSize;
+  } else {
+    return (tileSize * pixelRatio + 0.5) | 0;
+  }
 };
 
 
