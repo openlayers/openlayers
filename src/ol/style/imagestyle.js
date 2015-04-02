5bb7535207b18b5ad54ba83dede373f4053ecb51
Tim Schaub <tim.schaub@gmail.com>
2015-04-02 14:48:51 +0200

    Remove @api annotation from abstract method

--- src/ol/style/imagestyle.js
+++ src/ol/style/imagestyle.js
@@ -124,7 +124,6 @@ ol.style.Image.prototype.getAnchor = goog.abstractMethod;
  * @function
  * @param {number} pixelRatio Pixel ratio.
  * @return {HTMLCanvasElement|HTMLVideoElement|Image} Image element.
- * @api
  */
 ol.style.Image.prototype.getImage = goog.abstractMethod;
 
