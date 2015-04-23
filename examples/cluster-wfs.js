d821a891abb2ad05cef5043ca51ad719cfd979a0
cmiles74 <twitch@nervestaple.com>
2015-04-23 16:24:58 -0400

    More style changes

--- examples/cluster-wfs.js
+++ examples/cluster-wfs.js
@@ -1,28 +1,30 @@
-goog.require('ol.source.Vector');
+goog.require('ol.Map');
+goog.require('ol.View');
+goog.require('ol.format.GeoJSON');
+goog.require('ol.layer.Tile');
+goog.require('ol.layer.Vector');
 goog.require('ol.source.Cluster');
 goog.require('ol.source.OSM');
-goog.require('ol.format.GeoJSON');
-goog.require('ol.layer.Vector');
-goog.require('ol.layer.Tile');
+goog.require('ol.source.Vector');
 goog.require('ol.style.Circle');
 goog.require('ol.style.Fill');
 goog.require('ol.style.Text');
-goog.require('ol.Map');
-goog.require('ol.View');
 
 var vectorSource = new ol.source.Vector({
   'format': new ol.format.GeoJSON(),
   'loader': function(extent, resolution, projection) {
-    var url = "http://mhc-macris.net:8080/geoserver/MHC/ows?service=WFS&version=1.0.0"
-          + "&request=GetFeature&typeName=MHC:in_pts&outputFormat=json&outputFormat=text/javascript"
-          + "&format_options=callback:geoJsonLoader"
-          + "&srsname=EPSG:3857&bbox=" + extent.join(",") + ",EPSG:3857";
+    var url = 'http://mhc-macris.net:8080/geoserver/MHC/ows?service=WFS&version=1.0.0' +
+          '&request=GetFeature&typeName=MHC:in_pts&outputFormat=json&outputFormat=text/javascript' +
+          '&format_options=callback:geoJsonLoader' +
+          '&srsname=EPSG:3857&bbox=' + extent.join(',') + ',EPSG:3857';
     $.ajax({
       'url': url,
       'dataType': 'jsonp',
       'success': function(response) {
         var format = new ol.format.GeoJSON();
-        var features = format.readFeatures(response, {featureProjection: projection});
+          var features = format.readFeatures(response, {
+              featureProjection: projection
+          });
         vectorSource.addFeatures(features);
       }
     });
@@ -87,9 +89,10 @@ var tileLayer = new ol.layer.Tile({
 var map = new ol.Map({
   'target': 'map',
   'layers': [tileLayer, clusterLayer],
-  'view': new ol.View({
-    'center': ol.proj.transform([-72.638429, 42.313229], 'EPSG:4326', 'EPSG:3857'),
-    'zoom': 16,
+    'view': new ol.View({
+      'center': ol.proj.transform([-72.638429, 42.313229],
+                                  'EPSG:4326', 'EPSG:3857'),
+    'zoom': 16,xs
     'projection': 'EPSG:3857'
   })
 });
