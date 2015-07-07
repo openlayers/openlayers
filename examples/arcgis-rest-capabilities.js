ba5ab2c208425c3b678c444172f2a917ce9b89ea
Bart van den Eijnden <bartvde@boundlessgeo.com>
2015-07-07 20:51:50 +0200

    Example of using an external library

--- examples/arcgis-rest-capabilities.js
+++ examples/arcgis-rest-capabilities.js
@@ -12,112 +12,6 @@ goog.require('ol.tilegrid.TileGrid');
 
 var map;
 
-var getResolutionsFromConfig = function(config) {
-  var tileInfo = config['tileInfo'];
-  if (tileInfo) {
-    var resolutions = [];
-    for (var i = 0, ii = tileInfo.lods.length; i < ii; ++i) {
-      resolutions.push(tileInfo.lods[i].resolution);
-    }
-    return resolutions;
-  }
-};
-
-var getProjectionFromConfig = function(config) {
-  var epsg = 'EPSG:' + config.spatialReference.wkid;
-  var units = config.units === 'esriMeters' ? 'm' : 'degrees';
-  var projection = ol.proj.get(epsg) ? ol.proj.get(epsg) :
-      new ol.proj.Projection({code: epsg, units: units});
-  return projection;
-};
-
-var createXYZSource = function(config, serviceUrl, attributions, projection,
-    resolutions) {
-  var tileInfo = config['tileInfo'];
-  var tileSize = [
-    tileInfo.width || tileInfo.cols,
-    tileInfo.height || tileInfo.rows
-  ];
-  var tileOrigin = [
-    tileInfo.origin.x,
-    tileInfo.origin.y
-  ];
-  var urls;
-  var suffix = '/tile/{z}/{y}/{x}';
-  if (config.tileServers) {
-    urls = config.tileServers;
-    for (var i = 0, ii = urls.length; i < ii; ++i) {
-      urls[i] += suffix;
-    }
-  } else {
-    urls = [serviceUrl += suffix];
-  }
-  var width = tileSize[0] * resolutions[0];
-  var height = tileSize[1] * resolutions[0];
-  var tileUrlFunction, extent, tileGrid;
-  if (projection.getCode() === 'EPSG:4326') {
-    tileUrlFunction = function(tileCoord) {
-      var url = urls.length === 1 ? urls[0] :
-          urls[Math.floor(Math.random() * (urls.length - 0 + 1)) + 0];
-      return url.replace('{z}', (tileCoord[0] - 1).toString())
-          .replace('{x}', tileCoord[1].toString())
-          .replace('{y}', (-tileCoord[2] - 1).toString());
-    };
-  } else {
-    extent = [
-      tileOrigin[0],
-      tileOrigin[1] - height,
-      tileOrigin[0] + width,
-      tileOrigin[1]
-    ];
-    tileGrid = new ol.tilegrid.TileGrid({
-      origin: tileOrigin,
-      resolutions: resolutions
-    });
-  }
-  return {
-    extent: extent,
-    source: new ol.source.XYZ({
-      attributions: attributions,
-      projection: projection,
-      extent: extent,
-      tileSize: tileSize,
-      tileGrid: tileGrid,
-      tileUrlFunction: tileUrlFunction,
-      urls: urls
-    })
-  };
-};
-
-var createArcGISRestSource = function(attributions, serviceUrl) {
-  return {
-    source: new ol.source.TileArcGISRest({
-      url: serviceUrl,
-      attributions: attributions
-    })
-  };
-};
-
-var createLayerFromConfig = function(config, serviceUrl, projection,
-    resolutions) {
-  var attributions = [
-    new ol.Attribution({
-      html: config['copyrightText']
-    })
-  ];
-  var sourceInfo;
-  if (config.tileInfo) {
-    sourceInfo = createXYZSource(config, serviceUrl, attributions, projection,
-        resolutions);
-  } else {
-    sourceInfo = createArcGISRestSource(attributions, serviceUrl);
-  }
-  return new ol.layer.Tile({
-    source: sourceInfo.source,
-    extent: sourceInfo.extent
-  });
-};
-
 document.getElementById('get-caps').addEventListener('click', function() {
   var capsUrl = document.getElementById('caps-url').value;
   if (capsUrl.charAt(capsUrl.length - 1) === '?') {
@@ -135,16 +29,11 @@ document.getElementById('get-caps').addEventListener('click', function() {
         alert(config.error.message + '\n' +
             config.error.details.join('\n'));
       } else {
-        var projection = getProjectionFromConfig(config);
-        var resolutions = getResolutionsFromConfig(config);
-        var layer = createLayerFromConfig(config, capsUrl, projection,
-            resolutions);
-        var fullExtent = [
-          config['fullExtent']['xmin'],
-          config['fullExtent']['ymin'],
-          config['fullExtent']['xmax'],
-          config['fullExtent']['ymax']
-        ];
+        var generator = new LayerGenerator({config: config, url: capsUrl});
+        var layer = generator.createLayer();
+        var fullExtent = generator.getFullExtent();
+        var resolutions = generator.getResolutions();
+        var projection = generator.getProjection();
         if (!map) {
           map = new ol.Map({
             controls: ol.control.defaults().extend([
---
+++ examples/ol3-esri.js
@@ -0,0 +1,152 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/* global ol */
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var LayerGenerator = (function () {
+  function LayerGenerator(props) {
+    _classCallCheck(this, LayerGenerator);
+
+    this._config = props.config;
+    this._url = props.url;
+    this._resolutions = this._getResolutions();
+    this._projection = this._getProjection();
+    this._attribution = this._getAttribution();
+    this._fullExtent = this._getFullExtent();
+  }
+
+  _createClass(LayerGenerator, [{
+    key: 'getFullExtent',
+    value: function getFullExtent() {
+      return this._fullExtent;
+    }
+  }, {
+    key: '_getFullExtent',
+    value: function _getFullExtent() {
+      return [this._config.fullExtent.xmin, this._config.fullExtent.ymin, this._config.fullExtent.xmax, this._config.fullExtent.ymax];
+    }
+  }, {
+    key: 'getResolutions',
+    value: function getResolutions() {
+      return this._resolutions;
+    }
+  }, {
+    key: '_getResolutions',
+    value: function _getResolutions() {
+      var tileInfo = this._config.tileInfo;
+      if (tileInfo) {
+        var resolutions = [];
+        for (var i = 0, ii = tileInfo.lods.length; i < ii; ++i) {
+          resolutions.push(tileInfo.lods[i].resolution);
+        }
+        return resolutions;
+      }
+    }
+  }, {
+    key: '_getProjection',
+    value: function _getProjection() {
+      var epsg = 'EPSG:' + this._config.spatialReference.wkid;
+      var units = this._config.units === 'esriMeters' ? 'm' : 'degrees';
+      var projection = ol.proj.get(epsg) ? ol.proj.get(epsg) : new ol.proj.Projection({ code: epsg, units: units });
+      return projection;
+    }
+  }, {
+    key: 'getProjection',
+    value: function getProjection() {
+      return this._projection;
+    }
+  }, {
+    key: '_getAttribution',
+    value: function _getAttribution() {
+      return new ol.Attribution({
+        html: this._config.copyrightText
+      });
+    }
+  }, {
+    key: 'createArcGISRestSource',
+    value: function createArcGISRestSource() {
+      return new ol.source.TileArcGISRest({
+        url: this._url,
+        attributions: [this._attribution]
+      });
+    }
+  }, {
+    key: 'createXYZSource',
+    value: function createXYZSource() {
+      var tileInfo = this._config.tileInfo;
+      var tileSize = [tileInfo.width || tileInfo.cols, tileInfo.height || tileInfo.rows];
+      var tileOrigin = [tileInfo.origin.x, tileInfo.origin.y];
+      var urls;
+      var suffix = '/tile/{z}/{y}/{x}';
+      if (this._config.tileServers) {
+        urls = this._config.tileServers;
+        for (var i = 0, ii = urls.length; i < ii; ++i) {
+          urls[i] += suffix;
+        }
+      } else {
+        urls = [this._url += suffix];
+      }
+      var width = tileSize[0] * this._resolutions[0];
+      var height = tileSize[1] * this._resolutions[0];
+      var tileUrlFunction, extent, tileGrid;
+      if (this._projection.getCode() === 'EPSG:4326') {
+        tileUrlFunction = function (tileCoord) {
+          var url = urls.length === 1 ? urls[0] : urls[Math.floor(Math.random() * (urls.length - 0 + 1)) + 0];
+          return url.replace('{z}', (tileCoord[0] - 1).toString()).replace('{x}', tileCoord[1].toString()).replace('{y}', (-tileCoord[2] - 1).toString());
+        };
+      } else {
+        extent = [tileOrigin[0], tileOrigin[1] - height, tileOrigin[0] + width, tileOrigin[1]];
+        tileGrid = new ol.tilegrid.TileGrid({
+          origin: tileOrigin,
+          extent: extent,
+          resolutions: this._resolutions
+        });
+      }
+      return new ol.source.XYZ({
+        attributions: [this._attribution],
+        projection: this._projection,
+        extent: extent,
+        tileSize: tileSize,
+        tileGrid: tileGrid,
+        tileUrlFunction: tileUrlFunction,
+        urls: urls
+      });
+    }
+  }, {
+    key: 'createLayer',
+    value: function createLayer() {
+      var layer = new ol.layer.Tile();
+      if (this._config.tileInfo) {
+        layer.setSource(this.createXYZSource());
+      } else {
+        layer.setSource(this.createArcGISRestSource());
+      }
+      return layer;
+    }
+  }]);
+
+  return LayerGenerator;
+})();
+
+exports['default'] = LayerGenerator;
+module.exports = exports['default'];
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _LayerGeneratorJs = require('./LayerGenerator.js');
+
+var _LayerGeneratorJs2 = _interopRequireDefault(_LayerGeneratorJs);
+
+window.LayerGenerator = _LayerGeneratorJs2['default'];
+
+},{"./LayerGenerator.js":1}]},{},[2]);
