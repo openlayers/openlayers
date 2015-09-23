ee4f009d2f2798af300b8355a179249a18bfe9c8
Frederic Junod <frederic.junod@camptocamp.com>
2015-09-23 12:24:54 +0200

    Remove use of goog.object.containsKey

--- src/ol/pointer/mousesource.js
+++ src/ol/pointer/mousesource.js
@@ -30,7 +30,6 @@
 
 goog.provide('ol.pointer.MouseSource');
 
-goog.require('goog.object');
 goog.require('ol.pointer.EventSource');
 
 
@@ -161,11 +160,9 @@ ol.pointer.MouseSource.prepareEvent = function(inEvent, dispatcher) {
  */
 ol.pointer.MouseSource.prototype.mousedown = function(inEvent) {
   if (!this.isEventSimulatedFromTouch_(inEvent)) {
-    var p = goog.object.containsKey(this.pointerMap,
-        ol.pointer.MouseSource.POINTER_ID.toString());
     // TODO(dfreedman) workaround for some elements not sending mouseup
     // http://crbug/149091
-    if (p) {
+    if (ol.pointer.MouseSource.POINTER_ID.toString() in this.pointerMap) {
       this.cancel(inEvent);
     }
     var e = ol.pointer.MouseSource.prepareEvent(inEvent, this.dispatcher);
