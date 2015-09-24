7252227ac66b79c3e394b7cf262ae1fae9cf652b
Frederic Junod <frederic.junod@camptocamp.com>
2015-09-24 13:31:28 +0200

    Remove externs in externs/closure-compiler.js
    Fixed upstream:
     * https://github.com/google/closure-compiler/pull/1139
     * https://github.com/google/closure-compiler/pull/1140

--- externs/closure-compiler.js
+++ externs/closure-compiler.js
@@ -7,35 +7,3 @@
  *
  * @externs
  */
-
-/** @type {number} */
-Touch.prototype.force;
-
-
-/** @type {number} */
-Touch.prototype.radiusX;
-
-
-/** @type {number} */
-Touch.prototype.radiusY;
-
-
-/** @type {number} */
-Touch.prototype.webkitForce;
-
-
-/** @type {number} */
-Touch.prototype.webkitRadiusX;
-
-
-/** @type {number} */
-Touch.prototype.webkitRadiusY;
-
-
-// see https://github.com/google/closure-compiler/pull/1139
-
-/**
- * @type {string}
- * @see http://www.w3.org/TR/pointerevents/#the-touch-action-css-property
- */
-CSSProperties.prototype.touchAction;
