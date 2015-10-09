5a1e830046baaabc016814fa173f25a1927e88ca
Jonathan Martin <jonathan.martin@marss.com>
2015-10-09 09:57:22 +0200

    Correct typechecking annotations

--- src/ol/style/fillstyle.js
+++ src/ol/style/fillstyle.js
@@ -26,7 +26,7 @@ ol.style.Fill = function(opt_options) {
 
   /**
    * @private
-   * @type {function}
+   * @type {function()}
    */
   this.renderer_ = options.renderer !== undefined ? options.renderer : null;
 
@@ -62,7 +62,7 @@ ol.style.Fill.prototype.setColor = function(color) {
 
 /**
  * Get the fill renderer.
- * @return {function} Function.
+ * @return {function()} Function.
  * @api
  */
 ol.style.Fill.prototype.getRenderer = function() {
@@ -73,7 +73,7 @@ ol.style.Fill.prototype.getRenderer = function() {
 /**
  * Set the renderer.
  *
- * @param {function} renderer Function.
+ * @param {function()} renderer Function.
  * @api
  */
 ol.style.Fill.prototype.setRenderer = function(renderer) {
