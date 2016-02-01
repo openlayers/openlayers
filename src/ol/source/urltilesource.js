04db1c3e4de7db2de36f0c9ab49cc5400b985552
Andreas Hocevar <andreas.hocevar@gmail.com>
2016-02-01 17:58:38 +0100

    Expand urls before setting this.urls

--- src/ol/source/urltilesource.js
+++ src/ol/source/urltilesource.js
@@ -182,9 +182,9 @@ ol.source.UrlTile.prototype.setTileUrlFunction = function(tileUrlFunction) {
  * @api stable
  */
 ol.source.UrlTile.prototype.setUrl = function(url) {
-  this.urls = [url];
-  var urls = ol.TileUrlFunction.expandUrl(url);
-  this.setTileUrlFunction(this.fixedTileUrlFunction ||
+  var urls = this.urls = ol.TileUrlFunction.expandUrl(url);
+  this.setTileUrlFunction(this.fixedTileUrlFunction ?
+      this.fixedTileUrlFunction.bind(this) :
       ol.TileUrlFunction.createFromTemplates(urls, this.tileGrid));
 };
 
