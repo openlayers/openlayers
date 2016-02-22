d829da2cb9099dc5861fb295efefabcbd7c5567e
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2016-02-22 11:01:06 +0100

    add missing options in graticule example

--- examples/graticule.js
+++ examples/graticule.js
@@ -36,28 +36,37 @@ var latFormatter = function(lat) {
 
 // Create the graticule component
 var graticule = new ol.Graticule({
+  
   // the style to use for the lines, optional.
   strokeStyle: new ol.style.Stroke({
     color: 'rgba(255,120,0,0.9)',
     width: 2,
     lineDash: [0.5, 4]
   }),
-  lonLabelStyle: new ol.style.Text({
-    font: '14px Calibri,sans-serif',
-    fill: new ol.style.Fill({
-      color: 'rgba(0,0,0,1)'
-    })
-  }),
-  latLabelStyle: new ol.style.Text({
-    font: '14px Calibri,sans-serif',
-	  offsetX: -2,
-    textBaseline: 'bottom',
-    fill: new ol.style.Fill({
-      color: 'rgba(0,0,0,1)'
-    })
-  }),
-  showLabels: true,
-  lonLabelFormatter: lonFormatter,
-  latLabelFormatter: latFormatter
+  
+  //show labels
+  showLabels : true,
+	
+	//label positions
+	lonLabelPosition: 0.05,
+	latLabelPosition: 0.95,
+	
+	//style for longitude label
+	lonLabelStyle: new ol.style.Text({
+			font: '10px Verdana',
+			fill: new ol.style.Fill({
+				color: 'rgba(0,0,0,1)'
+			})
+	}),
+	
+	//style for latitude label
+	latLabelStyle: new ol.style.Text({
+			font: '10px Verdana',
+			offsetX: -2,
+			textBaseline: 'bottom',
+			fill: new ol.style.Fill({
+				color: 'rgba(0,0,0,1)'
+			})
+	})
 });
 graticule.setMap(map);
