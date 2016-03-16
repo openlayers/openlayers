6f2747cf1411cca664672b435851af52cfc065a5
Peter Robins <github@peterrobins.co.uk>
2016-03-16 16:30:59 +0000

    Note in docs that DOM renderer can also render vectors

--- externs/olx.js
+++ externs/olx.js
@@ -281,8 +281,8 @@ olx.MapOptions.prototype.overlays;
  * Renderer. By default, Canvas, DOM and WebGL renderers are tested for support
  * in that order, and the first supported used. Specify a
  * {@link ol.RendererType} here to use a specific renderer.
- * Note that at present only the Canvas renderer fully supports vector data,
- * though WebGL can render Point geometries.
+ * Note that at present the Canvas and DOM renderers fully support vector data,
+ * but WebGL can only render Point geometries.
  * @type {ol.RendererType|Array.<ol.RendererType|string>|string|undefined}
  * @api stable
  */
