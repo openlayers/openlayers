f99b307b2a15c7a531e2941599f03b6a4dec126b
nicholas <nicholas-l@users.noreply.github.com>
2016-03-29 20:30:40 +1300

    Add glMatrix

---
+++ externs/gl-matrix.js
@@ -0,0 +1,53 @@
+var SIMD = {};
+
+
+/**
+ * @typedef {Float32Array|Array}
+ */
+var Type;
+
+
+/**
+ * @typedef {Array} mat2
+ */
+var mat2;
+
+/**
+ * @typedef {Array} mat2
+ */
+var mat2d;
+
+/**
+ * @typedef {Array}
+ */
+var mat3;
+
+/**
+ * @typedef {Array}
+ */
+var mat4;
+
+/**
+ * @typedef {Array}
+ */
+var vec2;
+
+/**
+ * @typedef {Array}
+ */
+var vec3;
+
+/**
+ * @typedef {Array}
+ */
+var vec4;
+
+/**
+ * @typedef {Array}
+ */
+var quat;
+
+/**
+ * @typedef {Array}
+ */
+var quat4;
