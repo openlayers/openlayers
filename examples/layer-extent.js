6902791dc5c1e82c2379ad19e21230b9fc6ea348
Ian Schneider <planablediglet@gmail.com>
2016-06-15 11:12:07 -0600

    layer extent example changes for testing
    - add draw extent tool
    - rotation control

--- examples/layer-extent.js
+++ examples/layer-extent.js
@@ -3,6 +3,8 @@ goog.require('ol.View');
 goog.require('ol.layer.Tile');
 goog.require('ol.proj');
 goog.require('ol.source.TileJSON');
+goog.require('ol.interaction.Draw');
+goog.require('ol.interaction.DragRotateAndZoom');
 
 function transform(extent) {
   return ol.proj.transformExtent(extent, 'EPSG:4326', 'EPSG:3857');
@@ -42,6 +44,37 @@ var map = new ol.Map({
     zoom: 1
   })
 });
+
+function box(coordinates, geometry) {
+  if (!geometry) {
+    geometry = new ol.geom.Polygon(null);
+  }
+  var start = coordinates[0];
+  var end = coordinates[1];
+
+  geometry.setCoordinates([
+    [start, [start[0], end[1]], end, [end[0], start[1]], start]
+  ]);
+  return geometry;
+}
+
+
+var draw = new ol.interaction.Draw({
+  type: /** @type {ol.geom.GeometryType} */ ('LineString'),
+  geometryFunction: box,
+  maxPoints: 2
+});
+
+draw.on('drawend', function(ev) {
+  var extent = ev.feature.getGeometry().getExtent();
+  overlay.setExtent(extent);
+});
+
+map.addInteraction(new ol.interaction.DragRotateAndZoom());
+
+document.getElementById('draw').onclick = function() {
+  map.addInteraction(draw);
+};
 
 for (var key in extents) {
   document.getElementById(key).onclick = function(event) {
