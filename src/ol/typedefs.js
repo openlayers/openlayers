96dbea7788882a05221abee1dd5d16cfc58b76c1
Pavel Penev <ppenev@t-sciences.com>
2016-06-21 11:23:51 -0400

    Add {ModifySegmentDataType#featureSegments} field
    An optional {@link ModifySegmentDataType#featureSegments} field has
    been added. It is used to list all the modifyable line segments that
    are part of the same feature.
    
    This is currently needed for editing circles.  They are broken into two
    segments: center and circumference. If the center is modified, both
    segments need to be updated.

--- src/ol/typedefs.js
+++ src/ol/typedefs.js
@@ -344,7 +344,8 @@ ol.Transform;
  *            feature: ol.Feature,
  *            geometry: ol.geom.SimpleGeometry,
  *            index: (number),
- *            segment: Array.<ol.Extent>}}
+ *            segment: Array.<ol.Extent>,
+ *            featureSegments: (Array.<ol.ModifySegmentDataType>|undefined)}}
  */
 ol.ModifySegmentDataType;
 
