4cde6a0a6d6ddd2bec593a08c7954e69080978f3
Tim Schaub <tim.schaub@gmail.com>
2016-06-24 10:01:08 -0600

    Move assertions into `it` blocks

--- test/spec/ol/transform.test.js
+++ test/spec/ol/transform.test.js
@@ -40,45 +40,57 @@ describe('ol.transform', function() {
   });
 
   describe('ol.transform.translate()', function() {
-    var transform = ol.transform.create();
-    expect(ol.transform.translate(transform, 3, 4)).to.eql([1, 0, 0, 1, 3, 4]);
-    expect(transform).to.eql([1, 0, 0, 1, 3, 4]);
+    it('applies translation to a transform', function() {
+      var transform = ol.transform.create();
+      expect(ol.transform.translate(transform, 3, 4)).to.eql([1, 0, 0, 1, 3, 4]);
+      expect(transform).to.eql([1, 0, 0, 1, 3, 4]);
+    });
   });
 
   describe('ol.transform.scale()', function() {
-    var transform = ol.transform.create();
-    expect(ol.transform.scale(transform, 3, 4)).to.eql([3, 0, 0, 4, 0, 0]);
-    expect(transform).to.eql([3, 0, 0, 4, 0, 0]);
+    it('applies scaling to a transform', function() {
+      var transform = ol.transform.create();
+      expect(ol.transform.scale(transform, 3, 4)).to.eql([3, 0, 0, 4, 0, 0]);
+      expect(transform).to.eql([3, 0, 0, 4, 0, 0]);
+    });
   });
 
   describe('ol.transform.rotate()', function() {
-    var transform = ol.transform.create();
-    assertRoughlyEqual(ol.transform.rotate(transform, Math.PI / 2), [0, 1, -1, 0, 0, 0]);
-    assertRoughlyEqual(transform, [0, 1, -1, 0, 0, 0]);
+    it('applies rotation to a transform', function() {
+      var transform = ol.transform.create();
+      assertRoughlyEqual(ol.transform.rotate(transform, Math.PI / 2), [0, 1, -1, 0, 0, 0]);
+      assertRoughlyEqual(transform, [0, 1, -1, 0, 0, 0]);
+    });
   });
 
   describe('ol.transform.multiply()', function() {
-    var transform1 = [1, 2, 1, 2, 1, 2];
-    var transform2 = [1, 2, 1, 2, 1, 2];
-    expect(ol.transform.multiply(transform1, transform2)).to.eql([3, 6, 3, 6, 4, 8]);
-    expect(transform1).to.eql([3, 6, 3, 6, 4, 8]);
+    it('multiplies two transforms', function() {
+      var transform1 = [1, 2, 1, 2, 1, 2];
+      var transform2 = [1, 2, 1, 2, 1, 2];
+      expect(ol.transform.multiply(transform1, transform2)).to.eql([3, 6, 3, 6, 4, 8]);
+      expect(transform1).to.eql([3, 6, 3, 6, 4, 8]);
+    });
   });
 
   describe('ol.transform.invert()', function() {
-    var transform = [1, 0, 1, 0, 1, 0];
-    expect(function() {
-      ol.transform.invert(transform);
-    }).to.throwException();
-    transform = [1, 1, 1, 2, 2, 0];
-    expect(ol.transform.invert(transform)).to.eql([2, -1, -1, 1, -4, 2]);
-    expect(transform).to.eql([2, -1, -1, 1, -4, 2]);
+    it('inverts a transform', function() {
+      var transform = [1, 0, 1, 0, 1, 0];
+      expect(function() {
+        ol.transform.invert(transform);
+      }).to.throwException();
+      transform = [1, 1, 1, 2, 2, 0];
+      expect(ol.transform.invert(transform)).to.eql([2, -1, -1, 1, -4, 2]);
+      expect(transform).to.eql([2, -1, -1, 1, -4, 2]);
+    });
   });
 
   describe('ol.transform.apply()', function() {
-    var transform = ol.transform.translate(ol.transform.create(), 2, 3);
-    var point = [1, 2];
-    expect(ol.transform.apply(transform, point)).to.eql([3, 5]);
-    expect(point).to.eql([3, 5]);
+    it('applies a transform to a 2d vector', function() {
+      var transform = ol.transform.translate(ol.transform.create(), 2, 3);
+      var point = [1, 2];
+      expect(ol.transform.apply(transform, point)).to.eql([3, 5]);
+      expect(point).to.eql([3, 5]);
+    });
   });
 
 });
