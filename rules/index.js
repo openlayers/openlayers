279e10abe8537208abbe8c6463c4a76cf162420a
Tim Schaub <tim.schaub@gmail.com>
2016-08-08 23:43:04 -0600

    Rule to catch missing requires

--- rules/index.js
+++ rules/index.js
@@ -3,6 +3,7 @@
 module.exports = {
   rules: {
     'no-duplicate-requires': require('./no-duplicate-requires').rule,
+    'no-missing-requires': require('./no-missing-requires').rule,
     'no-unused-requires': require('./no-unused-requires').rule,
     'requires-first': require('./requires-first').rule,
     'valid-requires': require('./valid-requires').rule
