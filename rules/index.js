4fffa190c305da34b78c280f9782b7008ca2f21a
Tim Schaub <tim.schaub@gmail.com>
2016-08-11 21:50:09 -0600

    Add rule to enforce one provide per file

--- rules/index.js
+++ rules/index.js
@@ -5,6 +5,7 @@ module.exports = {
     'no-duplicate-requires': require('./no-duplicate-requires').rule,
     'no-missing-requires': require('./no-missing-requires').rule,
     'no-unused-requires': require('./no-unused-requires').rule,
+    'one-provide': require('./one-provide').rule,
     'requires-first': require('./requires-first').rule,
     'valid-requires': require('./valid-requires').rule
   }
