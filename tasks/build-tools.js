b6bbd5c95c059156b38db84059c628332e80d29c
tsauerwein <tobias.sauerwein@camptocamp.com>
2016-08-17 09:42:22 +0200

    Add task to check custom builds

---
+++ tasks/build-tools.js
@@ -0,0 +1,31 @@
+var fs = require('fs-extra');
+
+/**
+ * Read the build configuration file.
+ * @param {string} configPath Path to config file.
+ * @param {function(Error, Object)} callback Callback.
+ */
+function readConfig(configPath, callback) {
+  fs.readFile(configPath, function(err, data) {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        err = new Error('Unable to find config file: ' + configPath);
+      }
+      callback(err);
+      return;
+    }
+    var config;
+    try {
+      config = JSON.parse(String(data));
+    } catch (err2) {
+      callback(new Error('Trouble parsing config as JSON: ' + err2.message));
+      return;
+    }
+    callback(null, config);
+  });
+}
+
+
+module.exports = {
+  'readConfig': readConfig
+};
