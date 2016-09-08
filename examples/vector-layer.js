e5ac575eb3e7f035825ca291cef9b9c8aea70a68
Marc Jansen <jansen@terrestris.de>
2016-09-08 08:44:35 +0200

    Simplify basic vector layer example
    This effectively reverts the changes introduced by 860aa9ff465b274.

--- examples/vector-layer.js
+++ examples/vector-layer.js
@@ -11,21 +11,6 @@ goog.require('ol.style.Style');
 goog.require('ol.style.Text');
 
 
-var createPattern = function() {
-  var canvas = document.createElement('canvas');
-  var context = canvas.getContext('2d');
-  canvas.width = 4;
-  canvas.height = 4;
-
-  context.fillStyle = 'rgba(255,0,0,0.3)';
-  for (var i = 0; i < canvas.width; i++) {
-    context.fillRect(i, i, 1, 1);
-  }
-
-  return context.createPattern(canvas, 'repeat');
-};
-
-
 var style = new ol.style.Style({
   fill: new ol.style.Fill({
     color: 'rgba(255, 255, 255, 0.6)'
@@ -85,7 +70,7 @@ var featureOverlay = new ol.layer.Vector({
           width: 1
         }),
         fill: new ol.style.Fill({
-          color: createPattern()
+          color: 'rgba(255,0,0,0.1)'
         }),
         text: new ol.style.Text({
           font: '12px Calibri,sans-serif',
