780bb1b30dbf3f163780af9461c75c83f9d3e7d2
Tim Schaub <tim.schaub@gmail.com>
2016-12-04 13:03:58 -0800

    Dedicated module for ol.extent.Corner enum

--- src/ol/extent/index.js
+++ src/ol/extent/index.js
@@ -1,21 +1,9 @@
 goog.provide('ol.extent');
-goog.provide('ol.extent.Corner');
 goog.provide('ol.extent.Relationship');
 
 goog.require('ol');
 goog.require('ol.asserts');
-
-
-/**
- * Extent corner.
- * @enum {string}
- */
-ol.extent.Corner = {
-  BOTTOM_LEFT: 'bottom-left',
-  BOTTOM_RIGHT: 'bottom-right',
-  TOP_LEFT: 'top-left',
-  TOP_RIGHT: 'top-right'
-};
+goog.require('ol.extent.Corner');
 
 
 /**
