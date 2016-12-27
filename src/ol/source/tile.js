091daf9d7003aff2a531d346c937c0ed5a259df5
Tim Schaub <tim.schaub@gmail.com>
2016-12-27 14:11:10 -0700

    Shared module for tile source event type

--- src/ol/source/tile.js
+++ src/ol/source/tile.js
@@ -334,32 +334,3 @@ ol.source.Tile.Event = function(type, tile) {
 
 };
 ol.inherits(ol.source.Tile.Event, ol.events.Event);
-
-
-/**
- * @enum {string}
- */
-ol.source.Tile.EventType = {
-
-  /**
-   * Triggered when a tile starts loading.
-   * @event ol.source.Tile.Event#tileloadstart
-   * @api stable
-   */
-  TILELOADSTART: 'tileloadstart',
-
-  /**
-   * Triggered when a tile finishes loading.
-   * @event ol.source.Tile.Event#tileloadend
-   * @api stable
-   */
-  TILELOADEND: 'tileloadend',
-
-  /**
-   * Triggered if tile loading results in an error.
-   * @event ol.source.Tile.Event#tileloaderror
-   * @api stable
-   */
-  TILELOADERROR: 'tileloaderror'
-
-};
