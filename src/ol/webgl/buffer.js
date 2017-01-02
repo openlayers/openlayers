9c79564676c7bcd4e0ddaafee1b48c22f692a3d4
Andreas Hocevar <andreas.hocevar@gmail.com>
2017-01-02 21:52:31 +0100

    Wrap ol.webgl code in define condition

--- src/ol/webgl/buffer.js
+++ src/ol/webgl/buffer.js
@@ -4,52 +4,56 @@ goog.require('ol');
 goog.require('ol.webgl');
 
 
-/**
- * @constructor
- * @param {Array.<number>=} opt_arr Array.
- * @param {number=} opt_usage Usage.
- * @struct
- */
-ol.webgl.Buffer = function(opt_arr, opt_usage) {
+if (ol.ENABLE_WEBGL) {
 
   /**
-   * @private
-   * @type {Array.<number>}
+   * @constructor
+   * @param {Array.<number>=} opt_arr Array.
+   * @param {number=} opt_usage Usage.
+   * @struct
    */
-  this.arr_ = opt_arr !== undefined ? opt_arr : [];
+  ol.webgl.Buffer = function(opt_arr, opt_usage) {
+
+    /**
+     * @private
+     * @type {Array.<number>}
+     */
+    this.arr_ = opt_arr !== undefined ? opt_arr : [];
+
+    /**
+     * @private
+     * @type {number}
+     */
+    this.usage_ = opt_usage !== undefined ?
+        opt_usage : ol.webgl.Buffer.Usage_.STATIC_DRAW;
+
+  };
+
 
   /**
-   * @private
-   * @type {number}
+   * @return {Array.<number>} Array.
    */
-  this.usage_ = opt_usage !== undefined ?
-      opt_usage : ol.webgl.Buffer.Usage_.STATIC_DRAW;
-
-};
-
-
-/**
- * @return {Array.<number>} Array.
- */
-ol.webgl.Buffer.prototype.getArray = function() {
-  return this.arr_;
-};
+  ol.webgl.Buffer.prototype.getArray = function() {
+    return this.arr_;
+  };
 
 
-/**
- * @return {number} Usage.
- */
-ol.webgl.Buffer.prototype.getUsage = function() {
-  return this.usage_;
-};
+  /**
+   * @return {number} Usage.
+   */
+  ol.webgl.Buffer.prototype.getUsage = function() {
+    return this.usage_;
+  };
 
 
-/**
- * @enum {number}
- * @private
- */
-ol.webgl.Buffer.Usage_ = {
-  STATIC_DRAW: ol.webgl.STATIC_DRAW,
-  STREAM_DRAW: ol.webgl.STREAM_DRAW,
-  DYNAMIC_DRAW: ol.webgl.DYNAMIC_DRAW
-};
+  /**
+   * @enum {number}
+   * @private
+   */
+  ol.webgl.Buffer.Usage_ = {
+    STATIC_DRAW: ol.webgl.STATIC_DRAW,
+    STREAM_DRAW: ol.webgl.STREAM_DRAW,
+    DYNAMIC_DRAW: ol.webgl.DYNAMIC_DRAW
+  };
+
+}
--- src/ol/webgl/fragment.js
+++ src/ol/webgl/fragment.js
@@ -5,21 +5,25 @@ goog.require('ol.webgl');
 goog.require('ol.webgl.Shader');
 
 
-/**
- * @constructor
- * @extends {ol.webgl.Shader}
- * @param {string} source Source.
- * @struct
- */
-ol.webgl.Fragment = function(source) {
-  ol.webgl.Shader.call(this, source);
-};
-ol.inherits(ol.webgl.Fragment, ol.webgl.Shader);
+if (ol.ENABLE_WEBGL) {
+
+  /**
+   * @constructor
+   * @extends {ol.webgl.Shader}
+   * @param {string} source Source.
+   * @struct
+   */
+  ol.webgl.Fragment = function(source) {
+    ol.webgl.Shader.call(this, source);
+  };
+  ol.inherits(ol.webgl.Fragment, ol.webgl.Shader);
 
 
-/**
- * @inheritDoc
- */
-ol.webgl.Fragment.prototype.getType = function() {
-  return ol.webgl.FRAGMENT_SHADER;
-};
+  /**
+   * @inheritDoc
+   */
+  ol.webgl.Fragment.prototype.getType = function() {
+    return ol.webgl.FRAGMENT_SHADER;
+  };
+
+}
