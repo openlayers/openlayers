743b5bf8dc8a27cab38753130333b28c7fb2d214
Andreas Hocevar <andreas.hocevar@gmail.com>
2017-05-09 14:25:28 +0200

    Add more tests

--- test/spec/ol/vectorimagetile.test.js
+++ test/spec/ol/vectorimagetile.test.js
@@ -56,8 +56,50 @@ describe('ol.VectorImageTile', function() {
 
     tile.load();
 
-    ol.events.listen(tile, 'change', function(e) {
+    ol.events.listen(tile, 'change', function() {
       expect(tile.getState()).to.be(ol.TileState.EMPTY);
+      done();
+    });
+  });
+
+  it('#dispose() while loading', function() {
+    var format = new ol.format.GeoJSON();
+    var url = 'spec/ol/data/point.json';
+    var tile = new ol.VectorImageTile([0, 0, 0], 0, url, format,
+        ol.VectorImageTile.defaultLoadFunction, [0, 0, 0], function() {
+          return url;
+        }, ol.tilegrid.createXYZ(), ol.tilegrid.createXYZ({tileSize: 512}), {},
+        1, ol.proj.get('EPSG:3857'), ol.VectorTile);
+
+    tile.load();
+    expect(tile.loadListenerKeys_.length).to.be(4);
+    expect(tile.tileKeys.length).to.be(4);
+    expect(tile.getState()).to.be(ol.TileState.LOADING);
+    tile.dispose();
+    expect(tile.loadListenerKeys_.length).to.be(0);
+    expect(tile.tileKeys.length).to.be(0);
+    expect(tile.sourceTiles_).to.be(null);
+    expect(tile.getState()).to.be(ol.TileState.ABORT);
+  });
+
+  it('#dispose() when loaded', function(done) {
+    var format = new ol.format.GeoJSON();
+    var url = 'spec/ol/data/point.json';
+    var tile = new ol.VectorImageTile([0, 0, 0], 0, url, format,
+        ol.VectorImageTile.defaultLoadFunction, [0, 0, 0], function() {
+          return url;
+        }, ol.tilegrid.createXYZ(), ol.tilegrid.createXYZ({tileSize: 512}), {},
+        1, ol.proj.get('EPSG:3857'), ol.VectorTile);
+
+    tile.load();
+    ol.events.listenOnce(tile, 'change', function() {
+      expect(tile.getState()).to.be(ol.TileState.LOADED);
+      expect(tile.loadListenerKeys_.length).to.be(0);
+      expect(tile.tileKeys.length).to.be(4);
+      tile.dispose();
+      expect(tile.tileKeys.length).to.be(0);
+      expect(tile.sourceTiles_).to.be(null);
+      expect(tile.getState()).to.be(ol.TileState.ABORT);
       done();
     });
   });
