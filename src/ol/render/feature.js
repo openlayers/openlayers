1d7144564c9c064f272740e50a7688a9e001770e
Andreas Hocevar <andreas.hocevar@gmail.com>
2017-05-11 15:53:55 +0200

    Remove getGeometry() from the API

--- src/ol/render/feature.js
+++ src/ol/render/feature.js
@@ -104,7 +104,6 @@ ol.render.Feature.prototype.getFlatCoordinates =
 /**
  * Get the feature for working with its geometry.
  * @return {ol.render.Feature} Feature.
- * @api
  */
 ol.render.Feature.prototype.getGeometry = function() {
   return this;
