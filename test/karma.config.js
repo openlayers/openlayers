fb820f4b8124b9ba53754dff0db491c732aeed72
Tim Schaub <tim.schaub@gmail.com>
2017-07-03 16:34:28 -0600

    Run tests on Firefox in Travis

--- test/karma.config.js
+++ test/karma.config.js
@@ -1,4 +1,3 @@
-var pkg = require('../package.json');
 var path = require('path');
 
 /**
@@ -48,55 +47,9 @@ module.exports = function(karma) {
   });
 
   if (process.env.TRAVIS) {
-    if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
-      process.stderr.write('SAUCE_USERNAME or SAUCE_ACCESS_KEY not set\n');
-      process.exit(1);
-    }
-
-    // see https://wiki.saucelabs.com/display/DOCS/Platform+Configurator
-    // for platform and browserName options (Selenium API, node.js code)
-    var customLaunchers = {
-      SL_Chrome: {
-        base: 'SauceLabs',
-        browserName: 'chrome'
-      },
-      SL_Firefox: {
-        base: 'SauceLabs',
-        browserName: 'firefox'
-      },
-      SL_IE: {
-        base: 'SauceLabs',
-        platform: 'Windows 10',
-        browserName: 'internet explorer'
-      },
-      SL_Edge: {
-        base: 'SauceLabs',
-        platform: 'Windows 10',
-        browserName: 'MicrosoftEdge'
-      },
-      SL_Safari: {
-        base: 'SauceLabs',
-        platform: 'macos 10.12',
-        browserName: 'safari'
-      }
-    };
     karma.set({
-      sauceLabs: {
-        testName: pkg.name + ' ' + pkg.version,
-        recordScreenshots: false,
-        connectOptions: {
-          port: 5757
-        },
-        startConnect: false,
-        tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY
-      },
-      reporters: ['dots', 'saucelabs'],
-      captureTimeout: 240000,
-      browserNoActivityTimeout: 240000,
-      customLaunchers: customLaunchers,
-      browsers: Object.keys(customLaunchers)
+      reporters: ['dots'],
+      browsers: ['Firefox']
     });
   } else {
     karma.set({
