2907fc3297fd2dc193cd54dc6e8175c976289e62
Andreas Hocevar <andreas.hocevar@gmail.com>
2017-09-11 22:14:56 +0200

    Remove duplicate test

--- test/spec/ol/vectorimagetile.test.js
+++ test/spec/ol/vectorimagetile.test.js
@@ -32,23 +32,6 @@ describe('ol.VectorImageTile', function() {
     });
   });
 
-  it('sets LOADED state when source tiles fail to load', function(done) {
-    var format = new ol.format.GeoJSON();
-    var url = 'spec/ol/data/unavailable.json';
-    var tile = new ol.VectorImageTile([0, 0, -1], 0, url, format,
-        ol.VectorImageTile.defaultLoadFunction, [0, 0, -1], function() {
-          return url;
-        }, ol.tilegrid.createXYZ(), ol.tilegrid.createXYZ(), {},
-        1, ol.proj.get('EPSG:3857'), ol.VectorTile, function() {});
-
-    tile.load();
-
-    ol.events.listen(tile, 'change', function(e) {
-      expect(tile.getState()).to.be(ol.TileState.EMPTY);
-      done();
-    });
-  });
-
   it('sets LOADED state when previously failed source tiles are loaded', function(done) {
     var format = new ol.format.GeoJSON();
     var url = 'spec/ol/data/unavailable.json';
