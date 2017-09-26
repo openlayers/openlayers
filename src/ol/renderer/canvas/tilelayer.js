8033e25663e4be61b309563843b0f674083219e0
Tim Schaub <tim.schaub@gmail.com>
2017-09-25 21:13:00 -0700

    Vector tile rendering requires canvas to be cleared

--- src/ol/renderer/canvas/tilelayer.js
+++ src/ol/renderer/canvas/tilelayer.js
@@ -227,9 +227,7 @@ ol.renderer.canvas.TileLayer.prototype.prepareFrame = function(frameState, layer
         canvas.width = width;
         canvas.height = height;
       } else {
-        if (this.renderedExtent_ && !ol.extent.equals(imageExtent, this.renderedExtent_)) {
-          context.clearRect(0, 0, width, height);
-        }
+        context.clearRect(0, 0, width, height);
         oversampling = this.oversampling_;
       }
     }
