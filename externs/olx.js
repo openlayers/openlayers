260327341c78abe3fdca00a27fb3379b3795f5e0
Alexandre Silva <amsilva@infoportugal.impresa.pt>
2017-11-06 11:10:49 +0000

    Documented the default value.

--- externs/olx.js
+++ externs/olx.js
@@ -2029,7 +2029,8 @@ olx.format.GeoJSONOptions.prototype.geometryName;
  * Certain GeoJSON providers include the geometry_name field in the feature
  * geoJSON. If set to `true` the geoJSON reader will look for that field to
  * set the geometry name. If both this field is set to `true` and a
- * `geometryName` is provided the `geometryName` will take precedence.
+ * `geometryName` is provided, the `geometryName` will take precedence.
+ * Default is `false`.
  * @type {boolean|undefined}
  * @api
  */
