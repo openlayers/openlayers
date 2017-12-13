d04b25403d2a199e947eb828044cb005af7a1607
Tim Schaub <tim@planet.com>
2017-12-12 19:36:35 -0700

    Clean up filters

--- src/ol/format/filter/Comparison.js
+++ src/ol/format/filter/Comparison.js
@@ -2,25 +2,22 @@
  * @module ol/format/filter/Comparison
  */
 import {inherits} from '../../index.js';
-import _ol_format_filter_Filter_ from '../filter/Filter.js';
+import Filter from '../filter/Filter.js';
 
 /**
  * @classdesc
  * Abstract class; normally only used for creating subclasses and not instantiated in apps.
  * Base class for WFS GetFeature property comparison filters.
  *
- * deprecated: This class will no longer be exported starting from the next major version.
- *
  * @constructor
  * @abstract
  * @param {!string} tagName The XML tag name for this filter.
  * @param {!string} propertyName Name of the context property to compare.
  * @extends {ol.format.filter.Filter}
- * @api
  */
-var _ol_format_filter_Comparison_ = function(tagName, propertyName) {
+var Comparison = function(tagName, propertyName) {
 
-  _ol_format_filter_Filter_.call(this, tagName);
+  Filter.call(this, tagName);
 
   /**
    * @public
@@ -29,6 +26,6 @@ var _ol_format_filter_Comparison_ = function(tagName, propertyName) {
   this.propertyName = propertyName;
 };
 
-inherits(_ol_format_filter_Comparison_, _ol_format_filter_Filter_);
+inherits(Comparison, Filter);
 
-export default _ol_format_filter_Comparison_;
+export default Comparison;
