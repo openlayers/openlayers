d9f8667d2beb78f9c0410cc74bfb806d92f4fe2e
Tim Schaub <tim@planet.com>
2017-12-14 12:58:47 -0700

    Rename _ol_control_FullScreen_ to FullScreen

--- test/spec/ol/control/fullscreen.test.js
+++ test/spec/ol/control/fullscreen.test.js
@@ -1,12 +1,12 @@
-import _ol_control_FullScreen_ from '../../../../src/ol/control/FullScreen.js';
+import FullScreen from '../../../../src/ol/control/FullScreen.js';
 
 describe('ol.control.FullScreen', function() {
 
   describe('constructor', function() {
 
     it('can be constructed without arguments', function() {
-      var instance = new _ol_control_FullScreen_();
-      expect(instance).to.be.an(_ol_control_FullScreen_);
+      var instance = new FullScreen();
+      expect(instance).to.be.an(FullScreen);
     });
 
   });
