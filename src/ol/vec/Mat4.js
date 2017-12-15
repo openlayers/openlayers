a32aebfb3a0bcf7b171723cc7a98352a66d07220
Frederic Junod <frederic.junod@camptocamp.com>
2017-12-15 08:43:04 +0100

    Named exports from ol/vec/Mat4

--- src/ol/vec/Mat4.js
+++ src/ol/vec/Mat4.js
@@ -1,15 +1,14 @@
 /**
  * @module ol/vec/Mat4
  */
-var _ol_vec_Mat4_ = {};
 
 
 /**
  * @return {Array.<number>} 4x4 matrix representing a 3D identity transform.
  */
-_ol_vec_Mat4_.create = function() {
+export function create() {
   return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
-};
+}
 
 
 /**
@@ -17,7 +16,7 @@ _ol_vec_Mat4_.create = function() {
  * @param {ol.Transform} transform Transformation matrix.
  * @return {Array.<number>} 2D transformation matrix as flattened 4x4 matrix.
  */
-_ol_vec_Mat4_.fromTransform = function(mat4, transform) {
+export function fromTransform(mat4, transform) {
   mat4[0] = transform[0];
   mat4[1] = transform[1];
   mat4[4] = transform[2];
@@ -25,5 +24,4 @@ _ol_vec_Mat4_.fromTransform = function(mat4, transform) {
   mat4[12] = transform[4];
   mat4[13] = transform[5];
   return mat4;
-};
-export default _ol_vec_Mat4_;
+}
--- test/spec/ol/vec/mat4.test.js
+++ test/spec/ol/vec/mat4.test.js
@@ -1,11 +1,11 @@
-import _ol_vec_Mat4_ from '../../../../src/ol/vec/Mat4.js';
+import {create, fromTransform} from '../../../../src/ol/vec/Mat4.js';
 
 
 describe('ol.vec.Mat4', function() {
 
   describe('ol.vec.Mat4.create()', function() {
     it('returns the expected matrix', function() {
-      expect(_ol_vec_Mat4_.create()).to.eql(
+      expect(create()).to.eql(
           [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
     });
   });
@@ -13,8 +13,8 @@ describe('ol.vec.Mat4', function() {
   describe('ol.vec.Mat4.fromTransform()', function() {
     it('sets the expected transform on the matrix', function() {
       var transform = [1, 2, 3, 4, 5, 6];
-      var result = _ol_vec_Mat4_.create();
-      expect(_ol_vec_Mat4_.fromTransform(result, transform)).to.eql(
+      var result = create();
+      expect(fromTransform(result, transform)).to.eql(
           [1, 2, 0, 0, 3, 4, 0, 0, 0, 0, 1, 0, 5, 6, 0, 1]);
       expect(result).to.eql(
           [1, 2, 0, 0, 3, 4, 0, 0, 0, 0, 1, 0, 5, 6, 0, 1]);
