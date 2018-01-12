dc5b2a4ea0849f38ec5eeb37be38fc9e3b1bac34
Frederic Junod <frederic.junod@camptocamp.com>
2018-01-12 12:14:55 +0100

    Fix for loop in color test

--- test/spec/ol/color.test.js
+++ test/spec/ol/color.test.js
@@ -123,7 +123,7 @@ describe('ol.color', function() {
     it('throws an error on invalid colors', function() {
       const invalidColors = ['tuesday', '#12345', '#1234567', 'rgb(255.0,0,0)'];
       let i, ii;
-      for (i = 0, ii < invalidColors.length; i < ii; ++i) {
+      for (i = 0, ii = invalidColors.length; i < ii; ++i) {
         expect(function() {
           fromString(invalidColors[i]);
         }).to.throwException();
