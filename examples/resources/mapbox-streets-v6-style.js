9417e88c15d65ab38627d86d4164c0bd46c4e59e
Frederic Junod <frederic.junod@camptocamp.com>
2018-01-19 13:25:54 +0100

    Remove extra comment

--- examples/resources/mapbox-streets-v6-style.js
+++ examples/resources/mapbox-streets-v6-style.js
@@ -1,9 +1,6 @@
 // Styles for the mapbox-streets-v6 vector tile data set. Loosely based on
 // http://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6.json
 
-// Styles for the mapbox-streets-v6 vector tile data set. Loosely based on
-// http://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6.json
-
 function createMapboxStreetsV6Style(Style, Fill, Stroke, Icon, Text) {
   var fill = new Fill({color: ''});
   var stroke = new Stroke({color: '', width: 1});
