a05c4b01591ac7dd3a73c15be93b5d3eb197e75d
Andreas Hocevar <andreas.hocevar@gmail.com>
2018-03-07 13:54:43 +0100

    New typedef syntax for TransformFunction

--- src/ol/types.js
+++ src/ol/types.js
@@ -57,3 +57,12 @@
  * @typedef {!Array.<number>} Transform
  */
 
+/**
+ * A transform function accepts an array of input coordinate values, an optional
+ * output array, and an optional dimension (default should be 2).  The function
+ * transforms the input coordinate values, populates the output array, and
+ * returns the output array.
+ *
+ * @typedef {function(Array.<number>, Array.<number>=, number=): Array.<number>} TransformFunction
+ * @api
+ */
