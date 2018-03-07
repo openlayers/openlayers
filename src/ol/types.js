b6781ac186dc5e06577d59839711f83cb677d115
Andreas Hocevar <andreas.hocevar@gmail.com>
2018-03-07 13:52:08 +0100

    New typedef syntax for Pixel

--- src/ol/types.js
+++ src/ol/types.js
@@ -28,6 +28,13 @@
  */
 
 /**
+ * An array with two elements, representing a pixel. The first element is the
+ * x-coordinate, the second the y-coordinate of the pixel.
+ * @typedef {Array.<number>} Pixel
+ * @api
+ */
+
+/**
  * @typedef {function(module:ol/PluggableMap~PluggableMap, ?olx.FrameState): boolean} PostRenderFunction
  */
 
