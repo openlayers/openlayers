813b72251a53c47702369991bb1cd7c6b93d6de7
Frederic Junod <frederic.junod@camptocamp.com>
2018-03-12 15:52:11 +0100

    Mark optional property with square brackets

--- src/ol/control/ZoomSlider.js
+++ src/ol/control/ZoomSlider.js
@@ -28,7 +28,7 @@ const Direction = {
 /**
  * @typedef {Object} Options
  * @property {string|undefined} className CSS class name.
- * @property {number|undefined} duration Animation duration in milliseconds. Default is `200`.
+ * @property {number|undefined} [duration=200] Animation duration in milliseconds.
  * @property {number|undefined} maxResolution Maximum resolution.
  * @property {number|undefined} minResolution Minimum resolution.
  * @property {function(ol.MapEvent)|undefined} render Function called when the control
