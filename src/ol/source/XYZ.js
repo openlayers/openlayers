7aa5e5d80e137a2d197a1280ec1ca6b705f6d335
Frederic Junod <frederic.junod@camptocamp.com>
2018-04-04 15:59:04 +0200

    Fix wrong tilegrid type for ol/source/XYZ options

--- src/ol/source/XYZ.js
+++ src/ol/source/XYZ.js
@@ -19,7 +19,7 @@ import {createXYZ, extentFromProjection} from '../tilegrid.js';
  * Higher values can increase reprojection performance, but decrease precision.
  * @property {number} [maxZoom=18] Optional max zoom level.
  * @property {number} [maxZoom=0] Optional min zoom level.
- * @property {module:ol/tilegrid/WMTS~WMTSTileGrid} tileGrid Tile grid.
+ * @property {module:ol/tilegrid/TileGrid~TileGrid} [tileGrid] Tile grid.
  * @property {ol.TileLoadFunctionType} [tileLoadFunction] Optional function to load a tile given a URL. The default is
  * ```js
  * function(imageTile, src) {
