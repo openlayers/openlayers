223ef348986d7772b320959d90c58976b33ee0fc
Michael Kuenzli <michael.kuenzli@camptocamp.com>
2018-04-18 13:46:39 +0200

    Move ol.CanvasFunctionType to ol/source/ImageCanvas

--- src/ol/typedefs.js
+++ src/ol/typedefs.js
@@ -22,21 +22,6 @@ const ol = {};
 
 
 /**
- * A function returning the canvas element (`{HTMLCanvasElement}`)
- * used by the source as an image. The arguments passed to the function are:
- * {@link module:ol/extent~Extent} the image extent, `{number}` the image resolution,
- * `{number}` the device pixel ratio, {@link module:ol/size~Size} the image size, and
- * {@link module:ol/proj/Projection~Projection} the image projection. The canvas returned by
- * this function is cached by the source. The this keyword inside the function
- * references the {@link ol.source.ImageCanvas}.
- *
- * @typedef {function(this:ol.source.ImageCanvas, module:ol/extent~Extent, number,
- *     number, module:ol/size~Size, module:ol/proj/Projection~Projection): HTMLCanvasElement}
- */
-ol.CanvasFunctionType;
-
-
-/**
  * @typedef {{currentFillStyle: (module:ol/colorlike~ColorLike|undefined),
  *            currentStrokeStyle: (module:ol/colorlike~ColorLike|undefined),
  *            currentLineCap: (string|undefined),
