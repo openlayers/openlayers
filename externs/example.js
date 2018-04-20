6b1a2e884d0ad9b68c7550af0259427daf90d3f3
Tim Schaub <tim.schaub@gmail.com>
2018-04-19 18:08:43 -0600

    Correct types for style options

--- externs/example.js
+++ externs/example.js
@@ -1,8 +1,7 @@
 /**
  * @type {Object}
  */
-var common;
-
+let common;
 
 
 /**
@@ -13,12 +12,12 @@ common.getRendererFromQueryString = function(opt_default) {};
 
 
 /**
- * @param {function(new:ol.style.Style, olx.style.StyleOptions=)} Style Style constructor.
- * @param {function(new:ol.style.Fill, olx.style.FillOptions=)} Fill Fill constructor.
- * @param {function(new:ol.style.Stroke, olx.style.StrokeOptions=)} Stroke Stroke constructor.
- * @param {function(new:ol.style.Icon, olx.style.IconOptions=)} Icon Icon constructor.
- * @param {function(new:ol.style.Text, olx.style.TextOptions=)} Text Text constructor.
+ * @param {function(new:ol.style.Style, module:ol/style/Style~Options=)} Style Style constructor.
+ * @param {function(new:ol.style.Fill, module:ol/style/Fill~Options=)} Fill Fill constructor.
+ * @param {function(new:ol.style.Stroke, module:ol/style/Stroke~Options=)} Stroke Stroke constructor.
+ * @param {function(new:ol.style.Icon, module:ol/style/Icon~Options=)} Icon Icon constructor.
+ * @param {function(new:ol.style.Text, module:ol/style/Text~Options=)} Text Text constructor.
  * @return {function((module:ol/Feature~Feature|ol.render.Feature), number):
  *     Array.<ol.style.Style>}
  */
-var createMapboxStreetsV6Style = function(Style, Fill, Stroke, Icon, Text) {};
+const createMapboxStreetsV6Style = function(Style, Fill, Stroke, Icon, Text) {};
