56fec1909df0f648bacc47e7ad6d932a4a2e6fca
Dustin <dustyh53@hotmail.com>
2018-05-10 11:43:53 -0500

    Updated to a working example service

--- examples/wms-tiled.js
+++ examples/wms-tiled.js
@@ -6,24 +6,23 @@ import TileWMS from '../src/ol/source/TileWMS.js';
 
 
 const layers = [
-  new TileLayer({
-    source: new OSM()
+   new ol.layer.Tile({
+    source: new ol.source.OSM()
   }),
-  new TileLayer({
-    extent: [-13884991, 2870341, -7455066, 6338219],
-    source: new TileWMS({
-      url: 'https://ahocevar.com/geoserver/wms',
-      params: {'LAYERS': 'topp:states', 'TILED': true},
+  new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url: 'https://msdisweb.missouri.edu/arcgis/services/Geological_Geophysical/MO_2016_Inventory_of_Landslide_Occurrences/MapServer/WMSServer?',
+      params: {'LAYERS': '0', 'VERSION': '1.1.0'},
       serverType: 'geoserver',
       // Countries have transparency, so do not fade tiles:
       transition: 0
     })
   })
 ];
-const map = new Map({
+var map = new ol.Map({
   layers: layers,
   target: 'map',
-  view: new View({
+  view: new ol.View({
     center: [-10997148, 4569099],
     zoom: 4
   })
