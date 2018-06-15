a7c6df4d80367a2f6d1a0bf3a6751ca18a6a0d17
ahocevar <andreas.hocevar@gmail.com>
2018-06-15 19:43:10 +0200

    Remove the externs/ directory

--- externs/closure-compiler.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview Definitions for externs that are either missing or incorrect
- * in the current release version of the closure compiler we use.
- *
- * The entries must be removed once they are available/correct in the
- * version we use.
- *
- * @externs
- */
