785bef76402a256d49ebec53c585c511b7853950
ahocevar <andreas.hocevar@gmail.com>
2018-06-21 18:29:20 +0200

    Also re-export non-API classes

--- src/ol/index.js
+++ src/ol/index.js
@@ -2,15 +2,32 @@
  * @module ol
  */
 
+export {default as AssertionError} from './AssertionError.js';
 export {default as Collection} from './Collection.js';
+export {default as Disposable} from './Disposable.js';
 export {default as Feature} from './Feature.js';
 export {default as Geolocation} from './Geolocation.js';
 export {default as Graticule} from './Graticule.js';
+export {default as Image} from './Image.js';
+export {default as ImageBase} from './ImageBase.js';
+export {default as ImageCanvas} from './ImageCanvas.js';
+export {default as ImageTile} from './ImageTile.js';
 export {default as Kinetic} from './Kinetic.js';
 export {default as Map} from './Map.js';
+export {default as MapBrowserEvent} from './MapBrowserEvent.js';
+export {default as MapBrowserEventHandler} from './MapBrowserEventHandler.js';
+export {default as MapBrowserPointerEvent} from './MapBrowserPointerEvent.js';
+export {default as MapEvent} from './MapEvent.js';
+export {default as Object} from './Object.js';
 export {default as Observable} from './Observable.js';
 export {default as Overlay} from './Overlay.js';
 export {default as PluggableMap} from './PluggableMap.js';
+export {default as Tile} from './Tile.js';
+export {default as TileCache} from './TileCache.js';
+export {default as TileQueue} from './TileQueue.js';
+export {default as TileRange} from './TileRange.js';
+export {default as VectorImageTile} from './VectorImageTile.js';
+export {default as VectorTile} from './VectorTile.js';
 export {default as View} from './View.js';
 export {default as WebGLMap} from './WebGLMap.js';
 
