f6046c023c1f48a962993061647b451270e25d82
Tim Schaub <tim.schaub@gmail.com>
2018-07-17 18:30:15 -0600

    Clean up remaining ol/format/filter class

--- src/ol/format/filter/Within.js
+++ src/ol/format/filter/Within.js
@@ -1,30 +1,26 @@
 /**
  * @module ol/format/filter/Within
  */
-import {inherits} from '../../util.js';
 import Spatial from '../filter/Spatial.js';
 
 /**
  * @classdesc
  * Represents a `<Within>` operator to test whether a geometry-valued property
  * is within a given geometry.
- *
- * @constructor
- * @param {!string} geometryName Geometry name to use.
- * @param {!module:ol/geom/Geometry} geometry Geometry.
- * @param {string=} opt_srsName SRS name. No srsName attribute will be
- *    set on geometries when this is not provided.
- * @extends {module:ol/format/filter/Spatial}
- * @api
  */
-class Within {
+class Within extends Spatial {
 
+  /**
+   * @param {!string} geometryName Geometry name to use.
+   * @param {!module:ol/geom/Geometry} geometry Geometry.
+   * @param {string=} opt_srsName SRS name. No srsName attribute will be
+   *    set on geometries when this is not provided.
+   * @api
+   */
   constructor(geometryName, geometry, opt_srsName) {
-    Spatial.call(this, 'Within', geometryName, geometry, opt_srsName);
+    super('Within', geometryName, geometry, opt_srsName);
   }
 
 }
-
-inherits(Within, Spatial);
 
 export default Within;
