b56a7c0fa3d8f3ec6dea5e3552d095cbe09185a0
Tino Desjardins <tino.desjardins@arcor.de>
2018-08-20 17:03:59 +0200

    Update link to base class in docs

--- src/ol/MapBrowserEvent.js
+++ src/ol/MapBrowserEvent.js
@@ -6,7 +6,7 @@ import MapEvent from './MapEvent.js';
 /**
  * @classdesc
  * Events emitted as map browser events are instances of this type.
- * See {@link module:ol/Map~Map} for which events trigger a map browser event.
+ * See {@link module:ol/PluggableMap~PluggableMap} for which events trigger a map browser event.
  */
 class MapBrowserEvent extends MapEvent {
 
