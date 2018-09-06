82675d0bee8c0b6a4232f4c790bb986d754f2ef0
Tim Schaub <tim.schaub@gmail.com>
2018-09-06 09:29:44 -0600

    Mark optional geom constructor args as optional

--- src/ol/geom/MultiLineString.js
+++ src/ol/geom/MultiLineString.js
@@ -27,7 +27,7 @@ class MultiLineString extends SimpleGeometry {
    *     Coordinates or LineString geometries. (For internal use, flat coordinates in
    *     combination with `opt_layout` and `opt_ends` are also accepted.)
    * @param {import("./GeometryLayout.js").default=} opt_layout Layout.
-   * @param {Array<number>} opt_ends Flat coordinate ends for internal use.
+   * @param {Array<number>=} opt_ends Flat coordinate ends for internal use.
    */
   constructor(coordinates, opt_layout, opt_ends) {
 
