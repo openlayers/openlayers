75947a6759d98835e72dc9968c0aec87af2762a3
James Cheese <jamesc@intouch-ltd.com>
2018-09-21 16:21:41 +0100

    Revert changes from 7b687be
    Adapted the changes to new class structures introduced since the original
    deletion before committing, to ensure that everything compiles correctly.

--- test/spec/ol/control/zoomslider.test.js
+++ test/spec/ol/control/zoomslider.test.js
@@ -119,6 +119,7 @@ describe('ol.control.ZoomSlider', function() {
       dragger.dispatchEvent(event);
       expect(control.currentResolution_).to.be(16);
       expect(control.dragging_).to.be(true);
+      expect(control.dragListenerKeys_.length).to.be(6);
       event.type = 'pointermove';
       event.clientX = 6 * control.widthLimit_ / 8;
       event.clientY = 0;
@@ -131,6 +132,7 @@ describe('ol.control.ZoomSlider', function() {
       event.type = 'pointerup';
       dragger.dispatchEvent(event);
       expect(control.currentResolution_).to.be(1);
+      expect(control.dragListenerKeys_.length).to.be(0);
       expect(control.dragging_).to.be(false);
     });
     it('[vertical] handles a drag sequence', function() {
@@ -151,6 +153,7 @@ describe('ol.control.ZoomSlider', function() {
       dragger.dispatchEvent(event);
       expect(control.currentResolution_).to.be(0.0625);
       expect(control.dragging_).to.be(true);
+      expect(control.dragListenerKeys_.length).to.be(6);
       event.type = 'pointermove';
       event.clientX = 0;
       event.clientY = 2 * control.heightLimit_ / 8;
@@ -163,6 +166,7 @@ describe('ol.control.ZoomSlider', function() {
       event.type = 'pointerup';
       dragger.dispatchEvent(event);
       expect(control.currentResolution_).to.be(1);
+      expect(control.dragListenerKeys_.length).to.be(0);
       expect(control.dragging_).to.be(false);
     });
   });
