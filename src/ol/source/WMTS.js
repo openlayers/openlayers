43b9f70077abb8095abf1c74f94896cdf30b6b23
Roman Zoller <roman.zoller@camptocamp.com>
2018-10-15 13:29:19 +0200

    Clarify format option for ol/source/WMTS

--- src/ol/source/WMTS.js
+++ src/ol/source/WMTS.js
@@ -32,7 +32,7 @@ import {appendParams} from '../uri.js';
  * For example, if the tile service advertizes 256px by 256px tiles but actually sends 512px
  * by 512px images (for retina/hidpi devices) then `tilePixelRatio`
  * should be set to `2`.
- * @property {string} [format='image/jpeg'] Image format.
+ * @property {string} [format='image/jpeg'] Image format. Only used when `requestEncoding` is `'KVP'`.
  * @property {string} [version='1.0.0'] WMTS version.
  * @property {string} matrixSet Matrix set.
  * @property {!Object} [dimensions] Additional "dimensions" for tile requests.
