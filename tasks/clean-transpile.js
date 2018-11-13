2ea194262fddc9b8f5ee4430675c61384814d049
Filip Jirsák <filip@jirsak.org>
2018-11-13 16:07:52 +0100

    Don't create directory if it already exists.

--- tasks/clean-transpile.js
+++ tasks/clean-transpile.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
 const fse = require('fs-extra');
 const path = require('path');
 
 const buildDir = path.resolve(__dirname, '../build');
 fse.removeSync(path.join(buildDir, 'ol'));
-fse.mkdir(buildDir);
+if (!fs.existsSync(buildDir)) {
+  fse.mkdir(buildDir);
+}
