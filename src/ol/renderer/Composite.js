98a5b1256244be498707f339c27a7281469c70f2
Frederic Junod <frederic.junod@camptocamp.com>
2018-11-13 11:22:21 +0100

    Re-use style local variable

--- src/ol/renderer/Composite.js
+++ src/ol/renderer/Composite.js
@@ -36,8 +36,8 @@ class CompositeMapRenderer extends MapRenderer {
     style.justifyContent = 'center';
     style.width = '100%';
     style.height = '100%';
+    style.position = 'relative';
 
-    this.element_.style.position = 'relative';
     this.element_.className = CLASS_UNSELECTABLE;
 
     const container = map.getViewport();
