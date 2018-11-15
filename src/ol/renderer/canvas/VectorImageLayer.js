ed7825e13a256117b62da0ac527322a2c51cfe87
Tim Schaub <tim.schaub@gmail.com>
2018-11-15 10:27:31 +0100

    Render vector images

--- src/ol/renderer/canvas/VectorImageLayer.js
+++ src/ol/renderer/canvas/VectorImageLayer.js
@@ -77,7 +77,7 @@ class CanvasVectorImageLayerRenderer extends CanvasImageLayerRenderer {
               !equals(skippedFeatures, newSkippedFeatures))) {
           context.canvas.width = imageFrameState.size[0] * pixelRatio;
           context.canvas.height = imageFrameState.size[1] * pixelRatio;
-          vectorRenderer.compose(context, imageFrameState, layerState);
+          vectorRenderer.render(imageFrameState, layerState);
           skippedFeatures = newSkippedFeatures;
           callback();
         }
