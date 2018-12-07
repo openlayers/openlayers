b3520f821712909c943fafd8cdc94254ccee432b
Frederic Junod <frederic.junod@camptocamp.com>
2018-12-07 13:13:39 +0100

    Remove CANVAS_LINE_DASH from ol/has
    Test if `context.setLineDash` is defined instead.

--- src/ol/has.js
+++ src/ol/has.js
@@ -41,21 +41,6 @@ export const DEVICE_PIXEL_RATIO = window.devicePixelRatio || 1;
 
 
 /**
- * True if the browser's Canvas implementation implements {get,set}LineDash.
- * @type {boolean}
- */
-export const CANVAS_LINE_DASH = function() {
-  let has = false;
-  try {
-    has = !!document.createElement('canvas').getContext('2d').setLineDash;
-  } catch (e) {
-    // pass
-  }
-  return has;
-}();
-
-
-/**
  * Is HTML5 geolocation supported in the current browser?
  * @const
  * @type {boolean}
