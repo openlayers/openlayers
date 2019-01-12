4d54ad0dafa35b9ac19f38fddb28d1d6ca7cda4f
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2019-01-12 13:44:17 +0100

    Update Zoomify.js

--- src/ol/source/Zoomify.js
+++ src/ol/source/Zoomify.js
@@ -251,6 +251,10 @@ class Zoomify extends TileImage {
       tileUrlFunction: tileUrlFunction,
       transition: options.transition
     });
+    
+    console.log("ollll1",this.tilePixelRatio_)
+    console.log("ollll2",this._tilePixelRatio)
+    console.log("ollll3",this.tilePixelRatio)
 
   }
 
