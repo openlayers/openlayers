4f487c4df1b392677ca4d2caa267f0258d9dcbf2
ahocevar <andreas.hocevar@gmail.com>
2019-01-25 01:29:05 +0100

    Update comment

--- src/ol/renderer/canvas/VectorTileLayer.js
+++ src/ol/renderer/canvas/VectorTileLayer.js
@@ -479,8 +479,7 @@ class CanvasVectorTileLayerRenderer extends CanvasTileLayerRenderer {
       for (let t = 0, tt = executorGroups.length; t < tt; ++t) {
         const executorGroup = executorGroups[t];
         if (!executorGroup.hasExecutors(replayTypes)) {
-          // sourceTile was not yet loaded when this.createReplayGroup_() was
-          // called, or it has no replays of the types we want to render
+          // sourceTile has no instructions of the types we want to render
           continue;
         }
         const currentZ = tile.tileCoord[0];
