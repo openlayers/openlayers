2bf217207e37a82f4547dd9522db72f087b69744
Lutz Helm <helm@ub.uni-leipzig.de>
2019-03-21 11:51:42 +0100

    Test renderer behavior for Zoomify zDirection
    The zDirection addition to the Zoomify tile source only takes effect through
    the renderer, so these two renderer cases are added to test the behavior.
    
    zoomify-no-zdirection expects the default behavior.
    zoomify-zdirection expects the tiles for the next better resolution.

---
+++ rendering/cases/zoomify-zdirection/main.js
@@ -0,0 +1,26 @@
+import Map from '../../../src/ol/Map.js';
+import View from '../../../src/ol/View.js';
+import TileLayer from '../../../src/ol/layer/Tile.js';
+import Zoomify from '../../../src/ol/source/Zoomify.js';
+
+const layer = new TileLayer({
+  source: new Zoomify({
+    url: '/data/tiles/zoomify/',
+    size: [200, 200],
+    tileSize: 100,
+    zDirection: -1
+  })
+});
+
+new Map({
+  layers: [layer],
+  target: 'map',
+  view: new View({
+    resolutions: [2, 1],
+    extent: [0, -200, 200, 0],
+    center: [100, -100],
+    zoom: 0.4
+  })
+});
+
+render();
