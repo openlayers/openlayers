6b1bce2b9d50ff194739c240a363f49d8b3fd088
Frederic Junod <frederic.junod@camptocamp.com>
2019-05-07 14:50:39 +0200

    Add missing className property to BaseTile layer options

--- src/ol/layer/BaseTile.js
+++ src/ol/layer/BaseTile.js
@@ -8,6 +8,7 @@ import {assign} from '../obj.js';
 
 /**
  * @typedef {Object} Options
+ * @property {string} [className='ol-layer'] A CSS class name to set to the layer element.
  * @property {number} [opacity=1] Opacity (0, 1).
  * @property {boolean} [visible=true] Visibility.
  * @property {import("../extent.js").Extent} [extent] The bounding extent for layer rendering.  The layer will not be
