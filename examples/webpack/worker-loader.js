0d489f2ea9ed962b848fc3263dd50ddd19b40fad
Tim Schaub <tim.schaub@gmail.com>
2019-05-16 10:36:23 -0600

    Minify worker in examples for a production build

--- examples/webpack/worker-loader.js
+++ examples/webpack/worker-loader.js
@@ -3,7 +3,15 @@ const build = require('../../tasks/serialize-workers').build;
 function loader() {
   const callback = this.async();
 
-  build(this.resource, {minify: false})
+  let minify = false;
+
+  // TODO: remove when https://github.com/webpack/webpack/issues/6496 is addressed
+  const compilation = this._compilation;
+  if (compilation) {
+    minify = compilation.compiler.options.mode === 'production';
+  }
+
+  build(this.resource, {minify})
     .then(chunk => {
       for (const filePath in chunk.modules) {
         this.addDependency(filePath);
