c0339f167b8cf9f05f1077b9160a8a5bade72234
Tim Schaub <tim.schaub@gmail.com>
2019-05-20 10:33:59 -0600

    Use mode from the loader context
    See https://github.com/webpack/webpack/pull/9140.

--- examples/webpack/worker-loader.js
+++ examples/webpack/worker-loader.js
@@ -2,14 +2,7 @@ const build = require('../../tasks/serialize-workers').build;
 
 function loader() {
   const callback = this.async();
-
-  let minify = false;
-
-  // TODO: remove when https://github.com/webpack/webpack/issues/6496 is addressed
-  const compilation = this._compilation;
-  if (compilation) {
-    minify = compilation.compiler.options.mode === 'production';
-  }
+  const minify = this.mode === 'production';
 
   build(this.resource, {minify})
     .then(chunk => {
