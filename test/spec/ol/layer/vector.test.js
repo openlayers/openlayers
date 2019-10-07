f477fc18f2d956cabfca556a5b99c9afbe22c3e9
Andreas Hocevar <andreas.hocevar@gmail.com>
2019-10-08 00:35:13 +0200

    Add tests

--- test/spec/ol/layer/vector.test.js
+++ test/spec/ol/layer/vector.test.js
@@ -2,6 +2,10 @@ import Layer from '../../../../src/ol/layer/Layer.js';
 import VectorLayer from '../../../../src/ol/layer/Vector.js';
 import VectorSource from '../../../../src/ol/source/Vector.js';
 import Style, {createDefaultStyle} from '../../../../src/ol/style/Style.js';
+import Feature from '../../../../src/ol/Feature.js';
+import Point from '../../../../src/ol/geom/Point.js';
+import Map from '../../../../src/ol/Map.js';
+import View from '../../../../src/ol/View.js';
 
 
 describe('ol.layer.Vector', function() {
@@ -119,6 +123,57 @@ describe('ol.layer.Vector', function() {
       layer.setStyle(styleFunction);
       expect(layer.getStyle()).to.be(styleFunction);
 
+    });
+
+  });
+
+  describe('#getFeatures()', function() {
+
+    let map, layer;
+
+    beforeEach(function() {
+      layer = new VectorLayer({
+        source: new VectorSource({
+          features: [
+            new Feature({
+              geometry: new Point([-1000000, 0]),
+              name: 'feature1'
+            }),
+            new Feature({
+              geometry: new Point([1000000, 0]),
+              name: 'feture2'
+            })
+          ]
+        })
+      });
+      const container = document.createElement('div');
+      container.style.width = '256px';
+      container.style.height = '256px';
+      document.body.appendChild(container);
+      map = new Map({
+        target: container,
+        layers: [
+          layer
+        ],
+        view: new View({
+          zoom: 2,
+          center: [0, 0]
+        })
+      });
+    });
+
+    afterEach(function() {
+      document.body.removeChild(map.getTargetElement());
+      map.setTarget(null);
+    });
+
+    it('detects features properly', function(done) {
+      map.renderSync();
+      const pixel = map.getPixelFromCoordinate([-1000000, 0]);
+      layer.getFeatures(pixel).then(function(features) {
+        expect(features[0].get('name')).to.be('feature1');
+        done();
+      });
     });
 
   });
