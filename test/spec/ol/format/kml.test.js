7df9d2a67796106ddb9f5f2592dfd2144706ce37
mike-000 <49240900+mike-000@users.noreply.github.com>
2019-10-23 19:51:39 +0100

    Experimental IconStyle test
    This adds an IconStyle test from #10119 to determine if there is an existing issue.  It is unlikely to need merging.

--- test/spec/ol/format/kml.test.js
+++ test/spec/ol/format/kml.test.js
@@ -2552,6 +2552,52 @@ describe('ol.format.KML', function() {
           expect(s.getFill().getColor()).to.eql([0, 0, 0, 0]);
         });
 
+        it('can read a normal IconStyle', function() {
+          const text =
+              '<kml xmlns="http://earth.google.com/kml/2.2">' +
+              '  <Document>' +
+              '    <Placemark id="a">' +
+              '      <StyleMap>' +
+              '        <Pair>' +
+              '          <key>normal</key>' +
+              '          <Style>' +
+              '            <IconStyle>' +
+              '              <Icon>' +
+              '                <href>http://foo.png</href>' +
+              '              </Icon>' +
+              '            </IconStyle>' +
+              '          </Style>' +
+              '        </Pair>' +
+              '      </StyleMap>' +
+              '    </Placemark>' +
+              '  </Document>' +
+              '</kml>';
+          const fs = format.readFeatures(text);
+          expect(fs).to.have.length(1);
+          const f = fs[0];
+          expect(f).to.be.an(Feature);
+          const styleFunction = f.getStyleFunction();
+          expect(styleFunction).not.to.be(undefined);
+          const styleArray = styleFunction(f, 0);
+          expect(styleArray).to.be.an(Array);
+          expect(styleArray).to.have.length(1);
+          const style = styleArray[0];
+          expect(style).to.be.an(Style);
+          expect(style.getFill()).to.be(getDefaultFillStyle());
+          expect(style.getStroke()).to.be(getDefaultStrokeStyle());
+          const imageStyle = style.getImage();
+          expect(imageStyle).to.be.an(Icon);
+          expect(new URL(imageStyle.getSrc()).href).to.eql(new URL('http://foo.png').href);
+          expect(imageStyle.getAnchor()).to.be(null);
+          expect(imageStyle.getOrigin()).to.be(null);
+          expect(imageStyle.getRotation()).to.eql(0);
+          expect(imageStyle.getSize()).to.be(null);
+          expect(imageStyle.getScale()).to.be(1);
+          expect(imageStyle.getImage().crossOrigin).to.eql('anonymous');
+          expect(style.getText()).to.be(getDefaultTextStyle());
+          expect(style.getZIndex()).to.be(undefined);
+        });
+
         it('ignores highlight styles', function() {
           const text =
               '<kml xmlns="http://earth.google.com/kml/2.2">' +
