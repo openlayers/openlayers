7292ebb038365837c37986abac89c74bc998bb85
Michael Stenta <mike@mstenta.net>
2019-11-09 16:06:55 -0500

    Fix longitudes that wrap around the international date line when converting from EPSG:3857 to EPSG:4326 #3899

--- src/ol/proj/epsg3857.js
+++ src/ol/proj/epsg3857.js
@@ -140,6 +140,14 @@ export function toEPSG4326(input, opt_output, opt_dimension) {
   }
   for (let i = 0; i < length; i += dimension) {
     output[i] = 180 * input[i] / HALF_SIZE;
+    // fix longitudes that wrap around the international date line.
+    output[i] %= 360;
+    if (output[i] < -180) {
+      output[i] += 360;
+    }
+    if (output[i] > 180) {
+      output[i] -= 360;
+    }
     output[i + 1] = 360 * Math.atan(
       Math.exp(input[i + 1] / RADIUS)) / Math.PI - 90;
   }
