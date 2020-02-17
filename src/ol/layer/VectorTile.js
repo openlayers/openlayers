4cf093ab757b1be6c1336555b72c035b543f2059
mike-000 <49240900+mike-000@users.noreply.github.com>
2020-02-17 16:58:46 +0000

    add minZoom and maxZoom options

--- src/ol/layer/VectorTile.js
+++ src/ol/layer/VectorTile.js
@@ -24,6 +24,10 @@ import {assign} from '../obj.js';
  * visible.
  * @property {number} [maxResolution] The maximum resolution (exclusive) below which this layer will
  * be visible.
+ * @property {number} [minZoom] The minimum view zoom level (exclusive) above which this layer will be
+ * visible.
+ * @property {number} [maxZoom] The maximum view zoom level (inclusive) at which this layer will
+ * be visible.
  * @property {import("../render.js").OrderFunction} [renderOrder] Render order. Function to be used when sorting
  * features before rendering. By default features are drawn in the order that they are created. Use
  * `null` to avoid the sort, but get an undefined draw order.
