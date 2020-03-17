f80c175263be43b0e4c8110ae090701d42d1d039
Andreas Hocevar <andreas.hocevar@gmail.com>
2020-03-17 16:43:50 +0100

    Do not transform rotated views

--- examples/offscreen-canvas-tiles.js
+++ examples/offscreen-canvas-tiles.js
@@ -63,12 +63,15 @@ function updateContainerTransform() {
     const renderedCenter = renderedViewState.center;
     const renderedResolution = renderedViewState.resolution;
     const renderedRotation = renderedViewState.rotation;
-    const transform = compose(create(),
-      (renderedCenter[0] - center[0]) / resolution,
-      (center[1] - renderedCenter[1]) / resolution,
-      renderedResolution / resolution, renderedResolution / resolution,
-      rotation - renderedRotation,
-      0, 0);
+    const transform = create();
+    if (!rotation) {
+      compose(transform,
+        (renderedCenter[0] - center[0]) / resolution,
+        (center[1] - renderedCenter[1]) / resolution,
+        renderedResolution / resolution, renderedResolution / resolution,
+        rotation - renderedRotation,
+        0, 0);
+    }
     transformContainer.style.transform = createTransformString(transform);
   }
 
