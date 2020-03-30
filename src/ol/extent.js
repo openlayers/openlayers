660845f5b85a47c4546abe8c2cf54d2be95be912
mike-000 <49240900+mike-000@users.noreply.github.com>
2020-03-30 23:17:52 +0100

    Include center at right edge in calculations

--- src/ol/extent.js
+++ src/ol/extent.js
@@ -826,7 +826,7 @@ export function applyTransform(extent, transformFn, opt_extent, opt_stops) {
 export function wrapX(extent, projection) {
   const projectionExtent = projection.getExtent();
   const center = getCenter(extent);
-  if (projection.canWrapX() && (center[0] < projectionExtent[0] || center[0] > projectionExtent[2])) {
+  if (projection.canWrapX() && (center[0] < projectionExtent[0] || center[0] >= projectionExtent[2])) {
     const worldWidth = getWidth(projectionExtent);
     const worldsAway = Math.floor((center[0] - projectionExtent[0]) / worldWidth);
     const offset = (worldsAway * worldWidth);
