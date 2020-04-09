aad24d159241ae97bcfb1d245e25b02f78e5526b
user <pro.cyril.antolini@protonmail.com>
2020-04-09 18:21:45 +0300

    Dedicated module for geolocation event type

---
+++ src/ol/GeolocationEventType.js
@@ -0,0 +1,15 @@
+/**
+ * @module ol/GeolocationEventType
+ */
+
+/**
+ * @enum {string}
+ */
+export default {
+  /**
+   * Triggered when an error occurs in geolocation.
+   * @event module:ol/Geolocation~GeolocationError#error
+   * @api
+   */
+  ERROR: 'error',
+};
