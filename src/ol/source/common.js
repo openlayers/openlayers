bf77f596569f8f84112741cb389915fd9290e332
mike-000 <49240900+mike-000@users.noreply.github.com>
2020-04-30 21:33:00 +0100

    revert change to IMAGE_SMOOTHING_DISABLED

--- src/ol/source/common.js
+++ src/ol/source/common.js
@@ -1,7 +1,6 @@
 /**
  * @module ol/source/common
  */
-import {FIREFOX, SAFARI, WEBKIT} from '../has.js';
 
 /**
  * Default WMS version.
@@ -13,10 +12,7 @@ export const DEFAULT_WMS_VERSION = '1.3.0';
  * Context options to disable image smoothing.
  * @type {Object}
  */
-export const IMAGE_SMOOTHING_DISABLED =
-  FIREFOX || SAFARI || WEBKIT
-    ? {imageSmoothingEnabled: false}
-    : {
-        imageSmoothingEnabled: false,
-        msImageSmoothingEnabled: false,
-      };
+export const IMAGE_SMOOTHING_DISABLED = {
+  imageSmoothingEnabled: false,
+  msImageSmoothingEnabled: false,
+};
