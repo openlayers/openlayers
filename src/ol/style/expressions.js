8e6d7d8b0f57e34df1a646a54b1ae483a7b7f1d3
Michael Jurkoic <mjjurkoic@gmail.com>
2020-12-16 10:52:23 -0800

    Added tests
    Made some small changes in expressions.js and the example based on code review feedback.
    
    Added unit tests in expressions.test.js to test the 'in' expression parsing.

--- src/ol/style/expressions.js
+++ src/ol/style/expressions.js
@@ -803,7 +803,6 @@ Operators['in'] = {
       assertNumbers(input);
       let i = 0;
       let inputArr = ['array'];
-      // @ts-ignore
       input.forEach((element) => {
         inputArr.push(element);
         i++;
@@ -831,13 +830,14 @@ Operators['in'] = {
       const input = args[1];
       assertString(input);
       assertString(keyword);
-      // @ts-ignore
       result = input.includes(keyword).toString();
     } else {
       throw new Error(
         `Expected input to be an array of numbers or a string, got ${typeof args[1]} instead.`
       );
     }
+
+    console.log(result);
 
     return `(${result})`;
   },
