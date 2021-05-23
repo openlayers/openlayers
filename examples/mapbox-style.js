f6f731029fa2b0a7d3f2a1b4675ca2cc7a29d2c6
mike-000 <49240900+mike-000@users.noreply.github.com>
2021-05-23 11:02:44 +0100

    fix confusing import

--- examples/mapbox-style.js
+++ examples/mapbox-style.js
@@ -1,7 +1,7 @@
 import FullScreen from '../src/ol/control/FullScreen.js';
-import apply from 'ol-mapbox-style';
+import olms from 'ol-mapbox-style';
 
-apply(
+olms(
   'map',
   'https://api.maptiler.com/maps/topo/style.json?key=get_your_own_D6rA4zTHduk6KOKTXzGB'
 ).then(function (map) {
