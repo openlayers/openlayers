20d1b907bd1b6807416e7dc7a2be132e987a73e9
Simon Seyock <seyock@terrestris.de>
2021-07-02 10:53:01 +0200

    Fix documentation for GML class.

--- src/ol/format/GML.js
+++ src/ol/format/GML.js
@@ -4,11 +4,11 @@
 import GML3 from './GML3.js';
 
 /**
- * @classdesc
  * Feature format for reading and writing data in the GML format
  * version 3.1.1.
  * Currently only supports GML 3.1.1 Simple Features profile.
  *
+ * @class
  * @param {import("./GMLBase.js").Options} [opt_options]
  *     Optional configuration object.
  * @api
