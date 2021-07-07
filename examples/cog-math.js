429a8fbc1a547ed1edcfd3b2e0b84e9d2a55dfd4
Andreas Hocevar <andreas.hocevar@gmail.com>
2021-07-07 15:32:51 +0200

    Remove lint after dependency updates

--- examples/cog-math.js
+++ examples/cog-math.js
@@ -79,15 +79,13 @@ const map = new Map({
         sources: [
           {
             // visible red, band 1 in the style expression above
-            url:
-              'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/2020/S2A_36QWD_20200701_0_L2A/B04.tif',
+            url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/2020/S2A_36QWD_20200701_0_L2A/B04.tif',
             nodata: 0,
             max: 10000,
           },
           {
             // near infrared, band 2 in the style expression above
-            url:
-              'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/2020/S2A_36QWD_20200701_0_L2A/B08.tif',
+            url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/2020/S2A_36QWD_20200701_0_L2A/B08.tif',
             nodata: 0,
             max: 10000,
           },
--- examples/cog.js
+++ examples/cog.js
@@ -25,8 +25,7 @@ const map = new Map({
       source: new GeoTIFF({
         sources: [
           {
-            url:
-              'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/2020/S2A_36QWD_20200701_0_L2A/TCI.tif',
+            url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/2020/S2A_36QWD_20200701_0_L2A/TCI.tif',
             nodata: 0,
           },
         ],
