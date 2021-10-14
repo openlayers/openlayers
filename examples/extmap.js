7a74ba606a0250725e92339a9d74eb8da3b0b4fa
andrewcoder002 <92372311+andrewcoder002@users.noreply.github.com>
2021-10-14 10:05:46 +0200

    Test case of new functionality
    Test case of new functionality that enables to switch map to another browser window and back

---
+++ examples/extmap.js
@@ -0,0 +1,13 @@
+      var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([37.41, 8.82]),
+          zoom: 4
+        })
+      });
+    
