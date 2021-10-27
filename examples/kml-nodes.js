00a6c13d3e9d5bbd2bbb4ea8bcc9e1a8b13eec41
softarius <sergey@yushinin.ru>
2021-10-27 20:21:18 +0300

    Imports are sorted

--- examples/kml-nodes.js
+++ examples/kml-nodes.js
@@ -1,12 +1,12 @@
-import Map from '../src/ol/Map.js';
-import View from '../src/ol/View.js';
-import TileLayer from '../src/ol/layer/Tile.js';
-import OSM from '../src/ol/source/OSM.js';
-import { fromLonLat } from '../src/ol/proj.js';
 import KML from '../src/ol/format/KML.js';
-import VectorSource from '../src/ol/source/Vector.js';
 import { Vector as VectorLayer } from '../src/ol/layer.js';
+import TileLayer from '../src/ol/layer/Tile.js';
+import Map from '../src/ol/Map.js';
 import { unByKey } from '../src/ol/Observable.js';
+import { fromLonLat } from '../src/ol/proj.js';
+import OSM from '../src/ol/source/OSM.js';
+import VectorSource from '../src/ol/source/Vector.js';
+import View from '../src/ol/View.js';
 
 const kml = new VectorSource({
   url: 'data/kml/KrasnayaPolyanaResort.kml',
@@ -34,9 +34,9 @@ const map = new Map({
  * Autoextent
  */
 var listenerKey = kml.on('change', function (e) {
-   if (e.target.getState() == 'ready') {
-     map.getView().fit(e.target.getExtent(), map.getSize()); 
-    unByKey(listenerKey); 
+  if (e.target.getState() == 'ready') {
+    map.getView().fit(e.target.getExtent(), map.getSize());
+    unByKey(listenerKey);
   }
 });
 
