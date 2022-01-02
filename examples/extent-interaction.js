90248678935966d75d74eecb3db9e031b6fef2e6
Tim Schaub <tim.schaub@gmail.com>
2022-01-01 17:25:15 -0700

    Update extent interaction example

--- examples/extent-interaction.js
+++ examples/extent-interaction.js
@@ -1,23 +1,14 @@
 import ExtentInteraction from '../src/ol/interaction/Extent.js';
-import GeoJSON from '../src/ol/format/GeoJSON.js';
 import Map from '../src/ol/Map.js';
+import OSM from '../src/ol/source/OSM.js';
+import TileLayer from '../src/ol/layer/Tile.js';
 import View from '../src/ol/View.js';
-import {OSM, Vector as VectorSource} from '../src/ol/source.js';
-import {Tile as TileLayer, Vector as VectorLayer} from '../src/ol/layer.js';
 import {shiftKeyOnly} from '../src/ol/events/condition.js';
-
-const vectorSource = new VectorSource({
-  url: 'data/geojson/countries.geojson',
-  format: new GeoJSON(),
-});
 
 const map = new Map({
   layers: [
     new TileLayer({
       source: new OSM(),
-    }),
-    new VectorLayer({
-      source: vectorSource,
     }),
   ],
   target: 'map',
