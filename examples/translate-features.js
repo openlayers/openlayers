197dd2e06d36bbe3c5ca816a5661f2efec6856a4
Tim Schaub <tim.schaub@gmail.com>
2022-01-07 13:53:01 -0700

    Update translate features example

--- examples/translate-features.js
+++ examples/translate-features.js
@@ -1,6 +1,6 @@
 import GeoJSON from '../src/ol/format/GeoJSON.js';
 import Map from '../src/ol/Map.js';
-import OSM from '../src/ol/source/OSM.js';
+import VectorLayer from '../src/ol/layer/Vector.js';
 import VectorSource from '../src/ol/source/Vector.js';
 import View from '../src/ol/View.js';
 import {
@@ -8,15 +8,12 @@ import {
   Translate,
   defaults as defaultInteractions,
 } from '../src/ol/interaction.js';
-import {Tile as TileLayer, Vector as VectorLayer} from '../src/ol/layer.js';
-
-const raster = new TileLayer({
-  source: new OSM(),
-});
+import {fromLonLat} from '../src/ol/proj.js';
 
 const vector = new VectorLayer({
+  background: 'white',
   source: new VectorSource({
-    url: 'data/geojson/countries.geojson',
+    url: 'https://openlayers.org/data/vector/us-states.json',
     format: new GeoJSON(),
   }),
 });
@@ -29,10 +26,10 @@ const translate = new Translate({
 
 const map = new Map({
   interactions: defaultInteractions().extend([select, translate]),
-  layers: [raster, vector],
+  layers: [vector],
   target: 'map',
   view: new View({
-    center: [0, 0],
-    zoom: 2,
+    center: fromLonLat([-100, 38.5]),
+    zoom: 4,
   }),
 });
