ab6e05dd5c8333eecaa9552415636d7298e8d0c6
mike-000 <49240900+mike-000@users.noreply.github.com>
2022-01-08 18:52:41 +0000

    Use getCompositeCanvas method

--- examples/export-map.js
+++ examples/export-map.js
@@ -52,52 +52,7 @@ const map = new Map({
 
 document.getElementById('export-png').addEventListener('click', function () {
   map.once('rendercomplete', function () {
-    const mapCanvas = document.createElement('canvas');
-    const size = map.getSize();
-    mapCanvas.width = size[0];
-    mapCanvas.height = size[1];
-    const mapContext = mapCanvas.getContext('2d');
-    Array.prototype.forEach.call(
-      map.getViewport().querySelectorAll('.ol-layer canvas, canvas.ol-layer'),
-      function (canvas) {
-        if (canvas.width > 0) {
-          const opacity =
-            canvas.parentNode.style.opacity || canvas.style.opacity;
-          mapContext.globalAlpha = opacity === '' ? 1 : Number(opacity);
-
-          const backgroundColor = canvas.parentNode.style.backgroundColor;
-          if (backgroundColor) {
-            mapContext.fillStyle = backgroundColor;
-            mapContext.fillRect(0, 0, canvas.width, canvas.height);
-          }
-
-          let matrix;
-          const transform = canvas.style.transform;
-          if (transform) {
-            // Get the transform parameters from the style's transform matrix
-            matrix = transform
-              .match(/^matrix\(([^\(]*)\)$/)[1]
-              .split(',')
-              .map(Number);
-          } else {
-            matrix = [
-              parseFloat(canvas.style.width) / canvas.width,
-              0,
-              0,
-              parseFloat(canvas.style.height) / canvas.height,
-              0,
-              0,
-            ];
-          }
-          // Apply the transform to the export map context
-          CanvasRenderingContext2D.prototype.setTransform.apply(
-            mapContext,
-            matrix
-          );
-          mapContext.drawImage(canvas, 0, 0);
-        }
-      }
-    );
+    const mapCanvas = map.getCompositeCanvas();
     if (navigator.msSaveBlob) {
       // link download attribute does not work on MS browsers
       navigator.msSaveBlob(mapCanvas.msToBlob(), 'map.png');
