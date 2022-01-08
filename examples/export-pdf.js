ffcc6d3180b16f3210330281e550c28308551213
mike-000 <49240900+mike-000@users.noreply.github.com>
2022-01-08 18:56:31 +0000

    Use getCompositeCanvas method

--- examples/export-pdf.js
+++ examples/export-pdf.js
@@ -58,31 +58,7 @@ exportButton.addEventListener(
     const viewResolution = map.getView().getResolution();
 
     map.once('rendercomplete', function () {
-      const mapCanvas = document.createElement('canvas');
-      mapCanvas.width = width;
-      mapCanvas.height = height;
-      const mapContext = mapCanvas.getContext('2d');
-      Array.prototype.forEach.call(
-        document.querySelectorAll('.ol-layer canvas'),
-        function (canvas) {
-          if (canvas.width > 0) {
-            const opacity = canvas.parentNode.style.opacity;
-            mapContext.globalAlpha = opacity === '' ? 1 : Number(opacity);
-            const transform = canvas.style.transform;
-            // Get the transform parameters from the style's transform matrix
-            const matrix = transform
-              .match(/^matrix\(([^\(]*)\)$/)[1]
-              .split(',')
-              .map(Number);
-            // Apply the transform to the export map context
-            CanvasRenderingContext2D.prototype.setTransform.apply(
-              mapContext,
-              matrix
-            );
-            mapContext.drawImage(canvas, 0, 0);
-          }
-        }
-      );
+      const mapCanvas = map.getCompositeCanvas();
       const pdf = new jspdf.jsPDF('landscape', undefined, format);
       pdf.addImage(
         mapCanvas.toDataURL('image/jpeg'),
