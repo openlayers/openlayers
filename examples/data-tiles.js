46afa50f99402d72bf22e27b0f628466018a5b4d
mike-000 <49240900+mike-000@users.noreply.github.com>
2022-01-16 00:01:21 +0000

    Support hidpi tiles

--- examples/data-tiles.js
+++ examples/data-tiles.js
@@ -2,18 +2,22 @@ import DataTile from '../src/ol/source/DataTile.js';
 import Map from '../src/ol/Map.js';
 import TileLayer from '../src/ol/layer/WebGLTile.js';
 import View from '../src/ol/View.js';
+import {DEVICE_PIXEL_RATIO} from '../src/ol/has.js';
 
-const size = 256;
+const hiDPI = DEVICE_PIXEL_RATIO >= 1.5;
+const tilePixelRatio = hiDPI ? 2 : 1;
 
+const size = 256 * tilePixelRatio;
 const canvas = document.createElement('canvas');
 canvas.width = size;
 canvas.height = size;
 
 const context = canvas.getContext('2d');
 context.strokeStyle = 'white';
+context.lineWidth = tilePixelRatio;
 context.textAlign = 'center';
-context.font = '24px sans-serif';
-const lineHeight = 30;
+context.font = 24 * tilePixelRatio + 'px sans-serif';
+const lineHeight = 30 * tilePixelRatio;
 
 const map = new Map({
   target: 'map',
@@ -25,7 +29,7 @@ const map = new Map({
           context.clearRect(0, 0, size, size);
           context.fillStyle = 'rgba(100, 100, 100, 0.5)';
           context.fillRect(0, 0, size, size);
-          context.fillStyle = 'black';
+          context.fillStyle = hiDPI ? 'red' : 'black';
           context.fillText(`z: ${z}`, half, half - lineHeight);
           context.fillText(`x: ${x}`, half, half);
           context.fillText(`y: ${y}`, half, half + lineHeight);
@@ -34,6 +38,7 @@ const map = new Map({
           // converting to Uint8Array for increased browser compatibility
           return new Uint8Array(data.buffer);
         },
+        tilePixelRatio: tilePixelRatio,
         // disable opacity transition to avoid overlapping labels during tile loading
         transition: 0,
       }),
