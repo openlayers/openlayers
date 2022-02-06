a86f0704f3e7bc30bc817824e8510c46ffa6f0f5
Maximilian Krög <maxi_kroeg@web.de>
2022-02-06 21:50:41 +0100

    Fix error when disposing ol/renderer/webgl/TileLayer
    - helper may not exist
    - tileTextureCache not cleared when canvasCacheKey changes

--- src/ol/renderer/webgl/Layer.js
+++ src/ol/renderer/webgl/Layer.js
@@ -95,7 +95,7 @@ class WebGLLayerRenderer extends LayerRenderer {
      */
     this.helper;
 
-    layer.addChangeListener(LayerProperty.MAP, this.removeHelper_.bind(this));
+    layer.addChangeListener(LayerProperty.MAP, this.removeHelper.bind(this));
 
     this.dispatchPreComposeEvent = this.dispatchPreComposeEvent.bind(this);
     this.dispatchPostComposeEvent = this.dispatchPostComposeEvent.bind(this);
@@ -148,7 +148,10 @@ class WebGLLayerRenderer extends LayerRenderer {
     }
   }
 
-  removeHelper_() {
+  /**
+   * @protected
+   */
+  removeHelper() {
     if (this.helper) {
       this.helper.dispose();
       delete this.helper;
@@ -187,9 +190,7 @@ class WebGLLayerRenderer extends LayerRenderer {
         'map/' + frameState.mapId + '/group/' + groupNumber;
 
       if (!this.helper || !this.helper.canvasCacheKeyMatches(canvasCacheKey)) {
-        if (this.helper) {
-          this.helper.dispose();
-        }
+        this.removeHelper();
 
         this.helper = new WebGLHelper({
           postProcesses: this.postProcesses_,
@@ -227,7 +228,7 @@ class WebGLLayerRenderer extends LayerRenderer {
    * Clean up.
    */
   disposeInternal() {
-    this.removeHelper_();
+    this.removeHelper();
     super.disposeInternal();
   }
 
