d95d327a332eb3a9ca7b9c36dd721ecf3586e413
Arjen Hoogesteger <arjen.hoogesteger@sweco.nl>
2022-02-11 10:59:11 +0100

    Baby steps in creating a CurvePolygon geometry.

--- src/ol/geom/CurvePolygon.js
+++ src/ol/geom/CurvePolygon.js
@@ -2,7 +2,7 @@
  * @module ol/geom/CurvePolygon
  */
 import GeometryType from './GeometryType.js';
-import Polygon from './Polygon.js';
+import SimpleGeometry from './SimpleGeometry.js';
 import {createOrUpdateEmpty, extend} from '../extent.js';
 
 function getCoords(rings) {
@@ -11,24 +11,27 @@ function getCoords(rings) {
   for (let i = 0; i < rings.length; i++) {
     const ring = rings[i];
 
-    const ringType = ring.getType();
-    if (
-      ringType === GeometryType.CIRCULAR_STRING ||
-      ringType === GeometryType.LINE_STRING
-    ) {
-      coords.push(ring.getCoordinates());
-    } else {
-      const geometries = ring.getGeometries();
-      for (let ii = 0; ii < geometries.length; ii++) {
-        coords.push(geometries[ii].getCoordinates());
-      }
-    }
+    //const ringType = ring.getType();
+    coords.push(ring.getCoordinates());
+    // if (
+    //   ringType === GeometryType.CIRCULAR_STRING ||
+    //   ringType === GeometryType.LINE_STRING
+    // ) {
+    //   coords.push(ring.getCoordinates());
+    // } else {
+    //   const geometries = ring.getGeometries();
+    //   for (let ii = 0; ii < geometries.length; ii++) {
+    //     coords.push(geometries[ii].getCoordinates());
+    //   }
+    // }
   }
   return coords;
 }
 
-class CurvePolygon extends Polygon {
+class CurvePolygon extends SimpleGeometry {
   constructor(rings, opt_layout, opt_ends) {
+    // super([], opt_layout, opt_ends);
+    console.log(rings);
     super(getCoords(rings), opt_layout, opt_ends);
 
     /**
