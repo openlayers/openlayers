d2fc5caa21a33f15b11b05cb5227819c2c468775
mike-000 <49240900+mike-000@users.noreply.github.com>
2022-04-09 13:06:32 +0100

    do not add second interaction

--- examples/pinch-zoom.js
+++ examples/pinch-zoom.js
@@ -2,13 +2,8 @@ import Map from '../src/ol/Map.js';
 import OSM from '../src/ol/source/OSM.js';
 import TileLayer from '../src/ol/layer/Tile.js';
 import View from '../src/ol/View.js';
-import {
-  PinchZoom,
-  defaults as defaultInteractions,
-} from '../src/ol/interaction.js';
 
 const map = new Map({
-  interactions: defaultInteractions().extend([new PinchZoom()]),
   layers: [
     new TileLayer({
       source: new OSM(),
