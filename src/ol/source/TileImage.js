2ac6ccd31ed6701665dc51befa7a857b0f1eb92b
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-08-18 22:31:15 +0200

    Properly clear and refresh reprojected sources

--- src/ol/source/TileImage.js
+++ src/ol/source/TileImage.js
@@ -451,6 +451,13 @@ class TileImage extends UrlTile {
       }
     }
   }
+
+  clear() {
+    super.clear();
+    for (const id in this.tileCacheForProjection) {
+      this.tileCacheForProjection[id].clear();
+    }
+  }
 }
 
 /**
