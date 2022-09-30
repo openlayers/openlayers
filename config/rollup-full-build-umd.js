83c585ea281a0a87ffec29c42a76b4a1a68a5bc2
m.dellanave <m.dellanave@fabbricadigitale.it>
2022-09-30 09:46:41 +0200

    linted

--- config/rollup-full-build-umd.js
+++ config/rollup-full-build-umd.js
@@ -1,23 +1,23 @@
-import commonjs from "@rollup/plugin-commonjs";
-import externalGlobals from "rollup-plugin-external-globals";
-import resolve from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
+import commonjs from '@rollup/plugin-commonjs';
+import externalGlobals from 'rollup-plugin-external-globals';
+import resolve from '@rollup/plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser';
 
 export default {
-  input: "build/index.js",
+  input: 'build/index.js',
   output: {
-    name: "ol",
-    format: "umd",
-    exports: "default",
-    file: "build/umd/ol.js",
+    name: 'ol',
+    format: 'umd',
+    exports: 'default',
+    file: 'build/umd/ol.js',
     sourcemap: true,
   },
   plugins: [
-    resolve({ moduleDirectories: ["build", "node_modules"] }),
+    resolve({moduleDirectories: ['build', 'node_modules']}),
     commonjs(),
     externalGlobals({
-      geotiff: "GeoTIFF",
-      "ol-mapbox-style": "olms",
+      geotiff: 'GeoTIFF',
+      'ol-mapbox-style': 'olms',
     }),
     terser(),
   ],
