0004e8f5eb45c0b17c6a1a87a0e9ec5a4fff5baa
Anders Ekelund <anders.emil.ekelund@gmail.com>
2022-10-06 13:16:21 +0200

    Add helper method to calculate css scale based on map Canvas

---
+++ src/ol/canvasUtils.js
@@ -0,0 +1,18 @@
+import {DEVICE_PIXEL_RATIO} from './has.js';
+
+/**
+ * If the map is scaled using css transform scale(), pointer events for interactions will be offset.
+ * This function calculates the scale from the event so we can take that into account when handling the interactions.
+ *
+ * @param {HTMLCanvasElement} el Event target element (the map Canvas element)
+ * @return {Array} Array of x-scale and y-scale
+ */
+export function getCanvasScale(el) {
+  console.log({el})
+  if(!(el instanceof HTMLCanvasElement)) {
+    return [1, 1]
+  }
+  const scaleX = el.getBoundingClientRect().width * DEVICE_PIXEL_RATIO / el.offsetWidth
+  const scaleY = el.getBoundingClientRect().height * DEVICE_PIXEL_RATIO / el.offsetHeight
+  return [scaleX,scaleY]
+}
