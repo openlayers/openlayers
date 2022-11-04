2f0d1812725801d2e38d622ce062892436c86782
mike-000 <49240900+mike-000@users.noreply.github.com>
2022-11-04 20:02:10 +0000

    use local data

--- examples/cog-overviews.js
+++ examples/cog-overviews.js
@@ -2,45 +2,41 @@ import GeoTIFF from '../src/ol/source/GeoTIFF.js';
 import Map from '../src/ol/Map.js';
 import TileLayer from '../src/ol/layer/WebGLTile.js';
 
-// scale values in this range to 0 - 1
-const min = 10000;
-const max = 15000;
-
-const base =
-  'https://landsat-pds.s3.amazonaws.com/c1/L8/139/045/LC08_L1TP_139045_20170304_20170316_01_T1/LC08_L1TP_139045_20170304_20170316_01_T1';
+const base1 = 'data/geotiff/nldeel_2011.tif';
+const base2 = 'data/geotiff/claimregions.tif';
 
 const source = new GeoTIFF({
   sources: [
     {
-      url: `${base}_B6.TIF`,
-      overviews: [`${base}_B6.TIF.ovr`],
-      min: min,
-      max: max,
+      url: `${base1}`,
+      overviews: [`${base1}.ovr`],
       nodata: 0,
     },
     {
-      url: `${base}_B5.TIF`,
-      overviews: [`${base}_B5.TIF.ovr`],
-      min: min,
-      max: max,
-      nodata: 0,
-    },
-    {
-      url: `${base}_B3.TIF`,
-      overviews: [`${base}_B3.TIF.ovr`],
-      min: min,
-      max: max,
+      url: `${base2}`,
+      overviews: [`${base2}.ovr`],
       nodata: 0,
     },
   ],
 });
+
+const band1 = ['*', ['band', 1], 255 / 7];
+const band2 = ['*', ['band', 2], 255 / 7];
 
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
       style: {
-        saturation: -0.3,
+        // Use red and blue channels to display national regions,
+        // and green channel to show research sub-regionals.
+        color: [
+          'array',
+          ['-', 1, band1],
+          ['-', ['ceil', band2], band2],
+          band1,
+          ['band', 3],
+        ],
       },
       source: source,
     }),
