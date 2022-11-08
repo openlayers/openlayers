1b02bb2feea62132a1110b9f63e74fb3f0db6795
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-11-08 20:54:56 +0100

    Add resolutions option to Raster source

--- examples/shaded-relief.js
+++ examples/shaded-relief.js
@@ -126,12 +126,15 @@ function shade(inputs, data) {
 const elevation = new XYZ({
   url: 'https://{a-d}.tiles.mapbox.com/v3/aj.sf-dem/{z}/{x}/{y}.png',
   crossOrigin: 'anonymous',
+  maxZoom: 13,
 });
 
 const raster = new Raster({
   sources: [elevation],
   operationType: 'image',
   operation: shade,
+  // avoid interpolation artefacts by only rendering native elevation resolutions
+  resolutions: elevation.getTileGrid().getResolutions(),
 });
 
 const map = new Map({
@@ -149,7 +152,6 @@ const map = new Map({
     extent: [-13675026, 4439648, -13580856, 4580292],
     center: [-13615645, 4497969],
     minZoom: 10,
-    maxZoom: 16,
     zoom: 13,
   }),
 });
