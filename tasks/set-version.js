369dc8f41010fac43921420869064bb26e6c6a2c
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-11-10 23:09:38 +0100

    Ensure a replacement has been made
    Co-authored-by: MoonE <maxi_kroeg@web.de>

--- tasks/set-version.js
+++ tasks/set-version.js
@@ -12,10 +12,14 @@ async function main() {
   // update the version number in util.js
   const utilPath = path.join(buildDir, 'util.js');
   let utilSrc = await fse.readFile(utilPath, 'utf-8');
-  utilSrc = utilSrc.replace(
-    /const VERSION = '(?:[^']*)';/g,
-    `const VERSION = '${pkg.version}';`
-  );
+  let replaced = 0;
+  utilSrc = utilSrc.replace(/(?<=const VERSION = ')(?:[^']*)(?=';)/g, () => {
+    replaced++;
+    return pkg.version;
+  });
+  if (replaced !== 1) {
+    throw new Error('Failed to replace version');
+  }
   await fse.writeFile(utilPath, utilSrc, 'utf-8');
 }
 
