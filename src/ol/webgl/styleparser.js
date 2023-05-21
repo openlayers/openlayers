270e86e7e9365271736eb3b17ce2e2c381c64de3
Olivia Guyot <olivia.guyot@camptocamp.com>
2023-05-21 14:44:15 +0200

    webgl / adapt style parser to comply to VectorStyleRenderer input
    Also adapt the WebGLPointsLayer accordingly

--- src/ol/webgl/styleparser.js
+++ src/ol/webgl/styleparser.js
@@ -239,8 +239,8 @@ function parseFillProperties(
  * @property {boolean} hasSymbol Has a symbol style defined
  * @property {boolean} hasStroke Has a stroke style defined
  * @property {boolean} hasFill Has a fill style defined
- * @property {Object<string,import("./Helper").UniformValue>} uniforms Uniform definitions.
- * @property {Array<import("../renderer/webgl/PointsLayer").CustomAttribute>} attributes Attribute descriptions.
+ * @property {import("../render/webgl/VectorStyleRenderer.js").UniformDefinitions} uniforms Uniform definitions
+ * @property {import("../render/webgl/VectorStyleRenderer.js").AttributeDefinitions} attributes Attribute definitions
  */
 
 /**
@@ -394,7 +394,13 @@ export function parseLiteralStyle(style) {
     hasSymbol,
     hasStroke,
     hasFill,
-    attributes: attributes,
+    attributes: attributes.reduce(
+      (prev, curr) => ({
+        ...prev,
+        [curr.name]: {callback: curr.callback, size: curr.size},
+      }),
+      {}
+    ),
     uniforms: uniforms,
   };
 }
