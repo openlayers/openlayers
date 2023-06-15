89a81c138d8fb899d20af054bd0d358020c5f79f
Björn Harrtell <bjorn@wololo.org>
2023-06-15 11:31:13 +0200

    Reproduction test case for issue 14833

--- test/browser/spec/ol/format/wfs.test.js
+++ test/browser/spec/ol/format/wfs.test.js
@@ -1938,5 +1938,16 @@ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
       });
       expect(serialized.firstElementChild).to.xmleql(parse(text));
     });
+
+    it('WFS 1.1.0 resourceId filter', function () {
+      //const text = 'TBD';
+      const serialized = new WFS({version: '1.1.0'}).writeGetFeature({
+        srsName: 'EPSG:4326',
+        featureTypes: ['states'],
+        filter: resourceIdFilter(1),
+      });
+      expect(serialized).to.be.ok;
+      //expect(serialized.firstElementChild).to.xmleql(parse(text));
+    });
   });
 });
