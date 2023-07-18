9e49545088dd8ed70ed10e013199829ba81d154a
Gregg Roemhildt <roemhildtg@gmail.com>
2023-07-18 08:29:46 -0500

    Fix cluster source handling of points with null geometry

--- src/ol/source/Cluster.js
+++ src/ol/source/Cluster.js
@@ -115,7 +115,7 @@ class Cluster extends VectorSource {
       options.geometryFunction ||
       function (feature) {
         const geometry = /** @type {Point} */ (feature.getGeometry());
-        assert(geometry.getType() == 'Point', 10); // The default `geometryFunction` can only handle `Point` geometries
+        assert(!geomety || geometry.getType() === 'Point', 10); // The default `geometryFunction` can only handle `Point` or null geometries
         return geometry;
       };
 
