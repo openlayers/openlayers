c5e5a8a80541439b98689fd9dba593e1f7fc61c9
Maximilian Krög <maxi_kroeg@web.de>
2023-07-18 22:02:09 +0200

    Add test for cluster source with null geometries

--- test/browser/spec/ol/source/cluster.test.js
+++ test/browser/spec/ol/source/cluster.test.js
@@ -37,6 +37,16 @@ describe('ol.source.Cluster', function () {
       expect(source.getFeatures().length).to.be(1);
       expect(source.getFeatures()[0].get('features').length).to.be(2);
     });
+    it('clusters a source with point and null features', function () {
+      const source = new Cluster({
+        source: new VectorSource({
+          features: [new Feature(new Point([0, 0])), new Feature()],
+        }),
+      });
+      source.loadFeatures(extent, 1, projection);
+      expect(source.getFeatures().length).to.be(1);
+      expect(source.getFeatures()[0].get('features').length).to.be(1);
+    });
     it('clusters with a custom geometryFunction', function () {
       const source = new Cluster({
         geometryFunction: function (feature) {
