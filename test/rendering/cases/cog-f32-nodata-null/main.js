27c563a289c3285f0dee67ee868d537bca2f5cfa
mike-000 <49240900+mike-000@users.noreply.github.com>
2023-08-24 09:41:48 +0100

    Add alpha band if nodata is specified as null

---
+++ test/rendering/cases/cog-f32-nodata-null/main.js
@@ -0,0 +1,22 @@
+import GeoTIFF from '../../../../src/ol/source/GeoTIFF.js';
+import Map from '../../../../src/ol/Map.js';
+import TileLayer from '../../../../src/ol/layer/WebGLTile.js';
+
+const source = new GeoTIFF({
+  transition: 0,
+  sources: [{url: '/data/raster/elevation-f32.tif', nodata: null}],
+});
+
+new Map({
+  layers: [
+    new TileLayer({
+      source: source,
+    }),
+  ],
+  target: 'map',
+  view: source.getView(),
+});
+
+render({
+  message: 'normalize i16 data with nan nodata based on GDAL stats',
+});
