0e7f49a6e437d46c2152625700ee164fd366da86
Tim Schaub <tim@planet.com>
2023-09-02 13:00:12 -0600

    Accept a number or an array of numbers for icon scale

--- src/ol/render/canvas/style.js
+++ src/ol/render/canvas/style.js
@@ -996,7 +996,11 @@ function sizeLikeEvaluator(flatStyle, name, context) {
   if (!(name in flatStyle)) {
     return null;
   }
-  const evaluator = buildExpression(flatStyle[name], NumberArrayType, context);
+  const evaluator = buildExpression(
+    flatStyle[name],
+    NumberArrayType | NumberType,
+    context
+  );
   return function (context) {
     return requireSizeLike(evaluator(context), name);
   };
