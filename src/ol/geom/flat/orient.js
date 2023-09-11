e929b944be8d92277c2030dc45bac67fa6ae2da6
Andreas Hocevar <andreas.hocevar@gmail.com>
2023-09-11 16:00:46 +0200

    Make inflateEnds work regardless of the y coordinate orientation

--- src/ol/geom/flat/orient.js
+++ src/ol/geom/flat/orient.js
@@ -188,10 +188,15 @@ export function inflateEnds(flatCoordinates, ends) {
   const endss = [];
   let offset = 0;
   let prevEndIndex = 0;
+  let startOrientation;
   for (let i = 0, ii = ends.length; i < ii; ++i) {
     const end = ends[i];
     // classifies an array of rings into polygons with outer rings and holes
-    if (!linearRingIsClockwise(flatCoordinates, offset, end, 2)) {
+    const orientation = linearRingIsClockwise(flatCoordinates, offset, end, 2);
+    if (startOrientation === undefined) {
+      startOrientation = orientation;
+    }
+    if (orientation === startOrientation) {
       endss.push(ends.slice(prevEndIndex, i + 1));
     } else {
       if (endss.length === 0) {
