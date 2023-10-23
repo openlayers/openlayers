9a469faef4dedfab976172a550bac3101c445e10
Olivia Guyot <olivia.guyot@camptocamp.com>
2023-10-23 10:49:26 +0200

    Add stroke pattern option to webgl-draw-line example

--- examples/webgl-draw-line.js
+++ examples/webgl-draw-line.js
@@ -8,33 +8,10 @@ import {OSM, Vector as VectorSource} from '../src/ol/source.js';
 import {Tile as TileLayer} from '../src/ol/layer.js';
 import {fromLonLat} from '../src/ol/proj.js';
 
-const style = {
-  variables: {
-    width: 12,
-    offset: 0,
-    capType: 'butt',
-    joinType: 'miter',
-    miterLimit: 10, // ratio
-    dashLength1: 25,
-    dashLength2: 15,
-    dashLength3: 15,
-    dashLength4: 15,
-    dashOffset: 0,
-  },
-  'stroke-width': ['var', 'width'],
-  'stroke-color': 'rgba(24,86,34,0.7)',
-  'stroke-line-dash': [
-    ['var', 'dashLength1'],
-    ['var', 'dashLength2'],
-    ['var', 'dashLength3'],
-    ['var', 'dashLength4'],
-  ],
-  'stroke-line-dash-offset': ['var', 'dashOffset'],
-  'stroke-offset': ['var', 'offset'],
-  'stroke-line-cap': ['var', 'capType'],
-  'stroke-line-join': ['var', 'joinType'],
-  'stroke-miter-limit': ['var', 'miterLimit'],
-};
+/**
+ * @type {import('../src/ol/style/webgl.js').WebGLStyle}
+ */
+let style;
 
 class WebGLLayer extends Layer {
   createRenderer() {
@@ -45,26 +22,93 @@ class WebGLLayer extends Layer {
   }
 }
 
-const osm = new TileLayer({
-  source: new OSM(),
-});
-
 const source = new VectorSource({
   url: 'data/geojson/switzerland.geojson',
   format: new GeoJSON(),
 });
-const vector = new WebGLLayer({
-  source: source,
+
+/**
+ * @param {boolean} dash Include line dash
+ * @param {boolean} pattern Include image pattern
+ * @return {import('../src/ol/style/webgl.js').WebGLStyle} Generated style
+ */
+const getStyle = (dash, pattern) => {
+  let newStyle = {
+    variables: style
+      ? style.variables
+      : {
+          width: 12,
+          offset: 0,
+          capType: 'butt',
+          joinType: 'miter',
+          miterLimit: 10, // ratio
+          dashLength1: 25,
+          dashLength2: 15,
+          dashLength3: 15,
+          dashLength4: 15,
+          dashOffset: 0,
+          patternSpacing: 0,
+        },
+    'stroke-width': ['var', 'width'],
+    'stroke-color': 'rgba(24,86,34,0.7)',
+    'stroke-offset': ['var', 'offset'],
+    'stroke-miter-limit': ['var', 'miterLimit'],
+    'stroke-line-cap': ['var', 'capType'],
+    'stroke-line-join': ['var', 'joinType'],
+  };
+  if (dash) {
+    newStyle = {
+      ...newStyle,
+      'stroke-line-dash': [
+        ['var', 'dashLength1'],
+        ['var', 'dashLength2'],
+        ['var', 'dashLength3'],
+        ['var', 'dashLength4'],
+      ],
+      'stroke-line-dash-offset': ['var', 'dashOffset'],
+    };
+  }
+  if (pattern) {
+    delete newStyle['stroke-color'];
+    newStyle = {
+      ...newStyle,
+      'stroke-pattern-src': 'data/dot.svg',
+      'stroke-pattern-spacing': ['var', 'patternSpacing'],
+    };
+  }
+  return newStyle;
+};
+
+style = getStyle(false, false);
+
+let vector = new WebGLLayer({
+  source,
 });
 
 const map = new Map({
-  layers: [osm, vector],
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+    vector,
+  ],
   target: 'map',
   view: new View({
     center: fromLonLat([8.43, 46.82]),
     zoom: 7,
   }),
 });
+
+const rebuildStyle = () => {
+  const dash = document.getElementById('dashEnable').checked;
+  const pattern = document.getElementById('patternEnable').checked;
+  style = getStyle(dash, pattern);
+  map.removeLayer(vector);
+  vector = new WebGLLayer({
+    source,
+  });
+  map.addLayer(vector);
+};
 
 const modify = new Modify({source: source});
 map.addInteraction(modify);
@@ -100,3 +144,6 @@ const inputListener = (event) => {
 document
   .querySelectorAll('input.uniform')
   .forEach((input) => input.addEventListener('input', inputListener));
+document
+  .querySelectorAll('input.rebuild')
+  .forEach((input) => input.addEventListener('input', rebuildStyle));
