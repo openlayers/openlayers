04da45320a9e36bda0f1323e065717ff27bc46e0
Sandro Mani <manisandro@gmail.com>
2023-11-07 18:23:56 +0100

    Add segment information to snap result

--- src/ol/interaction/Snap.js
+++ src/ol/interaction/Snap.js
@@ -29,6 +29,7 @@ import {listen, unlistenByKey} from '../events.js';
  * @property {import("../coordinate.js").Coordinate|null} vertex Vertex.
  * @property {import("../pixel.js").Pixel|null} vertexPixel VertexPixel.
  * @property {import("../Feature.js").default|null} feature Feature.
+ * @property {Array<import("../coordinate.js").Coordinate>|null} segment Segment, or `null` if snapped to a vertex.
  */
 
 /**
@@ -476,6 +477,7 @@ class Snap extends PointerInteraction {
     let closestVertex;
     let minSquaredDistance = Infinity;
     let closestFeature;
+    let closestSegment = null;
 
     const squaredPixelTolerance = this.pixelTolerance_ * this.pixelTolerance_;
     const getResult = () => {
@@ -490,6 +492,7 @@ class Snap extends PointerInteraction {
               Math.round(vertexPixel[1]),
             ],
             feature: closestFeature,
+            segment: closestSegment,
           };
         }
       }
@@ -546,6 +549,7 @@ class Snap extends PointerInteraction {
           const delta = squaredDistance(projectedCoordinate, vertex);
           if (delta < minSquaredDistance) {
             closestVertex = toUserCoordinate(vertex, projection);
+            closestSegment = segmentData.segment;
             minSquaredDistance = delta;
           }
         }
