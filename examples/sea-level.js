c30df6b461cb327c0db7d8d7e8ad27bb2397f686
Frederic Junod <frederic.junod@camptocamp.com>
2023-12-06 11:23:54 +0100

    Update link to terrain-rgb in sea-level example

--- examples/sea-level.js
+++ examples/sea-level.js
@@ -28,7 +28,7 @@ const attributions =
 
 const elevation = new XYZ({
   // The RGB values in the source collectively represent elevation.
-  // Interpolation of individual colors would produce incorrect evelations and is disabled.
+  // Interpolation of individual colors would produce incorrect elevations and is disabled.
   url: 'https://api.maptiler.com/tiles/terrain-rgb/{z}/{x}/{y}.png?key=' + key,
   tileSize: 512,
   maxZoom: 12,
