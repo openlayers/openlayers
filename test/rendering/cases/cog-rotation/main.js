1d6c482fa2bf39cd955bff32b0b33e3eab9b1415
mike-000 <49240900+mike-000@users.noreply.github.com>
2024-01-13 19:31:23 +0000

    ModelTransformation rotation test

---
+++ test/rendering/cases/cog-rotation/main.js
@@ -0,0 +1,39 @@
+import Map from '../../../../src/ol/Map.js';
+import TileLayer from '../../../../src/ol/layer/WebGLTile.js';
+import proj4 from 'proj4';
+import {GeoTIFF, TileDebug} from '../../../../src/ol/source.js';
+import {register} from '../../../../src/ol/proj/proj4.js';
+
+const source = new GeoTIFF({
+  sources: [
+    {
+      url: '/data/raster/umbra.tif',
+    },
+  ],
+  transition: 0,
+});
+
+new Map({
+  layers: [
+    new TileLayer({
+      source: source,
+    }),
+    new TileLayer({
+      source: new TileDebug(),
+      opacity: 0.8,
+    }),
+  ],
+  target: 'map',
+  view: source.getView().then((viewConfig) => {
+    proj4.defs(
+      'EPSG:32759',
+      '+proj=utm +zone=59 +south +datum=WGS84 +units=m +no_defs',
+    );
+    register(proj4);
+    return viewConfig;
+  }),
+});
+
+render({
+  message: 'works with geotiffs that include a ModelTransformation rotation',
+});
