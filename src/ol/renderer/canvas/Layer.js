009f12e50a7658ca901a88e3e98f36db3ccf9bb5
Davide Menegatti <davide.menegatti@theoceanly.com>
2024-01-22 00:10:20 +0100

    #14954 - Add `willReadFrequently: true` to 2d context generation settings in CanvasLayerRenderer

--- src/ol/renderer/canvas/Layer.js
+++ src/ol/renderer/canvas/Layer.js
@@ -164,7 +164,9 @@ class CanvasLayerRenderer extends LayerRenderer {
     ) {
       const canvas = target.firstElementChild;
       if (canvas instanceof HTMLCanvasElement) {
-        context = canvas.getContext('2d');
+        context = canvas.getContext('2d', {
+          willReadFrequently: true,
+        });
       }
     }
     if (context && context.canvas.style.transform === transform) {
@@ -187,7 +189,9 @@ class CanvasLayerRenderer extends LayerRenderer {
       style.position = 'absolute';
       style.width = '100%';
       style.height = '100%';
-      context = createCanvasContext2D();
+      context = createCanvasContext2D(undefined, undefined, undefined, {
+        willReadFrequently: true,
+      });
       const canvas = context.canvas;
       container.appendChild(canvas);
       style = canvas.style;
