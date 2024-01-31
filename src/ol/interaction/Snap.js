93e6a8d379454ddb730fcdaa2b3941235efbb863
Frederic Junod <frederic.junod@camptocamp.com>
2024-01-31 16:25:36 +0100

    Change Geometry.transform return type to 'this'
    Otherwise the return type is `Geometry` for subclasses instead of the actual class.

--- src/ol/interaction/Snap.js
+++ src/ol/interaction/Snap.js
@@ -586,9 +586,9 @@ class Snap extends PointerInteraction {
     let circleGeometry = geometry;
     const userProjection = getUserProjection();
     if (userProjection) {
-      circleGeometry = /** @type {import("../geom/Circle.js").default} */ (
-        circleGeometry.clone().transform(userProjection, projection)
-      );
+      circleGeometry = circleGeometry
+        .clone()
+        .transform(userProjection, projection);
     }
     const polygon = fromCircle(circleGeometry);
     if (userProjection) {
