8f34bd58c09a84c2af3f117677c663f74277ea50
unknown <chenfei@boswinner.boswinner.com>
2024-04-12 01:10:01 +0800

    Perf:Compatible with one and multi line text

--- src/ol/render/canvas.js
+++ src/ol/render/canvas.js
@@ -373,21 +373,24 @@ export function getTextDimensions(baseStyle, chunks) {
   let width = 0;
   let lineWidth = 0;
   let height = 0;
+  let lineHeight = 0;
   for (let i = 0, ii = chunks.length; i <= ii; i += 2) {
     const text = chunks[i];
     if (text === '\n' || i === ii) {
       width = Math.max(width, lineWidth);
       lineWidths.push(lineWidth);
       lineWidth = 0;
+	  height += lineHeight;
+	  lineHeight = 0;
       continue;
     }
     const font = chunks[i + 1] || baseStyle.font;
     const currentWidth = measureTextWidth(font, text);
     widths.push(currentWidth);
     lineWidth += currentWidth;
-    const currentLineHeight = measureTextHeight(font);
-    heights.push(currentLineHeight);
-    height += currentLineHeight;
+	const currentHeight = measureTextHeight(font);
+    heights.push(currentHeight);
+    lineHeight = Math.max(lineHeight, currentHeight);
   }
   return {width, height, widths, heights, lineWidths};
 }
