263f863d5caae0933a0474786c83df99f9cc60ea
Roman Prokop <64583092+romprokop814@users.noreply.github.com>
2024-07-02 13:54:56 +0300

    Added Filter lines by line-metric with WebGL example

--- examples/filter-webgl-line.js
+++ examples/filter-webgl-line.js
@@ -1,94 +1,68 @@
-import GeoJSON from '../src/ol/format/GeoJSON.js';
+import IGC from '../src/ol/format/IGC.js';
 import Layer from '../src/ol/layer/Layer.js';
 import Map from '../src/ol/Map.js';
 import View from '../src/ol/View.js';
 import WebGLVectorLayerRenderer from '../src/ol/renderer/webgl/VectorLayer.js';
-import {Draw, Modify, Snap} from '../src/ol/interaction.js';
 import {OSM, Vector as VectorSource} from '../src/ol/source.js';
 import {Tile as TileLayer} from '../src/ol/layer.js';
 import {fromLonLat} from '../src/ol/proj.js';
+import IGC from '../src/ol/format/IGC.js';
 
-/**
- * @type {import('../src/ol/style/webgl.js').WebGLStyle}
- */
-let style;
+const lineStyle = {
+  variables: {
+    timestamp: 1303240000,
+  },
+  'stroke-width': 4,
+  'stroke-color': [
+    'interpolate',
+    ['linear'],
+    ['line-metric'],
+    1303200000,
+    'hsl(312,100%,39%)',
+    1303240000,
+    'hsl(36,100%,45%)',
+  ],
+  filter: ['<=', ['line-metric'], ['var', 'timestamp']],
+};
 
 class WebGLLayer extends Layer {
   createRenderer() {
     return new WebGLVectorLayerRenderer(this, {
       className: this.getClassName(),
-      style,
+      style: lineStyle,
     });
   }
 }
 
+const igcUrls = [
+  'data/igc/Clement-Latour.igc',
+  'data/igc/Damien-de-Baenst.igc',
+  'data/igc/Sylvain-Dhonneur.igc',
+  'data/igc/Tom-Payne.igc',
+  'data/igc/Ulrich-Prinz.igc',
+];
+
 const source = new VectorSource({
-  url: 'data/geojson/switzerland.geojson',
-  format: new GeoJSON(),
+  features: [],
 });
 
-/**
- * @param {boolean} dash Include line dash
- * @param {boolean} pattern Include image pattern
- * @return {import('../src/ol/style/webgl.js').WebGLStyle} Generated style
- */
-const getStyle = (dash, pattern) => {
-  let newStyle = {
-    variables: style
-      ? style.variables
-      : {
-          width: 12,
-          offset: 0,
-          capType: 'butt',
-          joinType: 'miter',
-          miterLimit: 10, // ratio
-          dashLength1: 25,
-          dashLength2: 15,
-          dashLength3: 15,
-          dashLength4: 15,
-          dashOffset: 0,
-          patternSpacing: 0,
-          minT: 40,
-          maxT: 90,
-        },
-    'stroke-width': ['var', 'width'],
-    'stroke-color': 'rgb(126,35,144, 0.8)',
-    'stroke-offset': ['var', 'offset'],
-    'stroke-miter-limit': ['var', 'miterLimit'],
-    'stroke-line-cap': ['var', 'capType'],
-    'stroke-line-join': ['var', 'joinType'],
-    filter: [
-      'all',
-      ['>=', ['line-metric'], ['var', 'minT']],
-      ['<=', ['line-metric'], ['var', 'maxT']],
-    ],
-  };
-  if (dash) {
-    newStyle = {
-      ...newStyle,
-      'stroke-line-dash': [
-        ['var', 'dashLength1'],
-        ['var', 'dashLength2'],
-        ['var', 'dashLength3'],
-        ['var', 'dashLength4'],
-      ],
-      'stroke-line-dash-offset': ['var', 'dashOffset'],
-    };
-  }
-  if (pattern) {
-    delete newStyle['stroke-color'];
-    newStyle = {
-      ...newStyle,
-      'stroke-pattern-src': 'data/dot.svg',
-      'stroke-pattern-spacing': ['var', 'patternSpacing'],
-    };
-  }
-  return newStyle;
+const START_TIME = 1303218342000;
+const END_TIME = 1303233916;
+
+const format = new IGC();
+for (let i = 0; i < igcUrls.length; ++i) {
+  fetch(igcUrls[i]).then(resp => resp.text()).then((data) => {
+    const features = format.readFeatures(data, {
+      featureProjection: 'EPSG:3857',
+    });
+    console.log(features)
+    source.addFeatures(features);
+  });
 };
 
-style = getStyle(false, false);
+console.log('source', source);
 
-let vector = new WebGLLayer({
+const vectorLayer = new WebGLLayer({
   source,
 });
 
@@ -97,60 +71,22 @@ const map = new Map({
     new TileLayer({
       source: new OSM(),
     }),
-    vector,
+    vectorLayer,
   ],
   target: 'map',
   view: new View({
-    center: fromLonLat([8.43, 46.82]),
-    zoom: 7,
+    center: [703365.7089403362, 5714629.865071137],
+    zoom: 9,
   }),
 });
 
-const rebuildStyle = () => {
-  const dash = document.getElementById('dashEnable').checked;
-  const pattern = document.getElementById('patternEnable').checked;
-  style = getStyle(dash, pattern);
-  map.removeLayer(vector);
-  vector = new WebGLLayer({
-    source,
-  });
-  map.addLayer(vector);
-};
-
-const modify = new Modify({source: source});
-map.addInteraction(modify);
-
-let draw, snap; // global so we can remove them later
-
-function addInteractions() {
-  draw = new Draw({
-    source: source,
-    type: 'LineString',
-  });
-  map.addInteraction(draw);
-  snap = new Snap({source: source});
-  map.addInteraction(snap);
+function showTime() {
+  const time = new Date(lineStyle.variables.timestamp * 1000);
+  document.getElementById('time-value').textContent = time.toUTCString();
 }
-
-addInteractions();
-
-const inputListener = (event) => {
-  const variables = style.variables;
-  const variableName = event.target.name;
-  if (event.target.type === 'radio') {
-    variables[variableName] = event.target.value;
-  } else {
-    variables[variableName] = parseFloat(event.target.value);
-  }
-  const valueSpan = document.getElementById(`value-${variableName}`);
-  if (valueSpan) {
-    valueSpan.textContent = variables[variableName];
-  }
+document.getElementById('time-input').addEventListener('input', (event) => {
+  lineStyle.variables.timestamp = parseFloat(event.target.value);
+  showTime();
   map.render();
-};
-document
-  .querySelectorAll('input.uniform')
-  .forEach((input) => input.addEventListener('input', inputListener));
-document
-  .querySelectorAll('input.rebuild')
-  .forEach((input) => input.addEventListener('input', rebuildStyle));
+});
+showTime();
