c5b438c568868eeab9b75398af3d887270979205
Maximilian Krög <maxi_kroeg@web.de>
2024-07-06 21:41:35 +0200

    Remove fallback for ImageData constructor in ol/source/Raster
    It is now widely supported and also used without fallback in other places.

--- test/browser/spec/ol/source/raster.test.js
+++ test/browser/spec/ol/source/raster.test.js
@@ -3,10 +3,7 @@ import ImageLayer from '../../../../../src/ol/layer/Image.js';
 import Map from '../../../../../src/ol/Map.js';
 import Point from '../../../../../src/ol/geom/Point.js';
 import Projection from '../../../../../src/ol/proj/Projection.js';
-import RasterSource, {
-  Processor,
-  newImageData,
-} from '../../../../../src/ol/source/Raster.js';
+import RasterSource, {Processor} from '../../../../../src/ol/source/Raster.js';
 import Source from '../../../../../src/ol/source/Source.js';
 import Static from '../../../../../src/ol/source/ImageStatic.js';
 import TileSource from '../../../../../src/ol/source/Tile.js';
@@ -520,7 +517,7 @@ where('Uint8ClampedArray').describe('Processor', function () {
       });
 
       const array = new Uint8ClampedArray([1, 2, 3, 4, 5, 6, 7, 8]);
-      const input = newImageData(array, 1, 2);
+      const input = new ImageData(array, 1, 2);
 
       processor.process([input], {count: 0, sum: 0}, function (err, output, m) {
         if (err) {
@@ -546,7 +543,7 @@ where('Uint8ClampedArray').describe('Processor', function () {
       });
 
       const array = new Uint8ClampedArray([1, 2, 3, 4, 5, 6, 7, 8]);
-      const input = newImageData(array, 1, 2);
+      const input = new ImageData(array, 1, 2);
 
       processor.process([input], {}, function (err, output, m) {
         if (err) {
@@ -587,7 +584,7 @@ where('Uint8ClampedArray').describe('Processor', function () {
       });
 
       const array = new Uint8ClampedArray([10, 2, 0, 0, 5, 8, 0, 1]);
-      const input = newImageData(array, 1, 2);
+      const input = new ImageData(array, 1, 2);
 
       processor.process([input], {}, function (err, output, m) {
         if (err) {
@@ -624,7 +621,7 @@ where('Uint8ClampedArray').describe('Processor', function () {
       }
 
       for (let i = 0; i < 5; ++i) {
-        const input = newImageData(new Uint8ClampedArray([1, 2, 3, 4]), 1, 1);
+        const input = new ImageData(new Uint8ClampedArray([1, 2, 3, 4]), 1, 1);
         processor.process([input], {}, createCallback(i));
       }
 
@@ -653,7 +650,7 @@ where('Uint8ClampedArray').describe('Processor', function () {
       }
 
       for (let i = 0; i < 5; ++i) {
-        const input = newImageData(new Uint8ClampedArray([1, 2, 3, 4]), 1, 1);
+        const input = new ImageData(new Uint8ClampedArray([1, 2, 3, 4]), 1, 1);
         processor.process([input], {}, createCallback(i));
       }
 
@@ -674,7 +671,7 @@ where('Uint8ClampedArray').describe('Processor', function () {
         operation: identity,
       });
 
-      const input = newImageData(new Uint8ClampedArray([1, 2, 3, 4]), 1, 1);
+      const input = new ImageData(new Uint8ClampedArray([1, 2, 3, 4]), 1, 1);
       processor.process([input], {}, function (err) {
         if (err) {
           done(err);
@@ -699,7 +696,7 @@ where('Uint8ClampedArray').describe('Processor', function () {
       });
 
       const array = new Uint8ClampedArray([1, 2, 3, 4, 5, 6, 7, 8]);
-      const input = newImageData(array, 1, 2);
+      const input = new ImageData(array, 1, 2);
 
       processor.process([input], {}, function (err, output, m) {
         if (err) {
@@ -720,7 +717,7 @@ where('Uint8ClampedArray').describe('Processor', function () {
       });
 
       const array = new Uint8ClampedArray([1, 2, 3, 4]);
-      const input = newImageData(array, 1, 1);
+      const input = new ImageData(array, 1, 1);
       const meta = {foo: 'bar'};
 
       processor.process([input], meta, function (err, output, m) {
@@ -742,7 +739,7 @@ where('Uint8ClampedArray').describe('Processor', function () {
       });
 
       const array = new Uint8ClampedArray([1, 2, 3, 4, 5, 6, 7, 8]);
-      const input = newImageData(array, 1, 2);
+      const input = new ImageData(array, 1, 2);
 
       processor.process([input], {}, function () {
         done(new Error('Expected abort to stop callback from being called'));
@@ -761,7 +758,7 @@ where('Uint8ClampedArray').describe('Processor', function () {
       });
 
       const array = new Uint8ClampedArray([1, 2, 3, 4, 5, 6, 7, 8]);
-      const input = newImageData(array, 1, 2);
+      const input = new ImageData(array, 1, 2);
 
       processor.process([input], {}, function () {
         done(new Error('Expected abort to stop callback from being called'));
