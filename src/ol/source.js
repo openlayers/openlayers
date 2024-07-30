b64be3c1e1df80dcf97d8301058950729376101f
kikuchan <kikuchan98@gmail.com>
2024-07-30 13:33:35 +0900

    Add ColorTileSource to provide simple color tiles

--- src/ol/source.js
+++ src/ol/source.js
@@ -8,6 +8,7 @@ import {getIntersection} from './extent.js';
 export {default as BingMaps} from './source/BingMaps.js';
 export {default as CartoDB} from './source/CartoDB.js';
 export {default as Cluster} from './source/Cluster.js';
+export {default as ColorTile} from './source/ColorTile.js';
 export {default as DataTile} from './source/DataTile.js';
 export {default as GeoTIFF} from './source/GeoTIFF.js';
 export {default as Google} from './source/Google.js';
