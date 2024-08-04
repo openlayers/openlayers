c61e3a2f0414b3c642f0f0c065c04b1b1b6fe05c
Olivia Guyot <olivia.guyot@camptocamp.com>
2024-08-04 23:49:07 +0200

    WebGL / always use a uniform for texture sizes
    The previous logic was not using a uniform if an image was ready
    synchronously. This would happen in very rare cases and in an unreliable way;
    if we don't want to make the style parsing asynchronous we have to always
    use a uniform for the image size.

--- src/ol/webgl/styleparser.js
+++ src/ol/webgl/styleparser.js
@@ -12,14 +12,13 @@ import {
   newParsingContext,
 } from '../expr/expression.js';
 import {ShaderBuilder} from './ShaderBuilder.js';
+import {asArray} from '../color.js';
 import {
-  arrayToGlsl,
   buildExpression,
   getStringNumberEquivalent,
   stringToGlsl,
   uniformNameForVariable,
 } from '../expr/gpu.js';
-import {asArray} from '../color.js';
 
 /**
  * Recursively parses a style expression and outputs a GLSL-compatible string. Takes in a compilation context that
@@ -202,23 +201,19 @@ function getColorFromDistanceField(
  */
 function parseImageProperties(style, builder, uniforms, prefix, textureId) {
   const image = new Image();
-  let size;
   image.crossOrigin =
     style[`${prefix}cross-origin`] === undefined
       ? 'anonymous'
       : style[`${prefix}cross-origin`];
   image.src = style[`${prefix}src`];
 
-  if (image.complete && image.width && image.height) {
-    size = arrayToGlsl([image.width, image.height]);
-  } else {
-    // the size is provided asynchronously using a uniform
-    uniforms[`u_texture${textureId}_size`] = () => {
-      return image.complete ? [image.width, image.height] : [0, 0];
-    };
-    builder.addUniform(`vec2 u_texture${textureId}_size`);
-    size = `u_texture${textureId}_size`;
-  }
+  // the size is provided asynchronously using a uniform
+  uniforms[`u_texture${textureId}_size`] = () => {
+    return image.complete ? [image.width, image.height] : [0, 0];
+  };
+  builder.addUniform(`vec2 u_texture${textureId}_size`);
+  const size = `u_texture${textureId}_size`;
+
   uniforms[`u_texture${textureId}`] = image;
   builder.addUniform(`sampler2D u_texture${textureId}`);
   return size;
