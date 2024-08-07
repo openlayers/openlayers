659511e9022c628d10f8f5458f91a90eaa5a7829
m-elseberg <unknown>
2024-08-07 20:04:15 +0200

    Fix indentation inside WFS.test.js

--- test/node/ol/format/WFS.test.js
+++ test/node/ol/format/WFS.test.js
@@ -5,24 +5,24 @@ import WFS from '../../../../src/ol/format/WFS.js';
 import expect from '../../expect.js';
 
 describe('ol/format/WFS.js', () => {
-    describe('#writeTransaction()', () => {
-        const writeTransaction = (gmlFormat) => {
-            const wfs = new WFS({ version: '2.0.0', gmlFormat });
-            const feature = new Feature({ someProperty: undefined });
-            wfs.writeTransaction([feature], [], [], {
-                featurePrefix: 'sf',
-                featureNS: 'http://www.openplans.org/spearfish',
-                featureType: 'bugsites',
-                nativeElements: []
-            });
-        };
+  describe('#writeTransaction()', () => {
+    const writeTransaction = (gmlFormat) => {
+      const wfs = new WFS({version: '2.0.0', gmlFormat});
+      const feature = new Feature({someProperty: undefined});
+      wfs.writeTransaction([feature], [], [], {
+        featurePrefix: 'sf',
+        featureNS: 'http://www.openplans.org/spearfish',
+        featureType: 'bugsites',
+        nativeElements: [],
+      });
+    };
 
-        it('does not throw on undefined property values when using GML2', () => {
-            expect(() => writeTransaction(new GML2())).to.not.throwException();
-        });
-
-        it('does not throw on undefined property values when using GML3', () => {
-            expect(() => writeTransaction(new GML3())).to.not.throwException();
-        });
+    it('does not throw on undefined property values when using GML2', () => {
+      expect(() => writeTransaction(new GML2())).to.not.throwException();
     });
+
+    it('does not throw on undefined property values when using GML3', () => {
+      expect(() => writeTransaction(new GML3())).to.not.throwException();
+    });
+  });
 });
