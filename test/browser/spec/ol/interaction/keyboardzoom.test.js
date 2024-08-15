237a22c4633e4cb732cf2940e9ab7497cc422c97
Christian Mayer <chris@meggsimum.de>
2024-08-15 16:44:55 +0200

    Tests to check Keyboard interactions in custom el
    Adds tests to check, if the KeyboardPan and
    KeyboardZoom interactions work with a map placed
    in custom HTML element.

--- test/browser/spec/ol/interaction/keyboardzoom.test.js
+++ test/browser/spec/ol/interaction/keyboardzoom.test.js
@@ -78,4 +78,69 @@ describe('ol.interaction.KeyboardZoom', function () {
       expect(spy.called).to.be(false);
     });
   });
+
+  describe('handleEvent with map in ShadowRoot', function () {
+    let olMap;
+    let customMapEl;
+    const options = {
+      altShiftDragRotate: false,
+      doubleClickZoom: false,
+      keyboard: true,
+      mouseWheelZoom: false,
+      shiftDragZoom: false,
+      dragPan: false,
+      pinchRotate: false,
+      pinchZoom: false,
+      onFocusOnly: true,
+    };
+
+    defineCustomMapEl({interactionOpts: options});
+
+    beforeEach(function () {
+      // create custom ol-map element and add to body
+      customMapEl = document.createElement('ol-map');
+      customMapEl.setAttribute('tabindex', '0');
+      customMapEl.style.position = 'absolute';
+      customMapEl.style.left = '-1000px';
+      customMapEl.style.top = '-1000px';
+      customMapEl.style.width = '100px';
+      customMapEl.style.height = '100px';
+      document.body.appendChild(customMapEl);
+
+      olMap = customMapEl.map;
+      olMap.renderSync();
+    });
+
+    afterEach(() => {
+      disposeMap(olMap);
+      customMapEl.remove();
+    });
+
+    it('zooms on +/- keys', function (done) {
+      // we have to wait until the map is rendered
+      olMap.on('rendercomplete', () => {
+        const view = map.getView();
+        const spy = sinon.spy(view, 'animateInternal');
+        const event = new MapBrowserEvent('keydown', map, {
+          type: 'keydown',
+          target: customMapEl,
+          preventDefault: Event.prototype.preventDefault,
+        });
+
+        event.originalEvent.key = '+';
+        olMap.handleMapBrowserEvent(event);
+        expect(spy.getCall(0).args[0].resolution).to.eql(1);
+        view.setResolution(2);
+
+        event.originalEvent.key = '-';
+        olMap.handleMapBrowserEvent(event);
+        expect(spy.getCall(1).args[0].resolution).to.eql(4);
+        view.setResolution(2);
+
+        view.animateInternal.restore();
+
+        done();
+      });
+    });
+  });
 });
