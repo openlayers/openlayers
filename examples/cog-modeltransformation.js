162d20865e4ad44f13260ee949911201a0797445
mike-000 <49240900+mike-000@users.noreply.github.com>
2024-10-12 10:52:05 +0100

    Show tile coordinates

--- examples/cog-modeltransformation.js
+++ examples/cog-modeltransformation.js
@@ -1,6 +1,7 @@
 import GeoTIFF from '../src/ol/source/GeoTIFF.js';
 import Map from '../src/ol/Map.js';
 import OSM from '../src/ol/source/OSM.js';
+import TileDebug from '../src/ol/source/TileDebug.js';
 import TileLayer from '../src/ol/layer/WebGLTile.js';
 import View from '../src/ol/View.js';
 import proj4 from 'proj4';
@@ -12,9 +13,20 @@ register(proj4);
 const cogSource = new GeoTIFF({
   sources: [
     {
-      url: 'https://umbra-open-data-catalog.s3.amazonaws.com/sar-data/tasks/Tanna%20Island,%20Vanuatu/9c76a918-9247-42bf-b9f6-3b4f672bc148/2023-02-12-21-33-56_UMBRA-04/2023-02-12-21-33-56_UMBRA-04_GEC.tif',
+      url:
+        'https://umbra-open-data-catalog.s3.amazonaws.com/sar-data/tasks/Tanna%20Island,%20Vanuatu/' +
+        '9c76a918-9247-42bf-b9f6-3b4f672bc148/2023-02-12-21-33-56_UMBRA-04/2023-02-12-21-33-56_UMBRA-04_GEC.tif',
     },
   ],
+});
+
+const showTilesCheckbox = document.getElementById('show-tiles');
+const debugLayer = new TileLayer({
+  source: new TileDebug({source: cogSource}),
+  visible: showTilesCheckbox.checked,
+});
+showTilesCheckbox.addEventListener('change', () => {
+  debugLayer.setVisible(showTilesCheckbox.checked);
 });
 
 const map = new Map({
@@ -27,6 +39,7 @@ const map = new Map({
       source: cogSource,
       opacity: 0.8,
     }),
+    debugLayer,
   ],
   view: new View(),
 });
