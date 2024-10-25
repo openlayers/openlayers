33f3bb1661a3bb482ade29a5f969a261b653587f
mike-000 <49240900+mike-000@users.noreply.github.com>
2024-10-25 15:55:30 +0100

    Use epsg.io proj4 instead of proj4 from exports

--- src/ol/proj/proj4.js
+++ src/ol/proj/proj4.js
@@ -177,49 +177,55 @@ export function epsgLookupMapTiler(key) {
         `Unexpected response from maptiler.com: ${response.status}`,
       );
     }
-    return response.json().then((json) => {
-      const results = json['results'];
-      if (results?.length > 0) {
-        const result = results.filter(
-          (r) =>
-            r['id']?.['authority'] === 'EPSG' && r['id']?.['code'] === code,
-        )[0];
-        if (result) {
-          const transforms = result['transformations'];
-          if (transforms?.length > 0) {
-            // use default transform if it does not require grids
-            const defaultTransform = result['default_transformation'];
-            if (
-              transforms.filter(
-                (t) =>
-                  t['id']?.['authority'] === defaultTransform?.['authority'] &&
-                  t['id']?.['code'] === defaultTransform?.['code'] &&
-                  t['grids']?.length === 0,
-              ).length > 0
-            ) {
+    const json = await response.json();
+    const results = json['results'];
+    if (results?.length > 0) {
+      const result = results.filter(
+        (r) => r['id']?.['authority'] === 'EPSG' && r['id']?.['code'] === code,
+      )[0];
+      if (result) {
+        const transforms = result['transformations'];
+        if (transforms?.length > 0) {
+          // use default transform if it does not require grids
+          const defaultTransform = result['default_transformation'];
+          if (
+            transforms.filter(
+              (t) =>
+                t['id']?.['authority'] === defaultTransform?.['authority'] &&
+                t['id']?.['code'] === defaultTransform?.['code'] &&
+                t['grids']?.length === 0,
+            ).length > 0
+          ) {
+            return result['exports']?.['proj4'];
+          }
+          // otherwise use most accurate alternative without grids
+          const transform = transforms
+            .filter(
+              (t) =>
+                t['grids']?.length === 0 &&
+                t['target_crs']?.['authority'] === 'EPSG' &&
+                t['target_crs']?.['code'] === 4326 &&
+                t['deprecated'] === false &&
+                t['usable'] === true,
+            )
+            .sort((t1, t2) => t1['accuracy'] - t2['accuracy'])[0]?.['id']?.[
+            'code'
+          ];
+          if (transform) {
+            const response = await fetch(
+              `https://epsg.io/${code}-${transform}.proj4`,
+            );
+            if (!response.ok) {
+              // fallback to default
               return result['exports']?.['proj4'];
             }
-            // otherwise use most accurate alternative without grids
-            const transform = transforms
-              .filter(
-                (t) =>
-                  t['grids']?.length === 0 &&
-                  t['target_crs']?.['authority'] === 'EPSG' &&
-                  t['target_crs']?.['code'] === 4326 &&
-                  t['deprecated'] === false &&
-                  t['usable'] === true,
-              )
-              .sort((t1, t2) => t1['accuracy'] - t2['accuracy'])[0]?.[
-              'exports'
-            ]?.['proj4'];
-            if (transform) {
-              return transform;
-            }
+            const text = await response.text();
+            return text;
           }
-          // fallback to default
-          return result['exports']?.['proj4'];
         }
+        // fallback to default
+        return result['exports']?.['proj4'];
       }
-    });
+    }
   };
 }
