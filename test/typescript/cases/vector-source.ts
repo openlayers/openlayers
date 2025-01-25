787096be20e335f0b63734bfe4c6d0dfc2a33fdf
Andreas Hocevar <andreas.hocevar@gmail.com>
2025-01-25 10:00:27 +0100

    TypeScript test for assigning more specific to less specific vector source

---
+++ test/typescript/cases/vector-source.ts
@@ -0,0 +1,16 @@
+import Feature from '../../../build/ol/Feature.js';
+import Point from '../../../build/ol/geom/Point.js';
+import Snap from '../../../build/ol/interaction/Snap.js';
+import VectorSource from '../../../build/ol/source/Vector.js';
+
+const firstSource: VectorSource<Feature<Point>> = new VectorSource();
+export const secondSource: VectorSource = firstSource; // secondSource is less specific
+
+firstSource.getFeatures()?.[0].getGeometry(); // point is of type Point
+secondSource.getFeatures()?.[0].getGeometry(); // point is of type Geometry
+
+export function testFunction(inputSource: VectorSource<Feature<Point>>) {
+  return new Snap({
+    source: inputSource, // source property is less specific
+  });
+}
