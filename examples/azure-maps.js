838edcfdcd353312bd5de223549d4c8ff5b0ea67
jeanpierre <jean.barbary@wanadoo.fr>
2025-06-13 16:21:02 +0200

    correction

--- examples/azure-maps.js
+++ examples/azure-maps.js
@@ -5,25 +5,27 @@ import {useGeographic} from 'ol/proj.js';
 import ImageTile from 'ol/source/ImageTile.js';
 
 useGeographic();
-const clientSecret = '';
+
+const azureLayer = new TileLayer();
 
 document.getElementById('auth-form').addEventListener('submit', (event) => {
   const clientSecret = event.target.elements['secret'].value;
-});
-
-const source = new ImageTile({
-  url:
-    'https://atlas.microsoft.com/map/tile?subscription-key=' +
-    clientSecret +
-    '&api-version=2.0&tilesetId=microsoft.imagery&zoom={z}&x={x}&y={y}&tileSize=256&language=EN',
+  azureLayer.setSource(
+    new ImageTile({
+      url:
+        'https://atlas.microsoft.com/map/tile?subscription-key=' +
+        clientSecret +
+        '&api-version=2.0&tilesetId=microsoft.imagery&zoom={z}&x={x}&y={y}&tileSize=256&language=EN',
+      crossOrigin: 'anonymous',
+    }),
+  );
 });
 
 const map = new Map({
-  layers: [new TileLayer({source})],
+  layers: [azureLayer],
   target: 'map',
   view: new View({
     center: [1, 45],
-    //   zoom: 10,
-    //    maxZoom: 13,
+    zoom: 12,
   }),
 });
