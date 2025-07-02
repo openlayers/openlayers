535156500485121b14ca0513accfe0d2534a7eca
Andreas Hocevar <andreas.hocevar@gmail.com>
2025-07-02 17:26:15 +0200

    Continue assigning the user provided context

--- src/ol/source/ogcTileUtil.js
+++ src/ol/source/ogcTileUtil.js
@@ -399,11 +399,15 @@ function parseTileMatrixSet(
       }
     }
 
-    Object.assign(localContext, {
-      z: localContext.tileMatrix,
-      x: localContext.tileCol,
-      y: localContext.tileRow,
-    });
+    Object.assign(
+      localContext,
+      {
+        z: localContext.tileMatrix,
+        x: localContext.tileCol,
+        y: localContext.tileRow,
+      },
+      context,
+    );
 
     const url = tileUrlTemplate.replace(/\{(\w+?)\}/g, function (m, p) {
       return localContext[p];
