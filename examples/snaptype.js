59e8e442f13eb4acdfde08ec62ba2fb16bb88ceb
Maximilian Krög <maxi_kroeg@web.de>
2025-09-21 09:30:01 +0200

    Use Icon to style snap type

--- examples/snaptype.js
+++ examples/snaptype.js
@@ -1,22 +1,16 @@
 import Map from '../src/ol/Map.js';
 import View from '../src/ol/View.js';
-import MultiLineString from '../src/ol/geom/MultiLineString.js';
-import Polygon from '../src/ol/geom/Polygon.js';
 import Draw from '../src/ol/interaction/Draw.js';
 import Modify from '../src/ol/interaction/Modify.js';
 import Select from '../src/ol/interaction/Select.js';
 import Snap from '../src/ol/interaction/Snap.js';
 import TileLayer from '../src/ol/layer/Tile.js';
 import VectorLayer from '../src/ol/layer/Vector.js';
-import {
-  fromUserCoordinate,
-  getUserProjection,
-  useGeographic,
-} from '../src/ol/proj.js';
+import {fromUserCoordinate, useGeographic} from '../src/ol/proj.js';
 import OSM from '../src/ol/source/OSM.js';
 import VectorSource from '../src/ol/source/Vector.js';
-import Stroke from '../src/ol/style/Stroke.js';
-import Style, {createEditingStyle} from '../src/ol/style/Style.js';
+import Icon from '../src/ol/style/Icon.js';
+import {createEditingStyle} from '../src/ol/style/Style.js';
 
 useGeographic(); //alias for setUserProjection('EPSG:4326')
 
@@ -46,11 +40,12 @@ const map = new Map({
 });
 
 class ExampleModify {
-  constructor(map, source) {
+  constructor(map, source, style) {
     this.select = new Select();
     const selectedCollection = this.select.getFeatures();
     this.modify = new Modify({
       features: selectedCollection,
+      style,
     });
 
     this.select.on('change:active', function () {
@@ -67,19 +62,13 @@ class ExampleModify {
     this.select.setActive(active);
     this.modify.setActive(active);
   }
-  setStyle(style) {
-    this.modify.getOverlay().setStyle(style);
-  }
 }
 
 class ExampleDraw {
-  constructor(map, source) {
+  constructor(map, source, style) {
     this.interactions = ['Point', 'LineString', 'Polygon', 'Circle'].reduce(
       (all, type) => {
-        const draw = new Draw({
-          type,
-          source,
-        });
+        const draw = new Draw({type, source, style});
         draw.setActive(false);
         map.addInteraction(draw);
         all[type] = draw;
@@ -100,141 +89,60 @@ class ExampleDraw {
     }
     this.activeDrawType = type;
   }
-  setStyle(style) {
-    if (this.activeDrawType) {
-      this.interactions[this.activeDrawType].getOverlay().setStyle(style);
-    }
-  }
 }
-
-const exampleModify = new ExampleModify(map, source);
-const exampleDraw = new ExampleDraw(map, source);
-exampleModify.setActive(optionsForm.elements['interaction'] === 'modify');
-exampleDraw.setActive(
-  optionsForm.elements['interaction'].value === 'draw'
-    ? optionsForm.elements['draw-type'].value
-    : null,
-);
 
 const bearing = function (p1, p2) {
   // p1 ---> p2
   return Math.atan2(p2[0] - p1[0], p2[1] - p1[1]);
 };
 
-const dist = function (p1, p2) {
-  return Math.sqrt(Math.pow(p2[0] - p1[0], 2) + Math.pow(p2[1] - p1[1], 2));
+const styles = createEditingStyle();
+const pointStyle = styles['Point'][0];
+const icons = {
+  intersection: new Icon({src: 'data/intersection.svg'}),
+  vertex: new Icon({src: 'data/vertex.svg'}),
+  edge: new Icon({src: 'data/edge.svg'}),
+  midpoint: new Icon({src: 'data/midpoint.svg'}),
+  none: pointStyle.getImage(),
 };
 
-const polar = function (pt, dir, dist) {
-  return [pt[0] + Math.sin(dir) * dist, pt[1] + Math.cos(dir) * dist];
-};
+const projection = map.getView().getProjection();
+let snapEvent;
+function styleFunction(feature) {
+  const geometryType = feature.getGeometry().getType();
+  if (geometryType === 'Point') {
+    const snapType = snapEvent?.snapType ?? 'none';
+    const icon = icons[snapType];
+    if (snapType === 'edge' || snapType === 'midpoint') {
+      const geometry = snapEvent.feature.getGeometry();
+      const rotation =
+        geometry.getType() == 'Circle'
+          ? bearing(
+              fromUserCoordinate(snapEvent.vertex, projection),
+              fromUserCoordinate(geometry.getCenter(), projection),
+            ) +
+            Math.PI / 2
+          : bearing(
+              fromUserCoordinate(snapEvent.segment[0], projection),
+              fromUserCoordinate(snapEvent.segment[1], projection),
+            );
+      icon.setRotation(rotation);
+    }
 
-const rotateGeometry = function (geom, rotationPoint, angle) {
-  const res = geom.clone();
-  const coord = res.flatCoordinates;
-  for (let i = 0; i < coord.length; i += res.stride) {
-    const d = dist(rotationPoint, [coord[i], coord[i + 1]]);
-    const dir = bearing(rotationPoint, [coord[i], coord[i + 1]]) + angle;
-    const pt = polar(rotationPoint, dir, d);
-    coord[i] = pt[0];
-    coord[i + 1] = pt[1];
+    pointStyle.setImage(icon);
   }
-  return res;
-};
 
-const updateStyle = function (e) {
-  const projection = map.getView().getProjection();
-  const userProjection = getUserProjection();
-  const newStyle = function (feature, resolution) {
-    const styles = createEditingStyle();
-    const color = '#3399cc';
-    const stroke = new Stroke({
-      width: 2,
-      color: color,
-    });
-    const size = 6 * resolution;
-    const [x, y] = fromUserCoordinate(e.vertex, projection);
+  return styles[geometryType];
+}
 
-    let geometry;
-
-    switch (e.snapType) {
-      case 'intersection':
-        geometry = new MultiLineString([
-          [
-            [x - size, y - size],
-            [x + size, y + size],
-          ],
-          [
-            [x - size, y + size],
-            [x + size, y - size],
-          ],
-        ]);
-        break;
-      case 'vertex':
-        geometry = new Polygon([
-          [
-            [x - size, y - size],
-            [x - size, y + size],
-            [x + size, y + size],
-            [x + size, y - size],
-            [x - size, y - size],
-          ],
-        ]);
-        break;
-      case 'edge':
-        geometry = rotateGeometry(
-          new Polygon([
-            [
-              [x - size, y - size],
-              [x + size, y + size],
-              [x - size, y + size],
-              [x + size, y - size],
-              [x - size, y - size],
-            ],
-          ]),
-          [x, y],
-          e.feature.getGeometry().getType() == 'Circle'
-            ? bearing(
-                [x, y],
-                fromUserCoordinate(
-                  e.feature.getGeometry().getCenter(),
-                  projection,
-                ),
-              )
-            : bearing(
-                fromUserCoordinate(e.segment[0], projection),
-                fromUserCoordinate(e.segment[1], projection),
-              ) +
-                Math.PI / 2,
-        );
-        break;
-      case 'midpoint':
-        geometry = new Polygon([
-          [
-            [x, y + size],
-            [x - size, y - size * 0.71],
-            [x + size, y - size * 0.71],
-            [x, y + size],
-          ],
-        ]);
-        break;
-      default:
-    }
-    if (e.type != 'unsnap') {
-      styles['Point'] = [
-        new Style({
-          geometry: userProjection
-            ? geometry.transform(projection, userProjection)
-            : geometry,
-          stroke: stroke,
-        }),
-      ];
-    }
-    return styles[feature.getGeometry().getType()];
-  };
-  exampleDraw.setStyle(newStyle);
-  exampleModify.setStyle(newStyle);
-};
+const exampleModify = new ExampleModify(map, source, styleFunction);
+const exampleDraw = new ExampleDraw(map, source, styleFunction);
+exampleModify.setActive(optionsForm.elements['interaction'] === 'modify');
+exampleDraw.setActive(
+  optionsForm.elements['interaction'].value === 'draw'
+    ? optionsForm.elements['draw-type'].value
+    : null,
+);
 
 // The Snap interaction must be added after the Modify and Draw interactions
 // in order for its map browser event handlers to be fired first.
@@ -251,7 +159,7 @@ const modifySnapOptions = function () {
     midpoint: optionsForm.elements['midpoint'].checked,
   });
   snap.on(['snap', 'unsnap'], (e) => {
-    updateStyle(e);
+    snapEvent = e.type === 'snap' ? e : undefined;
   });
   map.addInteraction(snap);
 };
