6f67029b4e0bd7058268df963d32c5aa6c31eacb
Pirmin Kalberer <pirmin.kalberer@gmail.com>
2025-10-18 10:32:12 +0200

    Update view on change:center

--- examples/equal-earth-geojson.js
+++ examples/equal-earth-geojson.js
@@ -1,6 +1,5 @@
 import {bbox as turfBbox} from '@turf/bbox';
 import {bboxClip} from '@turf/bbox-clip';
-import {booleanValid} from '@turf/boolean-valid';
 import proj4 from 'proj4';
 import Map from '../src/ol/Map.js';
 import View from '../src/ol/View.js';
@@ -8,11 +7,12 @@ import GeoJSON from '../src/ol/format/GeoJSON.js';
 import VectorLayer from '../src/ol/layer/Vector.js';
 import {register} from '../src/ol/proj/proj4.js';
 import {fromLonLat, get as getProjection, toLonLat} from '../src/ol/proj.js';
+import RenderFeature from '../src/ol/render/Feature.js';
 import VectorSource from '../src/ol/source/Vector.js';
 
 // Equal Earth projection with dynamic center meridian
-function dynEqualEarth(center) {
-  const lon0 = Math.round(center[0] / 15) * 15; // rounded to 15°
+function dynEqualEarth(center, round = 15) {
+  const lon0 = Math.round(center[0] / round) * round;
   const code = `EqualEarth${lon0}`;
   let prj = getProjection(code);
   if (!prj) {
@@ -30,18 +30,27 @@ function dynEqualEarth(center) {
   return prj;
 }
 
+const initialProjection = dynEqualEarth([0, 0]);
+
 (async () => {
   const response = await fetch(
     'https://openlayers.org/data/vector/ecoregions.json',
+    // 'https://openlayersbook.github.io/openlayers_book_samples/assets/data/countries.geojson',
   );
   const geojson = await response.json();
 
-  const vectorLayer = new VectorLayer({
-    source: new VectorSource({
+  function jsonSource(geojson, projection) {
+    return new VectorSource({
       features: new GeoJSON({
-        featureProjection: dynEqualEarth([0, 0]),
+        featureProjection: projection,
+        featureClass: RenderFeature,
       }).readFeatures(geojson),
-    }),
+      overlaps: false,
+    });
+  }
+
+  const vectorLayer = new VectorLayer({
+    source: jsonSource(geojson, initialProjection),
     extent: [-17243959.06, -8392927.6, 17243959.06, 8392927.6],
     wrapX: false,
     style: {
@@ -53,32 +62,60 @@ function dynEqualEarth(center) {
     layers: [vectorLayer],
     target: 'map',
     view: new View({
-      projection: dynEqualEarth([0, 0]),
+      projection: initialProjection,
       center: [0, 0],
-      zoom: 1.5,
-      extent: [-17243959.06, -8392927.6, 17243959.06, 8392927.6],
-      constrainOnlyCenter: true,
+      zoom: 0,
+      showFullExtent: true,
     }),
   });
 
+  // Event handler for updating view projection
+  function handleCenterChange() {
+    const degStep = 5;
+    const curView = map.getView();
+    const center = toLonLat(curView.getCenter(), curView.getProjection());
+    const newProjection = dynEqualEarth(center, degStep);
+    if (curView.getProjection().getCode() !== newProjection.getCode()) {
+      curView.un('change:center', handleCenterChange);
+      const lon0 = Math.round(center[0] / degStep) * degStep;
+      // Clip polygons at the new antimeridian to avoid rendering artifacts
+      const clippedJson = clipPolygon(geojson, lon0);
+      // Reload source to apply the new projection
+      vectorLayer.setSource(jsonSource(clippedJson, newProjection));
+      map.setView(
+        new View({
+          projection: newProjection,
+          center: fromLonLat(center, newProjection),
+          zoom: curView.getZoom(),
+          rotation: curView.getRotation(),
+          showFullExtent: true,
+        }),
+      );
+      map.getView().on('change:center', handleCenterChange);
+    }
+  }
+
+  map.getView().on('change:center', handleCenterChange);
+
   function clipPolygon(geojson, lon0) {
-    const eps = 0;
     const minX = lon0 - 180.0;
     const maxX = lon0 + 180.0;
     const clippedJson = {type: 'FeatureCollection', features: []};
     for (const feature of geojson.features) {
       const bbox = turfBbox(feature);
+      const eps = 0.01;
       if (
-        (bbox[0] < minX && bbox[2] > minX) ||
-        (bbox[0] < maxX && bbox[2] > maxX)
+        (bbox[0] < minX + eps && bbox[2] > minX - eps) ||
+        (bbox[0] < maxX + eps && bbox[2] > maxX - eps)
       ) {
-        const clippedFeat = bboxClip(feature, [
-          minX + eps,
-          -90,
-          maxX - eps,
-          90,
-        ]);
-        clippedJson.features.push(clippedFeat);
+        const clippedFeat = bboxClip(feature, [minX, -90, maxX, 90]);
+        const empty = clippedFeat.geometry.coordinates.every(
+          (polygon) => !polygon.length,
+        );
+        if (!empty) {
+          clippedJson.features.push(clippedFeat);
+        }
+
         const offset = bbox[0] < minX ? 360 : -360;
         const transformedFeat = structuredClone(feature);
         if (transformedFeat.geometry.type === 'Polygon') {
@@ -94,23 +131,14 @@ function dynEqualEarth(center) {
             }
           }
         }
-        const wrappedFeat = bboxClip(transformedFeat, [
-          minX + eps,
-          -90,
-          maxX - eps,
-          90,
-        ]);
+        const wrappedFeat = bboxClip(transformedFeat, [minX, -90, maxX, 90]);
         // Remove empty polygons
         // wrappedFeat.geometry.coordinates =
-        //   wrappedFeat.geometry.coordinates.filter(
-        //     (polygon) => !polygon.length,
-        //   );
+        //   wrappedFeat.geometry.coordinates.filter((polygon) => !polygon.length);
         // Remove empty rings
         wrappedFeat.geometry.coordinates = wrappedFeat.geometry.coordinates.map(
           (polygon) => polygon.filter((ring) => !ring.length),
         );
-        // if (booleanValid(wrappedFeat)) {
-        // }
         clippedJson.features.push(wrappedFeat);
       } else {
         clippedJson.features.push(feature);
@@ -118,37 +146,6 @@ function dynEqualEarth(center) {
     }
     return clippedJson;
   }
-
-  // Event handler for updating view projection
-  function handleMoveEnd(evt) {
-    const curView = evt.map.getView();
-    const center = toLonLat(curView.getCenter(), curView.getProjection());
-    const newProjection = dynEqualEarth(center);
-    if (curView.getProjection().getCode() !== newProjection.getCode()) {
-      // map.getView().setCenter([0, 0]);
-      map.setView(
-        new View({
-          projection: newProjection,
-          center: fromLonLat(center, newProjection),
-          zoom: curView.getZoom(),
-          rotation: curView.getRotation(),
-          extent: [-17243959.06, -8392927.6, 17243959.06, 8392927.6],
-          constrainOnlyCenter: true,
-        }),
-      );
-      const lon0 = Math.round(center[0] / 15) * 15;
-      const clippedJson = clipPolygon(geojson, lon0);
-      vectorLayer.setSource(
-        new VectorSource({
-          features: new GeoJSON({
-            featureProjection: dynEqualEarth(center),
-          }).readFeatures(clippedJson),
-        }),
-      );
-    }
-  }
-
-  map.on('moveend', (evt) => handleMoveEnd(evt));
 
   const featureOverlay = new VectorLayer({
     source: new VectorSource(),
@@ -167,7 +164,6 @@ function dynEqualEarth(center) {
 
     const info = document.getElementById('info');
     if (feature) {
-      // console.log(feature.getGeometry().getCoordinates());
       info.innerHTML = feature.get('ECO_NAME') || '&nbsp;';
     } else {
       info.innerHTML = '&nbsp;';
