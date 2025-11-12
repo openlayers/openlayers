c8de737cdc7b1ce2fe097e01214b9e2be5ca760d
Tim Schaub <tim@planet.com>
2025-11-12 10:37:18 +0100

    Updated GeoZarr example

--- examples/geozarr.js
+++ examples/geozarr.js
@@ -1,10 +1,16 @@
 import Map from '../src/ol/Map.js';
+import {
+  getView,
+  withExtentCenter,
+  withHigherResolutions,
+  withLowerResolutions,
+} from '../src/ol/View.js';
 import TileLayer from '../src/ol/layer/WebGLTile.js';
 import GeoZarr from '../src/ol/source/GeoZarr.js';
 import OSM from '../src/ol/source/OSM.js';
 
 const source = new GeoZarr({
-  url: 'http://localhost:5173/s2l2_test.zarr',
+  url: 'https://storage.googleapis.com/open-cogs/geozarr/S2A_MSIL2A_20251025T095111_N0511_R079_T33SVB_20251025T121315.zarr',
   group: 'measurements/reflectance/r20m',
   bands: ['b04', 'b03', 'b02'],
 });
@@ -34,5 +40,10 @@ const map = new Map({
     }),
   ],
   target: 'map',
-  view: source.getView(),
+  view: getView(
+    source,
+    withLowerResolutions(1),
+    withHigherResolutions(2),
+    withExtentCenter(),
+  ),
 });
