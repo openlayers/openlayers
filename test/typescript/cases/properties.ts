9096b14bc2cba08deec67d69e354b052ef77eff7
daniluk4000 <daniluk4000@outlook.com>
2026-01-25 16:04:47 +0300

    Add a test

--- test/typescript/cases/properties.ts
+++ test/typescript/cases/properties.ts
@@ -1,5 +1,7 @@
 import Feature from '../../../build/ol/Feature.js';
+import Map from '../../../build/ol/Map.js';
 import Point from '../../../build/ol/geom/Point.js';
+import type Layer from '../../../build/ol/layer/Layer.js';
 
 export const feature = new Feature<Point, {foo: 'bar'}>({
   geometry: new Point([0, 0]),
@@ -18,3 +20,15 @@ export const untypedFeature = new Feature({
 
 untypedFeature.setProperties({foo: 1});
 untypedFeature.setProperties({foo: 'bar'});
+
+const map = new Map();
+
+map.getFeaturesAtPixel([0, 0], {
+  layerFilter: (layer) => layer.getProperties().foo === 'bar',
+});
+
+map.getFeaturesAtPixel([0, 0], {
+  layerFilter: (layer) =>
+    //@ts-expect-error Should fail because of layer properties generic
+    (layer as Layer<any, any, {foo: 'foo'}>).getProperties().foo === 'bar',
+});
