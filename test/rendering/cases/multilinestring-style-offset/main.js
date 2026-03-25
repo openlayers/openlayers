9b7b4eabc45ed49531163b540ceb128b9384eb86
solovjova <viktorija.solovjova@disy.net>
2026-03-25 20:30:04 +0100

    Aded line offset rendering test with detailed river geometries

---
+++ test/rendering/cases/multilinestring-style-offset/main.js
@@ -0,0 +1,39 @@
+import Map from '../../../../src/ol/Map.js';
+import View from '../../../../src/ol/View.js';
+import GeoJSON from '../../../../src/ol/format/GeoJSON.js';
+import VectorLayer from '../../../../src/ol/layer/Vector.js';
+import VectorSource from '../../../../src/ol/source/Vector.js';
+import Stroke from '../../../../src/ol/style/Stroke.js';
+import Style from '../../../../src/ol/style/Style.js';
+
+// Original dotted gray style for all geometries
+const originalLineStyle = new Style({
+  stroke: new Stroke({color: '#aaa', width: 1, lineDash: [3, 3]}),
+});
+
+// Style for offset lines
+const offsetLineStyle = new Style({
+  stroke: new Stroke({color: '#f00', width: 2, offset: 10}),
+});
+
+const vectorSource = new VectorSource({
+  url: '/data/rivers.geojson',
+  format: new GeoJSON(),
+});
+
+const vectorLayer = new VectorLayer({
+  source: vectorSource,
+  style: [originalLineStyle, offsetLineStyle],
+});
+
+new Map({
+  pixelRatio: 1,
+  layers: [vectorLayer],
+  target: 'map',
+  view: new View({
+    center: [816041.5, 6827504.5],
+    resolution: 350,
+  }),
+});
+
+render();
