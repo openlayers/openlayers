f40eb533a54cf53c6e577c833786c6de3f9f0669
Olivia Guyot <olivia.guyot@camptocamp.com>
2026-04-23 11:16:42 +0200

    Add text to webgl examples
    Also switch flat styles in the mapbox vector tiles examples (canvas and webgl)

--- examples/webgl-vector-tiles.js
+++ examples/webgl-vector-tiles.js
@@ -24,6 +24,7 @@ const style = [
       ['==', ['get', 'layer'], 'landuse'],
       ['==', ['get', 'class'], 'cemetery'],
     ],
+    else: true,
     style: {
       'fill-color': '#e0e4dd',
     },
@@ -34,6 +35,7 @@ const style = [
       ['==', ['get', 'layer'], 'landuse'],
       ['==', ['get', 'class'], 'hospital'],
     ],
+    else: true,
     style: {
       'fill-color': '#fde',
     },
@@ -44,6 +46,7 @@ const style = [
       ['==', ['get', 'layer'], 'landuse'],
       ['==', ['get', 'class'], 'school'],
     ],
+    else: true,
     style: {
       'fill-color': '#f0e8f8',
     },
@@ -54,12 +57,14 @@ const style = [
       ['==', ['get', 'layer'], 'landuse'],
       ['==', ['get', 'class'], 'wood'],
     ],
+    else: true,
     style: {
       'fill-color': 'rgb(233,238,223)',
     },
   },
   {
     filter: ['==', ['get', 'layer'], 'waterway'],
+    else: true,
     style: {
       'stroke-color': '#a0c8f0',
       'stroke-width': 1,
@@ -67,6 +72,7 @@ const style = [
   },
   {
     filter: ['==', ['get', 'layer'], 'water'],
+    else: true,
     style: {
       'fill-color': '#a0c8f0',
     },
@@ -77,6 +83,7 @@ const style = [
       ['==', ['get', 'layer'], 'aeroway'],
       ['==', ['geometry-type'], 'Polygon'],
     ],
+    else: true,
     style: {
       'fill-color': 'rgb(242,239,235)',
     },
@@ -88,12 +95,14 @@ const style = [
       ['==', ['geometry-type'], 'LineString'],
       ['<=', ['resolution'], 76.43702828517625],
     ],
+    else: true,
     style: {
       'fill-color': '#f0ede9',
     },
   },
   {
     filter: ['==', ['get', 'layer'], 'building'],
+    else: true,
     style: {
       'fill-color': '#f2eae2',
       'stroke-color': '#dfdbd7',
@@ -106,6 +115,7 @@ const style = [
       ['==', ['get', 'layer'], 'tunnel'],
       ['==', ['get', 'class'], 'motorway_link'],
     ],
+    else: true,
     style: {
       'stroke-color': '#e9ac77',
       'stroke-width': 1,
@@ -117,6 +127,7 @@ const style = [
       ['==', ['get', 'layer'], 'tunnel'],
       ['==', ['get', 'class'], 'service'],
     ],
+    else: true,
     style: {
       'stroke-color': '#cfcdca',
       'stroke-width': 1,
@@ -132,6 +143,7 @@ const style = [
         ['==', ['get', 'class'], 'street_limited'],
       ],
     ],
+    else: true,
     style: {
       'stroke-color': '#cfcdca',
       'stroke-width': 1,
@@ -144,6 +156,7 @@ const style = [
       ['==', ['get', 'class'], 'main'],
       ['<=', ['resolution'], 1222.99245256282],
     ],
+    else: true,
     style: {
       'stroke-color': '#e9ac77',
       'stroke-width': 1,
@@ -155,6 +168,7 @@ const style = [
       ['==', ['get', 'layer'], 'tunnel'],
       ['==', ['get', 'class'], 'motorway'],
     ],
+    else: true,
     style: {
       'stroke-color': '#e9ac77',
       'stroke-width': 1,
@@ -166,6 +180,7 @@ const style = [
       ['==', ['get', 'layer'], 'tunnel'],
       ['==', ['get', 'class'], 'path'],
     ],
+    else: true,
     style: {
       'stroke-color': '#cba',
       'stroke-width': 1,
@@ -177,6 +192,7 @@ const style = [
       ['==', ['get', 'layer'], 'tunnel'],
       ['==', ['get', 'class'], 'major_rail'],
     ],
+    else: true,
     style: {
       'stroke-color': '#bbb',
       'stroke-width': 2,
@@ -188,6 +204,7 @@ const style = [
       ['==', ['get', 'layer'], 'road'],
       ['==', ['get', 'class'], 'motorway_link'],
     ],
+    else: true,
     style: {
       'stroke-color': '#e9ac77',
       'stroke-width': 1,
@@ -204,6 +221,7 @@ const style = [
       ],
       ['==', ['geometry-type'], 'LineString'],
     ],
+    else: true,
     style: {
       'stroke-color': '#cfcdca',
       'stroke-width': 1,
@@ -216,6 +234,7 @@ const style = [
       ['==', ['get', 'class'], 'main'],
       ['<=', ['resolution'], 1222.99245256282],
     ],
+    else: true,
     style: {
       'stroke-color': '#e9ac77',
       'stroke-width': 1,
@@ -228,6 +247,7 @@ const style = [
       ['==', ['get', 'class'], 'motorway'],
       ['<=', ['resolution'], 4891.96981025128],
     ],
+    else: true,
     style: {
       'stroke-color': '#e9ac77',
       'stroke-width': 1,
@@ -239,6 +259,7 @@ const style = [
       ['==', ['get', 'layer'], 'road'],
       ['==', ['get', 'class'], 'path'],
     ],
+    else: true,
     style: {
       'stroke-color': '#cba',
       'stroke-width': 1,
@@ -250,6 +271,7 @@ const style = [
       ['==', ['get', 'layer'], 'road'],
       ['==', ['get', 'class'], 'major_rail'],
     ],
+    else: true,
     style: {
       'stroke-color': '#bbb',
       'stroke-width': 2,
@@ -265,6 +287,7 @@ const style = [
         ['==', ['get', 'class'], 'motorway_link'],
       ],
     ],
+    else: true,
     style: {
       'stroke-color': '#e9ac77',
       'stroke-width': 1,
@@ -281,6 +304,7 @@ const style = [
         ['==', ['get', 'class'], 'service'],
       ],
     ],
+    else: true,
     style: {
       'stroke-color': '#cfcdca',
       'stroke-width': 1,
@@ -293,6 +317,7 @@ const style = [
       ['==', ['get', 'class'], 'main'],
       ['<=', ['resolution'], 1222.99245256282],
     ],
+    else: true,
     style: {
       'stroke-color': '#e9ac77',
       'stroke-width': 1,
@@ -304,6 +329,7 @@ const style = [
       ['==', ['get', 'layer'], 'bridge'],
       ['==', ['get', 'class'], 'path'],
     ],
+    else: true,
     style: {
       'stroke-color': '#cba',
       'stroke-width': 1,
@@ -315,6 +341,7 @@ const style = [
       ['==', ['get', 'layer'], 'bridge'],
       ['==', ['get', 'class'], 'major_rail'],
     ],
+    else: true,
     style: {
       'stroke-color': '#bbb',
       'stroke-width': 2,
@@ -327,6 +354,7 @@ const style = [
       ['>=', ['get', 'admin_level'], 2],
       ['==', ['get', 'maritime'], 0],
     ],
+    else: true,
     style: {
       'stroke-color': '#9e9cab',
       'stroke-width': 1,
@@ -339,13 +367,207 @@ const style = [
       ['>=', ['get', 'admin_level'], 2],
       ['==', ['get', 'maritime'], 1],
     ],
+    else: true,
     style: {
       'stroke-color': '#a0c8f0',
       'stroke-width': 1,
     },
   },
   {
-    style: {'circle-radius': 4, 'circle-fill-color': '#777'},
+    filter: [
+      'any',
+      ['==', ['get', 'layer'], 'country_label'],
+      ['==', ['get', 'layer'], 'place_label'],
+    ],
+    else: true,
+    style: {
+      'text-value': ['get', 'name_en'],
+      'text-font': 'bold 11px "Open Sans", "Arial Unicode MS", sans-serif',
+      'text-fill-color': '#334',
+      'text-stroke-color': 'rgba(255,255,255,0.8)',
+      'text-stroke-width': 2,
+    },
+  },
+  {
+    filter: [
+      'all',
+      ['==', ['get', 'layer'], 'country_label'],
+      ['==', ['get', 'scalerank'], 2],
+      ['<=', ['resolution'], 19567.87924100512],
+    ],
+    else: true,
+    style: {
+      'text-value': ['get', 'name_en'],
+      'text-font': 'bold 10px "Open Sans", "Arial Unicode MS", sans-serif',
+      'text-fill-color': '#334',
+      'text-stroke-color': 'rgba(255,255,255,0.8)',
+      'text-stroke-width': 2,
+    },
+  },
+  {
+    filter: [
+      'all',
+      ['==', ['get', 'layer'], 'country_label'],
+      ['==', ['get', 'scalerank'], 3],
+      ['<=', ['resolution'], 9783.93962050256],
+    ],
+    else: true,
+    style: {
+      'text-value': ['get', 'name_en'],
+      'text-font': 'bold 9px "Open Sans", "Arial Unicode MS", sans-serif',
+      'text-fill-color': '#334',
+      'text-stroke-color': 'rgba(255,255,255,0.8)',
+      'text-stroke-width': 2,
+    },
+  },
+  {
+    filter: [
+      'all',
+      ['==', ['get', 'layer'], 'country_label'],
+      ['==', ['get', 'scalerank'], 4],
+      ['<=', ['resolution'], 4891.96981025128],
+    ],
+    else: true,
+    style: {
+      'text-value': ['get', 'name_en'],
+      'text-font': 'bold 8px "Open Sans", "Arial Unicode MS", sans-serif',
+      'text-fill-color': '#334',
+      'text-stroke-color': 'rgba(255,255,255,0.8)',
+      'text-stroke-width': 2,
+    },
+  },
+  {
+    filter: [
+      'all',
+      ['==', ['get', 'layer'], 'marine_label'],
+      ['==', ['get', 'labelrank'], 1],
+      ['==', ['geometry-type'], 'Point'],
+    ],
+    else: true,
+    style: {
+      'text-value': ['get', 'name_en'],
+      'text-font': 'italic 11px "Open Sans", "Arial Unicode MS", sans-serif',
+      'text-fill-color': '#74aee9',
+      'text-stroke-color': 'rgba(255,255,255,0.8)',
+      'text-stroke-width': 1,
+    },
+  },
+  {
+    filter: [
+      'all',
+      ['==', ['get', 'layer'], 'marine_label'],
+      ['==', ['get', 'labelrank'], 2],
+      ['==', ['geometry-type'], 'Point'],
+    ],
+    else: true,
+    style: {
+      'text-value': ['get', 'name_en'],
+      'text-font': 'italic 11px "Open Sans", "Arial Unicode MS", sans-serif',
+      'text-fill-color': '#74aee9',
+      'text-stroke-color': 'rgba(255,255,255,0.8)',
+      'text-stroke-width': 1,
+    },
+  },
+  {
+    filter: [
+      'all',
+      ['==', ['get', 'layer'], 'marine_label'],
+      ['==', ['get', 'labelrank'], 3],
+      ['==', ['geometry-type'], 'Point'],
+    ],
+    else: true,
+    style: {
+      'text-value': ['get', 'name_en'],
+      'text-font': 'italic 10px "Open Sans", "Arial Unicode MS", sans-serif',
+      'text-fill-color': '#74aee9',
+      'text-stroke-color': 'rgba(255,255,255,0.8)',
+      'text-stroke-width': 1,
+    },
+  },
+  {
+    filter: [
+      'all',
+      ['==', ['get', 'layer'], 'marine_label'],
+      ['==', ['get', 'labelrank'], 4],
+      ['==', ['geometry-type'], 'Point'],
+    ],
+    else: true,
+    style: {
+      'text-value': ['get', 'name_en'],
+      'text-font': 'italic 9px "Open Sans", "Arial Unicode MS", sans-serif',
+      'text-fill-color': '#74aee9',
+      'text-stroke-color': 'rgba(255,255,255,0.8)',
+      'text-stroke-width': 1,
+    },
+  },
+  {
+    filter: [
+      'all',
+      ['==', ['get', 'layer'], 'place_label'],
+      ['==', ['get', 'type'], 'city'],
+      ['<=', ['resolution'], 1222.99245256282],
+    ],
+    else: true,
+    style: {
+      'text-value': ['get', 'name_en'],
+      'text-font': '11px "Open Sans", "Arial Unicode MS", sans-serif',
+      'text-fill-color': '#333',
+      'text-stroke-color': 'rgba(255,255,255,0.8)',
+      'text-stroke-width': 1,
+    },
+  },
+  {
+    filter: [
+      'all',
+      ['==', ['get', 'layer'], 'place_label'],
+      ['==', ['get', 'type'], 'town'],
+      ['<=', ['resolution'], 305.748113140705],
+    ],
+    else: true,
+    style: {
+      'text-value': ['get', 'name_en'],
+      'text-font': '9px "Open Sans", "Arial Unicode MS", sans-serif',
+      'text-fill-color': '#333',
+      'text-stroke-color': 'rgba(255,255,255,0.8)',
+      'text-stroke-width': 1,
+    },
+  },
+  {
+    filter: [
+      'all',
+      ['==', ['get', 'layer'], 'place_label'],
+      ['==', ['get', 'type'], 'village'],
+      ['<=', ['resolution'], 38.21851414258813],
+    ],
+    else: true,
+    style: {
+      'text-value': ['get', 'name_en'],
+      'text-font': '8px "Open Sans", "Arial Unicode MS", sans-serif',
+      'text-fill-color': '#333',
+      'text-stroke-color': 'rgba(255,255,255,0.8)',
+      'text-stroke-width': 1,
+    },
+  },
+  {
+    filter: [
+      'all',
+      ['==', ['get', 'layer'], 'place_label'],
+      ['<=', ['resolution'], 19.109257071294063],
+      [
+        'any',
+        ['==', ['get', 'type'], 'hamlet'],
+        ['==', ['get', 'type'], 'suburb'],
+        ['==', ['get', 'type'], 'neighbourhood'],
+      ],
+    ],
+    else: true,
+    style: {
+      'text-value': ['get', 'name_en'],
+      'text-font': 'bold 9px "Arial Narrow"',
+      'text-fill-color': '#633',
+      'text-stroke-color': 'rgba(255,255,255,0.8)',
+      'text-stroke-width': 1,
+    },
   },
 ];
 
@@ -370,5 +592,6 @@ const map = new Map({
   view: new View({
     center: [0, 0],
     zoom: 2,
+    multiWorld: true,
   }),
 });
