661544f3005486070ebc96fb7629967444083404
Olivia Guyot <olivia.guyot@camptocamp.com>
2026-05-26 23:54:13 +0200

    Style / add a more general flatStyleLikeToStyleFunction util
    This is now used for canvas vector layers as well

--- test/browser/spec/ol/layer/Vector.test.js
+++ test/browser/spec/ol/layer/Vector.test.js
@@ -201,6 +201,47 @@ describe('ol.layer.Vector', function () {
       expect(secondStroke.getColor()).to.eql([255, 255, 0, 1]);
       expect(secondStroke.getWidth()).to.be(5);
     });
+
+    it('accepts an array of flat style rules', () => {
+      const layer = new VectorLayer();
+      layer.setStyle([
+        {
+          style: {
+            'stroke-color': 'red',
+            'stroke-width': 10,
+          },
+        },
+        {
+          style: {
+            'stroke-color': 'yellow',
+            'stroke-width': 5,
+          },
+        },
+      ]);
+
+      const styleFunction = layer.getStyleFunction();
+      expect(styleFunction).to.be.a(Function);
+
+      const styles = styleFunction(new Feature(), 1);
+      expect(styles).to.be.an(Array);
+      expect(styles).to.have.length(2);
+
+      const first = styles[0];
+      expect(first).to.be.a(Style);
+
+      const firstStroke = first.getStroke();
+      expect(firstStroke).to.be.a(Stroke);
+      expect(firstStroke.getColor()).to.eql([255, 0, 0, 1]);
+      expect(firstStroke.getWidth()).to.be(10);
+
+      const second = styles[1];
+      expect(second).to.be.a(Style);
+
+      const secondStroke = second.getStroke();
+      expect(secondStroke).to.be.a(Stroke);
+      expect(secondStroke.getColor()).to.eql([255, 255, 0, 1]);
+      expect(secondStroke.getWidth()).to.be(5);
+    });
   });
 
   describe('#getFeatures()', function () {
