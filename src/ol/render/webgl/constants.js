ff0d2340a587fb80d359359ccc3db6c2cd5da812
Olivia Guyot <olivia.guyot@camptocamp.com>
2026-05-28 20:04:27 +0200

    Webgl / use a common function for messaging workers
    this fixes memory leaks because some listeners were never unsubscribed

--- src/ol/render/webgl/constants.js
+++ src/ol/render/webgl/constants.js
@@ -26,7 +26,6 @@ export const TextOverlayWorkerMessageType = {
  * When the buffers are generated, the worked will send a message of the same type to the main thread, with
  * the generated buffers in it.
  * Note that any addition properties present in the message *will* be sent back to the main thread.
- * @property {number} id Message id; will be used both in request and response as a means of identification
  * @property {WebGLWorkerMessageType} type Message type
  * @property {ArrayBufferLike} renderInstructions render instructions raw binary buffer.
  * @property {number} [customAttributesSize] Amount of hit detection + custom attributes count in the render instructions.
@@ -34,6 +33,7 @@ export const TextOverlayWorkerMessageType = {
  * @property {ArrayBuffer} [vertexAttributesBuffer] Vertex attributes array raw binary buffer (sent by the worker).
  * @property {ArrayBuffer} [instanceAttributesBuffer] Instance attributes array raw binary buffer (sent by the worker).
  * @property {import("../../transform.js").Transform} [renderInstructionsTransform] Transformation matrix used to project the instructions coordinates
+ * @property {number} [id] Message id; will be used both in request and response as a means of identification
  */
 
 /**
@@ -42,7 +42,6 @@ export const TextOverlayWorkerMessageType = {
  * - BUILD_INSTRUCTIONS is used to transform render instructions into canvas text rendering batches
  * - RENDER is used to actually draw all current text rendering batches on the offscreen canvas; the render list is cleared after each render
  * @property {TextOverlayWorkerMessageType} type Message type
- * @property {number} id Message id; will be used both in request and response as a means of identification
  * @property {ArrayBuffer} [polygonRenderInstructions] Polygon render instructions array buffer
  * @property {ArrayBuffer} [lineStringRenderInstructions] Line string render instructions array buffer
  * @property {ArrayBuffer} [pointRenderInstructions] Point render instructions array buffer
@@ -50,4 +49,8 @@ export const TextOverlayWorkerMessageType = {
  * @property {import("../../Map.js").FrameState} [frameState] Frame state of the rendered image
  * @property {string} [instructionsSetKey] Key corresponding to a generated text instructions set
  * @property {import('../../style/flat.js').FlatStyleLike} [style] Flat style
+ * @property {Uint8Array} [labelsArray] Labels array
+ * @property {Object<string, number>} [customAttributesSizes] Size of each custom attribute (by name)
+ * @property {import("../../transform.js").Transform} [renderInstructionsTransform] Transformation matrix used to project the instructions coordinates
+ * @property {number} [id] Message id; will be used both in request and response as a means of identification
  */
