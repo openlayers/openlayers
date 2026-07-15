7c9474bb4a7c11032bc61886d85d0149b85b5522
Ziga Cernigoj <ziga.cernigoj@planet.com>
2026-07-15 18:26:08 +0200

    Expand SentinelHub custom format example with choice for setting format for initialization

--- examples/sentinel-hub-custom-format.js
+++ examples/sentinel-hub-custom-format.js
@@ -6,62 +6,68 @@ import SentinelHub from '../src/ol/source/SentinelHub.js';
 
 useGeographic();
 
-const source = new SentinelHub({
-  data: [
-    {
-      type: 'sentinel-2-l2a',
-      dataFilter: {
-        timeRange: {
-          from: '2024-05-30T00:00:00Z',
-          to: '2024-06-01T00:00:00Z',
-        },
-      },
-    },
-  ],
-  evalscript: {
-    setup: () => ({
-      input: ['B12', 'B08', 'B04'],
-      output: {bands: 3},
-    }),
-    evaluatePixel: (sample) => [
-      2.5 * sample.B12,
-      2 * sample.B08,
-      2 * sample.B04,
-    ],
-  },
-  format: 'image/png',
-});
+let source;
 
-const map = new Map({
-  layers: [new TileLayer({source})],
-  target: 'map',
-  view: new View({
-    center: [-121.75, 46.85],
-    zoom: 10,
-    minZoom: 7,
-    maxZoom: 13,
-  }),
+function reportError() {
+  if (source.getState() === 'error') {
+    alert(source.getError());
+  }
+}
+
+const formatDropdown = document.getElementById('format');
+
+formatDropdown.addEventListener('change', () => {
+  const newFormat =
+    formatDropdown.value === '' ? undefined : formatDropdown.value;
+  source.setFormat(newFormat);
 });
 
 document.getElementById('auth-form').addEventListener('submit', (event) => {
   const clientId = event.target.elements['id'].value;
   const clientSecret = event.target.elements['secret'].value;
-  source.setAuth({clientId, clientSecret});
-});
+  const initFormatValue = event.target.elements['initFormat'].value;
+  const initFormat = initFormatValue === '' ? undefined : initFormatValue;
 
-const formatDropdown = document.getElementById('format');
+  source = new SentinelHub({
+    data: [
+      {
+        type: 'sentinel-2-l2a',
+        dataFilter: {
+          timeRange: {
+            from: '2024-05-30T00:00:00Z',
+            to: '2024-06-01T00:00:00Z',
+          },
+        },
+      },
+    ],
+    evalscript: {
+      setup: () => ({
+        input: ['B12', 'B08', 'B04'],
+        output: {bands: 3},
+      }),
+      evaluatePixel: (sample) => [
+        2.5 * sample.B12,
+        2 * sample.B08,
+        2 * sample.B04,
+      ],
+    },
+    format: initFormat,
+    auth: {clientId, clientSecret},
+  });
+  source.on('change', reportError);
+  reportError();
 
-function updateInputData() {
-  const newFormat =
-    formatDropdown.value === '' ? undefined : formatDropdown.value;
-  source.setFormat(newFormat);
-}
+  new Map({
+    layers: [new TileLayer({source})],
+    target: 'map',
+    view: new View({
+      center: [-121.75, 46.85],
+      zoom: 10,
+      minZoom: 7,
+      maxZoom: 13,
+    }),
+  });
 
-formatDropdown.addEventListener('change', () => updateInputData());
-updateInputData();
-
-source.on('change', () => {
-  if (source.getState() === 'error') {
-    alert(source.getError());
-  }
+  formatDropdown.value = initFormatValue;
+  formatDropdown.disabled = false;
 });
